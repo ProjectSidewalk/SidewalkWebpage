5b4b45b1ca412534d69036d9e0358b609d51ea02
Kotaro Hara <koe.bluebear@gmail.com>
2015-11-18 15:55:16 -0500

    Added region models

---
+++ sidewalk-webpage/app/models/region/RegionTable.scala
@@ -0,0 +1,41 @@
+package models.region
+
+import java.sql.Timestamp
+
+import com.vividsolutions.jts.geom.Polygon
+import models.utils.MyPostgresDriver.simple._
+import play.api.Play.current
+
+import scala.slick.lifted.ForeignKeyQuery
+
+case class Region(regionId: Int, regionTypeId: Int, dataSource: String, description: String, geom: Polygon)
+
+class RegionTable(tag: Tag) extends Table[Region](tag, Some("sidewalk"), "region") {
+  def regionId = column[Int]("region_id", O.PrimaryKey)
+  def regionTypeId = column[Int]("region_type_id", O.NotNull)
+  def dataSource = column[String]("data_source", O.Nullable)
+  def description = column[String]("description", O.Nullable)
+  def geom = column[Polygon]("geom")
+
+  def * = (regionId, regionTypeId, dataSource, description, geom) <> ((Region.apply _).tupled, Region.unapply)
+
+  def regionType: ForeignKeyQuery[RegionTypeTable, RegionType] =
+    foreignKey("region_region_type_id_fkey", regionTypeId, TableQuery[RegionTypeTable])(_.regionTypeId)
+}
+
+
+/**
+ * Data access object for the sidewalk_edge table
+ */
+object SidewalkEdgeTable {
+  val db = play.api.db.slick.DB
+  val regions = TableQuery[RegionTable]
+
+  /**
+   * Returns a list of all the sidewalk edges
+   * @return A list of SidewalkEdge objects.
+   */
+  def all: List[Region] = db.withSession { implicit session =>
+    regions.list
+  }
+}
---
+++ sidewalk-webpage/app/models/region/RegionType.scala
@@ -0,0 +1,29 @@
+package models.region
+
+import com.vividsolutions.jts.geom.Polygon
+import models.utils.MyPostgresDriver.simple._
+import play.api.Play.current
+
+import scala.slick.lifted.ForeignKeyQuery
+
+case class RegionType(regionTypeId: Int, description: String)
+
+class RegionTypeTable(tag: Tag) extends Table[RegionType](tag, Some("sidewalk"), "region_type") {
+  def regionTypeId = column[Int]("region_type_id", O.PrimaryKey)
+  def description = column[String]("description", O.NotNull)
+
+  def * = (regionTypeId, description) <> ((RegionType.apply _).tupled, RegionType.unapply)
+}
+
+object RegionTypeTable {
+  val db = play.api.db.slick.DB
+  val regionTypes = TableQuery[RegionTypeTable]
+
+  /**
+   * Returns a list of all the sidewalk edges
+   * @return A list of SidewalkEdge objects.
+   */
+  def all: List[RegionType] = db.withSession { implicit session =>
+    regionTypes.list
+  }
+}
