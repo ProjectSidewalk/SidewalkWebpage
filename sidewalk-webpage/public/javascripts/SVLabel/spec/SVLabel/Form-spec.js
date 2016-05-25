2bedbe830d77e29a0aa3e637413b62a0af6debeb
Kotaro Hara <koe.bluebear@gmail.com>
2016-05-25 12:56:27 -0400

    Removed sidewalk-webpage directory

--- sidewalk-webpage/public/javascripts/SVLabel/spec/SVLabel/Form-spec.js
+++
@@ -1,9 +0,0 @@
-// describe("Tests for the Form module.", function () {
-//
-//   describe("The Form constructor", function(){
-//     it("should XXX", function() {
-//       // Todo
-//     });
-//   });
-//
-// });
