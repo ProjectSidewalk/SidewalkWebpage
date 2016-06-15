2c357f05d1b438391e1f8154786d0623d756905a
Kotaro Hara <koe.bluebear@gmail.com>
2016-06-15 14:51:32 -0400

    FAQ for conflicting images #63

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -65,6 +65,21 @@ class ApplicationController @Inject() (implicit val env: Environment[User, Sessi
     }
   }
 
+  def faq = UserAwareAction.async { implicit request =>
+    val now = new DateTime(DateTimeZone.UTC)
+    val timestamp: Timestamp = new Timestamp(now.getMillis)
+    val ipAddress: String = request.remoteAddress
+
+    request.identity match {
+      case Some(user) =>
+        WebpageActivityTable.save(WebpageActivity(0, user.userId.toString, ipAddress, "Visit_FAQ", timestamp))
+        Future.successful(Ok(views.html.faq("Project Sidewalk - About", Some(user))))
+      case None =>
+        WebpageActivityTable.save(WebpageActivity(0, anonymousUser.userId.toString, ipAddress, "Visit_FAQ", timestamp))
+        Future.successful(Ok(views.html.faq("Project Sidewalk - About")))
+    }
+  }
+
   /**
     * Returns the terms page
     * @return
