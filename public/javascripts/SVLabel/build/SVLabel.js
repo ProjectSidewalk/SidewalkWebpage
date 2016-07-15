2fe7c96c7707ed6cb020685e75c5556c4da69282
Dan <daniilzadorozhnyy@gmail.com>
2016-07-15 16:35:12 -0400

    Map Overlay D3 Animation

--- public/javascripts/SVLabel/build/SVLabel.js
+++ public/javascripts/SVLabel/build/SVLabel.js
@@ -4266,8 +4266,6 @@ function Map ($, google, turf, params) {
         if ("missionProgress" in svl) {
             svl.missionProgress.update();
         }
-
->>>>>>> c8e4c22be2fc482447b4627fd1c695f9572cbf0f
         if ("taskContainer" in svl) {
             svl.taskContainer.update();
 
@@ -11715,7 +11713,7 @@ function ModalMissionComplete ($, d3, L) {
         .enter().append("rect")
         .attr("x", 0)
         .attr("y", 0)
-        .attr("fill", "rgba(49,189,100,1)")
+        .attr("fill", "rgba(100,240,110,1)")
         .attr("height", svgCoverageBarHeight)
         .attr("width", 0);
     var horizontalBarMissionLabel = gBarChart2.selectAll("text")
@@ -11748,12 +11746,158 @@ function ModalMissionComplete ($, d3, L) {
     }
 
     function _updateMissionProgressStatistics (auditedDistance, missionDistance, remainingDistance, unit) {
-        if (!unit) unit = "kilometers"; //why?
+        if (!unit) unit = "kilometers";
         svl.ui.modalMissionComplete.totalAuditedDistance.html(auditedDistance.toFixed(1) + " " + unit);
         svl.ui.modalMissionComplete.missionDistance.html(missionDistance.toFixed(1) + " " + unit);
         svl.ui.modalMissionComplete.remainingDistance.html(remainingDistance.toFixed(1) + " " + unit);
     }
+    
+    // converts GeoJSON from the LineString format that we use into a collection of Points for the d3 animation
+    function linestringToPoint(coll){
+        // getting point data from list
+        var coorList = coll.features[0].geometry.coordinates;
+        var featureList = [];
+        var len = coorList.length;
 
+        for(i = 0; i < len; i ++){
+            // convert to point GeoJSON
+            var feature = turf.point(coorList[i]);
+            featureList.push(feature);
+        }
+        var geoJSON = {
+            "type": "FeatureCollection",
+            "features": featureList
+        };
+        return geoJSON;
+    }
+
+    // animates each segment of the 
+    function _animateMissionTasks(coll, index, max){
+        var collection = linestringToPoint(coll[index].getGeoJSON());
+        var featuresdata = collection.features;
+    
+        svg = d3.select(map.getPanes().overlayPane).append("svg"),
+
+        g = svg.append("g").attr("class", "leaflet-zoom-hide");
+
+        var transform = d3.geo.transform({
+            point: projectPoint
+        });
+
+        var d3path = d3.geo.path().projection(transform);
+
+        var toLine = d3.svg.line()
+            .interpolate("linear")
+            .x(function(d) {
+                return applyLatLngToLayer(d).x;
+            })
+            .y(function(d) {
+                return applyLatLngToLayer(d).y;
+            });
+
+        var linePath = g.selectAll(".lineConnect")
+            .data([featuresdata])
+            .enter()
+            .append("path")
+            .attr("class", "lineConnect");
+
+        var originANDdestination = [featuresdata[0], featuresdata[featuresdata.length-1]];
+
+        // reset projection on zoom
+        map.on("viewreset", reset);
+
+        // this puts stuff on the map! 
+        reset();
+        transition();
+
+        // Reposition the SVG to cover the features.
+        function reset() {
+            var bounds = d3path.bounds(collection),
+                topLeft = bounds[0],
+                bottomRight = bounds[1];
+
+            // Setting the size and location of the overall SVG container
+            svg.attr("width", bottomRight[0] - topLeft[0] + 120)
+                .attr("height", bottomRight[1] - topLeft[1] + 120)
+                .style("left", topLeft[0] - 50 + "px")
+                .style("top", topLeft[1] - 50 + "px");
+
+            // set opacity to zero before it is rendered
+            linePath.style("opacity", "0").attr("d", toLine);
+            g.attr("transform", "translate(" + (-topLeft[0] + 50) + "," + (-topLeft[1] + 50) + ")");
+
+        } // end reset
+
+        
+        function transition() {
+            linePath.transition()
+                .duration(3000)
+                .attrTween("stroke-dasharray", tweenDash)
+                .each("end", function() {
+                    if(index < max){
+                        // recursively call the next animation render when this one is done
+                        _animateMissionTasks(coll, index+1, max);
+                    }
+                    else{
+                        // unlock zooming
+                        map.touchZoom.enable();
+                        map.doubleClickZoom.enable();
+                        map.scrollWheelZoom.enable();
+                        map.boxZoom.enable();
+                        //render the complete path as plain svg to avoid scaling issues
+                        renderPath(coll);
+
+                    }
+                    
+                }); 
+        } //end transition
+
+        // this function feeds the attrTween operator above with the 
+        // stroke and dash lengths
+        function tweenDash() {
+            return function(t) {
+                // reapply opacity to prevent phantom segment
+                linePath.style("opacity", "1");
+                //total length of path (single value)
+                var l = linePath.node().getTotalLength(); 
+                interpolate = d3.interpolateString("0," + l, l + "," + l);
+                var p = linePath.node().getPointAtLength(t * l);
+
+                return interpolate(t);
+            }
+        } //end tweenDash
+
+        function projectPoint(x, y) {
+            var point = map.latLngToLayerPoint(new L.LatLng(y, x));
+            this.stream.point(point.x, point.y);
+        }
+
+        function applyLatLngToLayer(d) {
+            var y = d.geometry.coordinates[1];
+            var x = d.geometry.coordinates[0];
+            return map.latLngToLayerPoint(new L.LatLng(y, x));
+        }
+    }
+
+    // create array of street edge ids from mission tasks
+    function streetEdgeIds(missionTasks){
+        var ids = [];
+        var len = missionTasks.length;
+        for(var i = 0; i < len; i++){
+            ids.push(missionTasks[i].getStreetEdgeId());
+        }
+        return ids;
+    }
+    // render svg segments
+    function renderPath(missionTasks){
+        var missionTaskLayerStyle = { color: "rgb(100,240,110)", opacity: 1, weight: 3 };
+        var len = missionTasks.length;
+        for (var i = 0; i < len; i++) {
+            var  geojsonFeature = missionTasks[i].getFeature();
+            var layer = L.geoJson(geojsonFeature).addTo(map);
+            layer.setStyle(missionTaskLayerStyle);
+        }
+    }
     /**
      * This method takes tasks that has been completed in the current mission and *all* the tasks completed in the
      * current neighborhood so far.
@@ -11764,7 +11908,6 @@ function ModalMissionComplete ($, d3, L) {
      * @param completedTasks
      * @private
      */
-
     function _updateNeighborhoodStreetSegmentVisualization(missionTasks, completedTasks) {
         // var completedTasks = svl.taskContainer.getCompletedTasks(regionId);
         // var missionTasks = mission.getRoute();
@@ -11772,30 +11915,38 @@ function ModalMissionComplete ($, d3, L) {
         if (completedTasks && missionTasks) {
             // Add layers http://leafletjs.com/reference.html#map-addlayer
             var i, len, geojsonFeature, featureCollection, layer,
-                completedTaskLayerStyle = { color: "rgb(49,130,189)", opacity: 1, weight: 3 },
-                missionTaskLayerStyle = { color: "rgb(49,189,100)", opacity: 1, weight: 3 };
+                completedTaskLayerStyle = { color: "rgb(49,130,189)", opacity: 1, weight: 3 };
 
-            // Add the completed task layer
-            
+            // remove after animation, otherwise segments remain green from previous tasks
+            d3.select(map.getPanes().overlayPane)
+                .selectAll("svg")
+                .selectAll(".lineConnect")
+                .remove();
+
+            var newStreets = streetEdgeIds(missionTasks);
             len = completedTasks.length;
+            // Add the completed task layer
             for (i = 0; i < len; i++) {
-                geojsonFeature = completedTasks[i].getFeature();
-                layer = L.geoJson(geojsonFeature).addTo(map);
-                layer.setStyle(completedTaskLayerStyle);
+                var streetEdgeId = completedTasks[i].getStreetEdgeId();
+                // do not render streets already in blue
+                if(newStreets.indexOf(streetEdgeId) == -1){
+                    geojsonFeature = completedTasks[i].getFeature();
+                    layer = L.geoJson(geojsonFeature).addTo(map);
+                    layer.setStyle(completedTaskLayerStyle);
+                }
             }
             
             // Add the current mission layer
-            len = missionTasks.length;
-            for (i = 0; i < len; i++) {
-                geojsonFeature = missionTasks[i].getFeature();
-                layer = L.geoJson(geojsonFeature).addTo(map);
-                layer.setStyle(missionTaskLayerStyle);
-            } 
             
-        
+            len = missionTasks.length;
+            // disable zooming while animating, gets re-enabled in animation function
+            map.touchZoom.disable();
+            map.doubleClickZoom.disable();
+            map.scrollWheelZoom.disable();
+            map.boxZoom.disable();
+            _animateMissionTasks(missionTasks, 0, len-1);           
         }
 
-        _animateMissionTasks();
     }
 
     /**
