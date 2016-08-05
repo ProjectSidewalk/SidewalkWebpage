c75228653b72bcaefc06afdfc9e93122661e152f
Kotaro Hara <koe.bluebear@gmail.com>
2016-08-05 15:03:44 -0400

    Fixed MissionContainer errors

--- public/javascripts/SVLabel/spec/SVLabel/mission/MissionContainer-spec.js
+++ public/javascripts/SVLabel/spec/SVLabel/mission/MissionContainer-spec.js
@@ -1,34 +1,32 @@
 describe("Test for the MissionContainer module.", function () {
-    // Jasmine-ajax. http://jasmine.github.io/2.0/ajax.html
-    var svl = svl || {};
-    svl.missionContainer = new MissionContainer($);
-    svl.missionFactory = MissionFactory();
+    var missionContainer = new MissionContainer($);
+    var missionFactory = new MissionFactory();
 
     beforeEach(function () {
-        svl.missionContainer.refresh();
+        missionContainer.refresh();
     });
 
     describe("add method", function(){
         it("should be able to add a new mission to the container", function() {
-            var m1 = svl.missionFactory.create(1, 1, "test1", 1, null, null, false);
-            var m2 = svl.missionFactory.create(1, 2, "test1", 2, null, null, false);
-            svl.missionContainer.add(1, m1);
-            svl.missionContainer.add(1, m2);
-            var missions = svl.missionContainer.getMissionsByRegionId(1);
+            var m1 = missionFactory.create(1, 1, "test1", 1, null, null, false);
+            var m2 = missionFactory.create(1, 2, "test1", 2, null, null, false);
+            missionContainer.add(1, m1);
+            missionContainer.add(1, m2);
+            var missions = missionContainer.getMissionsByRegionId(1);
             expect(missions.length).toBe(2);
 
-            var m3 = svl.missionFactory.create(0, 3, "test1", 1, null, null, false);
-            svl.missionContainer.add(0, m3);
-            missions = svl.missionContainer.getMissinoByRegionId(0);
-            expect(missions.length).toBe(0);
+            var m3 = missionFactory.create(0, 3, "test1", 1, null, null, false);
+            missionContainer.add(0, m3);
+            missions = missionContainer.getMissionsByRegionId(0);
+            expect(missions.length).toBe(1);
         });
 
         it("should check duplicate", function () {
-            var m1 = svl.missionFactory.create(1, 1, "test1", 1, null, null, false);
-            var m2 = svl.missionFactory.create(1, 1, "test1", 1, null, null, false);
-            svl.missionContainer.add(1, m1);
-            svl.missionContainer.add(1, m2);
-            var missions = svl.missionContainer.getMissionsByRegionId(1);
+            var m1 = missionFactory.create(1, 1, "test1", 1, null, null, false);
+            var m2 = missionFactory.create(1, 1, "test1", 1, null, null, false);
+            missionContainer.add(1, m1);
+            missionContainer.add(1, m2);
+            var missions = missionContainer.getMissionsByRegionId(1);
             expect(missions.length).toBe(1);
         });
     });
