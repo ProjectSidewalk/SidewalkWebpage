6bef0d2896b05388fbd273b0667fe8954a68c19b
Kotaro Hara <koe.bluebear@gmail.com>
2016-08-18 15:07:36 -0400

    Status Model spec

---
+++ public/javascripts/SVLabel/spec/SVLabel/modal/ModalSkip-spec.js
@@ -0,0 +1,3 @@
+describe("ModalSkip module", function () {
+
+});
--- public/javascripts/SVLabel/src/SVLabel/status/StatusModel.js
+++ public/javascripts/SVLabel/src/SVLabel/status/StatusModel.js
@@ -5,9 +5,9 @@ function StatusModel () {
 _.extend(StatusModel.prototype, Backbone.Events);
 
 StatusModel.prototype.setMissionCompletionRate = function (completionRate) {
-
+    this.trigger("StatusFieldMissionProgressBar:setCompletionRate", completionRate);
 };
 
 StatusModel.prototype.setProgressBar = function (completionRate) {
-
+    this.trigger("StatusFieldMissionProgressBar:setBar", completionRate);
 };
