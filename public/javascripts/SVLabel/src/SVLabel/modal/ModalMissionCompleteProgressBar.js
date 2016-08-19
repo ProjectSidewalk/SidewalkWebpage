b9babf31457dc04e3d612dc63c6508e672bec473
Dan <daniilzadorozhnyy@gmail.com>
2016-08-19 15:56:20 -0400

    ModalMissionComplete components comments

--- public/javascripts/SVLabel/src/SVLabel/modal/ModalMissionCompleteProgressBar.js
+++ public/javascripts/SVLabel/src/SVLabel/modal/ModalMissionCompleteProgressBar.js
@@ -7,6 +7,7 @@ function ModalMissionCompleteProgressBar (uiModalMissionComplete) {
         .attr("width", svgCoverageBarWidth)
         .attr("height", svgCoverageBarHeight);
 
+    // gray background for the incomplete parts of the bar
     var gBackground = svgCoverageBar.append("g").attr("class", "g-background");
     var horizontalBarBackground = gBackground.selectAll("rect")
         .data([1])
@@ -17,6 +18,7 @@ function ModalMissionCompleteProgressBar (uiModalMissionComplete) {
         .attr("height", svgCoverageBarHeight)
         .attr("width", svgCoverageBarWidth);
 
+    // blue bar segment representing percent of neighborhood complete prior to current audit goal
     var gBarChart = svgCoverageBar.append("g").attr("class", "g-bar-chart");
     var horizontalBarPreviousContribution = gBarChart.selectAll("rect")
         .data([0])
@@ -28,6 +30,7 @@ function ModalMissionCompleteProgressBar (uiModalMissionComplete) {
         .attr("height", svgCoverageBarHeight)
         .attr("width", 0);
 
+    // green bar segment representing percent of neighborhood completed in current audit
     var gBarChart2 = svgCoverageBar.append("g").attr("class", "g-bar-chart");
     var horizontalBarMission = gBarChart2.selectAll("rect")
         .data([0])
@@ -38,6 +41,8 @@ function ModalMissionCompleteProgressBar (uiModalMissionComplete) {
         .attr("fill", "rgba(100,240,110,1)")
         .attr("height", svgCoverageBarHeight)
         .attr("width", 0);
+
+    // text over bar indicating total percentage of neighborhood that has been audited
     var horizontalBarMissionLabel = gBarChart2.selectAll("text")
         .data([""])
         .enter().append("text")
@@ -56,21 +61,26 @@ function ModalMissionCompleteProgressBar (uiModalMissionComplete) {
      * Update the bar graph visualization
      * @param missionDistanceRate
      * @param auditedDistanceRate
-     * @private
      */
     this.update = function (missionDistanceRate, auditedDistanceRate) {
+        // bar starts empty for 0.2 seconds
+        // then fills up with blue up to auditedDistanceRate over 0.8 seconds
         horizontalBarPreviousContribution.attr("width", 0)
             .transition()
             .delay(200)
             .duration(800)
             .attr("width", auditedDistanceRate * svgCoverageBarWidth);
 
+        // green bar starts to animate immediately after the blue bar
+        // fills up to auditedDistanceRate + missionDistanceRate
         horizontalBarMission.attr("width", 0)
             .attr("x", auditedDistanceRate * svgCoverageBarWidth)
             .transition()
             .delay(1000)
             .duration(500)
             .attr("width", missionDistanceRate * svgCoverageBarWidth);
+
+        // text updates to match total audited in neighborhood so far    
         horizontalBarMissionLabel.text(parseInt((auditedDistanceRate + missionDistanceRate) * 100, 10) + "%");
     };
 }
