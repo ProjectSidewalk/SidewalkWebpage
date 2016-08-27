36960178d15f655d0dcfab12a5e921edbbc03240
Kotaro Hara <koe.bluebear@gmail.com>
2016-08-27 15:07:18 -0400

    css update

--- public/javascripts/SVLabel/spec/SVLabel/status/StatusFieldNeighborhood-spec.js
+++ public/javascripts/SVLabel/spec/SVLabel/status/StatusFieldNeighborhood-spec.js
@@ -1,3 +1,10 @@
 describe("StatusFieldNeighborhood module", function () {
     var statusFieldNeighborhood;
+    var uiStatusField;
+    var $uiStatusField;
+
+    beforeEach(function () {
+        $uiStatusField;
+    });
+
 });
