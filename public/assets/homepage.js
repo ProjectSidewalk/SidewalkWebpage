33e36c6be2211a50d13c43c1bcca238311bf2a9e
Anthony Li <antli.inbox@gmail.com>
2017-04-05 21:50:44 -0400

    Lots of styling changes

--- public/assets/homepage.js
+++ public/assets/homepage.js
@@ -2,7 +2,7 @@
  * Created by anthony on 4/1/17.
  */
 function playVideo(){
-    document.getElementById("vidembed").innerHTML = '<div class="video-container"><iframe id="youtubeframe" width="853" height="480" src="https://www.youtube.com/embed/_GBLqZDXB_0?autoplay=1" frameborder="0" allowfullscreen</iframe</div>';
+    document.getElementById("vidembed").innerHTML = '<div class="video-container"><iframe id="youtubeframe" width="853" height="480" src="https://www.youtube.com/embed/bZCBWZ95sIY?autoplay=1" frameborder="0" allowfullscreen</iframe</div>';
     var vidheight = $('#youtubeframe').height();
     $('#vidembed').height(vidheight);
 }
