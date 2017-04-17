7d4ceb32c1510b62d6c2e805e3d3390cc3881395
Hanuma Teja Maddali <hanumatejamaddali@wireless-10-105-216-69.umd.edu>
2017-04-17 17:41:24 -0400

    changed external url

--- test_hit_creation.py
+++ test_hit_creation.py
@@ -15,11 +15,11 @@ mturk = boto3.client('mturk',
 
 print mturk.get_account_balance()['AvailableBalance'] # Sample line of code to get account balance [$10,000.00]
 
-url = 'https://httpbin.org/response-headers?'
+url = 'https://sidewalk.umiacs.umd.edu/audit?' #'https://httpbin.org/response-headers?'
 title = "A test hit"
 description = "Description"
 keywords = "yes,no,test,why"
-frame_height = 500 # the height of the iframe holding the external hit
+frame_height = 1000 # the height of the iframe holding the external hit
 amount = 0.0
  
 # The external question object allows you to view an external url inside an iframe 
@@ -34,7 +34,8 @@ external_question = '<ExternalQuestion xmlns="http://mechanicalturk.amazonaws.co
 mturk.create_hit(
     Title = title,
     LifetimeInSeconds = 3600, 
-    AssignmentDurationInSeconds = 30,
+    AssignmentDurationInSeconds = 3600,
+    MaxAssignments = 10,
     Description = description,
     Keywords = keywords,
     Question = external_question,
