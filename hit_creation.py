cdf39e3fd069c991e2d96e254f3ad3158e38223a
Manaswi Saha <manaswi@cs.umd.edu>
2017-05-15 23:51:35 -0400

    Test mturk HIT params

--- hit_creation.py
+++ hit_creation.py
@@ -147,14 +147,14 @@ if __name__ == '__main__':
         # specific_routes = specific_routes[0: min(number_of_routes, len(specific_routes))]
 
         specific_routes = final_specific_routes
-        for route in specific_routes:
+        for route in specific_routes[0:1]:
             # Create a sample HIT that expires after an 'LifetimeInSeconds'
 
             mturk.create_hit(
                 Title=title,
                 LifetimeInSeconds=86400,
                 AssignmentDurationInSeconds=3600,
-                MaxAssignments=5,
+                MaxAssignments=1,
                 Description=description,
                 Keywords=keywords,
                 Question=external_question,
