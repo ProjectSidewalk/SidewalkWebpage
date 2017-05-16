b433831339e79d321398ea1b0a24b9f1e4e78933
Manaswi Saha <manaswi@cs.umd.edu>
2017-05-16 14:15:09 -0400

    Final commit for the mturk config params

--- hit_creation.py
+++ hit_creation.py
@@ -147,7 +147,6 @@ if __name__ == '__main__':
         # specific_routes = specific_routes[0: min(number_of_routes, len(specific_routes))]
 
         specific_routes = final_specific_routes
-        specific_routes = [220]
         for route in specific_routes:
             # Create a sample HIT that expires after an 'LifetimeInSeconds'
 
