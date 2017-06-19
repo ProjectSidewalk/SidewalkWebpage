db87557d363245ea064e3a113ba7503978203387
misaugstad <michaelssaugstad@gmail.com>
2017-06-19 11:04:11 -0400

    changed chorpleth style to make neighborhood names more prominent

--- public/javascripts/Admin/src/Admin.js
+++ public/javascripts/Admin/src/Admin.js
@@ -36,31 +36,28 @@ function Admin(_, $, c3, turf) {
 
     // Construct a bounding box for these maps that the user cannot move out of
     // https://www.mapbox.com/mapbox.js/example/v1.0.0/maxbounds/
-    var southWest = L.latLng(38.761, -77.262),
-        northEast = L.latLng(39.060, -76.830),
-        bounds = L.latLngBounds(southWest, northEast),
+    var southWest = L.latLng(38.761, -77.262);
+    var northEast = L.latLng(39.060, -76.830);
+    var bounds = L.latLngBounds(southWest, northEast);
 
-        // var tileUrl = "https://a.tiles.mapbox.com/v4/kotarohara.mmoldjeh/page.html?access_token=pk.eyJ1Ijoia290YXJvaGFyYSIsImEiOiJDdmJnOW1FIn0.kJV65G6eNXs4ATjWCtkEmA#13/38.8998/-77.0638";
-        tileUrl = "https:\/\/a.tiles.mapbox.com\/v4\/kotarohara.8e0c6890\/{z}\/{x}\/{y}.png?access_token=pk.eyJ1Ijoia290YXJvaGFyYSIsImEiOiJDdmJnOW1FIn0.kJV65G6eNXs4ATjWCtkEmA",
-        mapboxTiles = L.tileLayer(tileUrl, {
-            attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>'
-        }),
-        map = L.mapbox.map('admin-map', "kotarohara.8e0c6890", {
-            // set that bounding box as maxBounds to restrict moving the map
-            // see full maxBounds documentation:
-            // http://leafletjs.com/reference.html#map-maxbounds
-            maxBounds: bounds,
-            maxZoom: 19,
-            minZoom: 9
-        })
-        // .addLayer(mapboxTiles)
-            .fitBounds(bounds)
-            .setView([38.892, -77.038], 12),
-        // a grayscale tileLayer for the choropleth
-        bwTile = L.tileLayer('http://korona.geog.uni-heidelberg.de/tiles/roadsg/x={x}&y={y}&z={z}', {
-            maxZoom: 19,
-            attribution: 'Imagery from <a href="http://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }),
+    // var tileUrl = "https://a.tiles.mapbox.com/v4/kotarohara.mmoldjeh/page.html?access_token=pk.eyJ1Ijoia290YXJvaGFyYSIsImEiOiJDdmJnOW1FIn0.kJV65G6eNXs4ATjWCtkEmA#13/38.8998/-77.0638";
+    var tileUrl = "https:\/\/a.tiles.mapbox.com\/v4\/kotarohara.8e0c6890\/{z}\/{x}\/{y}.png?access_token=pk.eyJ1Ijoia290YXJvaGFyYSIsImEiOiJDdmJnOW1FIn0.kJV65G6eNXs4ATjWCtkEmA";
+    var mapboxTiles = L.tileLayer(tileUrl, {
+        attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>'
+    });
+    var map = L.mapbox.map('admin-map', "kotarohara.8e0c6890", {
+        // set that bounding box as maxBounds to restrict moving the map
+        // see full maxBounds documentation:
+        // http://leafletjs.com/reference.html#map-maxbounds
+        maxBounds: bounds,
+        maxZoom: 19,
+        minZoom: 9
+    })
+        .fitBounds(bounds)
+        .setView([38.892, -77.038], 12);
+
+    // a grayscale tileLayer for the choropleth
+    L.mapbox.accessToken = 'pk.eyJ1IjoibWlzYXVnc3RhZCIsImEiOiJjajN2dTV2Mm0wMDFsMndvMXJiZWcydDRvIn0.IXE8rQNF--HikYDjccA7Ug';
         choropleth = L.mapbox.map('admin-choropleth', "kotarohara.8e0c6890", {
             // set that bounding box as maxBounds to restrict moving the map
             // see full maxBounds documentation:
@@ -72,10 +69,12 @@ function Admin(_, $, c3, turf) {
                 position: 'bottomleft'
             }
         })
-            .addLayer(bwTile)
             .fitBounds(bounds)
-            .setView([38.892, -77.038], 12),
-        popup = L.popup().setContent('<p>Hello world!<br />This is a nice popup.</p>');
+            .setView([38.892, -77.038], 12);
+    L.mapbox.styleLayer('mapbox://styles/mapbox/light-v9').addTo(choropleth);
+
+    var popup = L.popup().setContent('<p>Hello world!<br />This is a nice popup.</p>');
+
 
     $.getJSON("/contribution/auditCounts/all", function (data) {
         var dates = ['Date'].concat(data[0].map(function (x) {
@@ -107,7 +106,6 @@ function Admin(_, $, c3, turf) {
             }
         });
     });
-
     // Initialize the map
     /**
      * This function adds a semi-transparent white polygon on top of a map
@@ -265,7 +263,7 @@ function Admin(_, $, c3, turf) {
                         weight: 1,
                         opacity: 0.25,
                         fillColor: getColor2(rates[i].rate),
-                        fillOpacity: 0.25 + (0.75 * rates[i].rate / 100)
+                        fillOpacity: 0.25 + (0.5 * rates[i].rate / 100)
                     }
                 }
             }
