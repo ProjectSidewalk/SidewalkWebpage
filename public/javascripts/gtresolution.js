29243f93bb3f2096443bc0f9865f21fe4a9a56ae
Aditya Dash <jetrax12@gmail.com>
2017-07-18 11:04:59 -0400

    Added two panoramas

--- public/javascripts/gtresolution.js
+++ public/javascripts/gtresolution.js
@@ -1,3 +1,25 @@
 $(document).ready(function () {
+	function initializePanoramas(coordinates){
+		var fenway = {lat: 42.345573, lng: -71.098326};
+		var panorama1 = new google.maps.StreetViewPanorama(
+		    document.getElementById('panorama-1'), {
+			    position: fenway,
+		    	pov: {
+		        	heading: 34,
+		        	pitch: 10
+		        },
+		        disableDefaultUI: true
+		    });
+		var panorama2 = new google.maps.StreetViewPanorama(
+		    document.getElementById('panorama-2'), {
+			    position: fenway,
+		    	pov: {
+		        	heading: 34,
+		        	pitch: 10
+		        },
+		        disableDefaultUI: true
+		    });
+	}
 
+	initializePanoramas();
 });
