cd1f21351e8f9243eb756bcfa63da626b3c46eef
misaugstad <michaelssaugstad@gmail.com>
2017-07-28 13:53:54 -0400

    fixed indentation

--- public/resources/sample_labels.js
+++ public/resources/sample_labels.js
@@ -1,291 +1,291 @@
 var sampleLabels = {
-		features: [{
-	geometry: {
-		coordinates: [
-			-77.04338836669922,
-			38.90653991699219
-		],
-		type: "Point"
-	},
-	properties: {
-		audit_task_id: 4306,
-		gsv_panorama_id: "tQCMMo25nchU87fdnJGmmQ",
-		label_id: 20232,
-		heading :90.3125,
-		pitch: -35.0,
-		label_type: "NoCurbRamp",
-		severity: 3
-	},
-	type: "Feature"
-			},
-			{
-				geometry: {
-					coordinates: [
-						-77.04351043701172,
-						38.90653610229492
-					],
-					type: "Point"
-				},
-				properties: {
-					audit_task_id: 4306,
-					gsv_panorama_id: "tQCMMo25nchU87fdnJGmmQ",
-					label_id: 20233,
-					heading: 242.9375,
-					pitch:-29.9375,
-					label_type: "NoCurbRamp",
-					severity: 3
-				},
-				type: "Feature"
-			},
-			{
-				geometry: {
-					coordinates: [
-						-77.04351043701172,
-						38.906402587890625
-					],
-					type: "Point"
-				},
-				properties: {
-					audit_task_id: 4306,
-					gsv_panorama_id: "LyhDWKVOM1keWutriZGFqA",
-					label_id: 20239,
-					heading:255.875,
-					pitch:-33.5,
-					label_type: "NoCurbRamp",
-					severity: 3
-				},
-				type: "Feature"
-			},
-			{
-				geometry: {
-					coordinates: [
-						-77.04337310791016,
-						38.90642547607422
-					],
-					type: "Point"
-				},
-				properties: {
-					audit_task_id: 4306,
-					gsv_panorama_id: "GkI7OXvuFkXt53ys0BWD6g",
-					label_id: 20234,
-					heading:122.1875,
-					pitch:-33.125,
-					label_type: "CurbRamp",
-					severity: 1
-				},
-				type: "Feature"
-			},
-			{
-				geometry: {
-					coordinates: [
-						-77.04337310791016,
-						38.906410217285156
-					],
-					type: "Point"
-				},
-				properties: {
-					audit_task_id: 4306,
-					gsv_panorama_id: "bT-OJVyH3HsmoUh700xUpw",
-					label_id: 20228,
-					heading:219.875,
-					pitch:-28.625,
-					label_type: "CurbRamp",
-					severity: 1
-				},
-				type: "Feature"
-			},
-			{
-				type: "Feature",
-				properties: {
-					audit_task_id:1693,
-					gsv_panorama_id:"rb3UJINCRDFJwbPzKhOFyg",
-					label_id:7823,
-					heading:325.8125,
-					pitch:-29.1875,
-					label_type:"CurbRamp",
-					severity:1
-				},
-				geometry: {
-					coordinates: [-77.03860473632812, 38.912681579589844],
-					type: "Point"
-				}
-			},
-			{
-				type: "Feature",
-				properties: {
-					audit_task_id:4168,
-					gsv_panorama_id:"vxMmytN--JycF7nGKPUBvQ",
-					label_id:19536,
-					heading:314.5,
-					pitch:-6.0625,
-					label_type:"CurbRamp",
-					severity:1
-				},
-				geometry: {
-					coordinates: [-77.03857421875,38.912689208984375],
-					type: "Point"
-				}
-			},
-			{
-				type: "Feature",
-				properties: {
-					audit_task_id:4168,
-					gsv_panorama_id:"ncHz5AFhGssO2GB6QIsosA",
-					label_id:7805,
-					heading:245.5625,
-					pitch:-7.1875,
-					label_type:"CurbRamp",
-					severity:1
-				},
-				geometry: {
-					coordinates: [-77.0385513305664,38.91270065307617],
-					type: "Point"
-				}
-			},
-			{
-				type: "Feature",
-				properties: {
-					audit_task_id:1693,
-					gsv_panorama_id:"rb3UJINCRDFJwbPzKhOFyg",
-					label_id:7822,
-					heading:238.0625,
-					pitch:-20.0,
-					label_type:"CurbRamp",
-					severity:1
-				},
-				geometry: {
-					coordinates: [-77.0385971069336,38.91253662109375],
-					type: "Point"
-				}
-			},
-			{
-				type: "Feature",
-				properties: {
-					audit_task_id:4168,
-					gsv_panorama_id:"vxMmytN--JycF7nGKPUBvQ",
-					label_id:19535,
-					heading:259.9375,
-					pitch:-9.625,
-					label_type:"CurbRamp",
-					severity:1
-				},
-				geometry: {
-					coordinates: [-77.03857421875,38.91255187988281],
-					type: "Point"
-				}
-			},
-			{
-				type: "Feature",
-				properties: {
-					audit_task_id:1693,
-					gsv_panorama_id:"7BXtxjBQQeQESx0OIQvcRg",
-					label_id:7828,
-					heading:244.4375,
-					pitch:-23.9375,
-					label_type:"CurbRamp",
-					severity:1
-				},
-				geometry: {
-					coordinates: [-77.03854370117188,38.91252136230469],
-					type: "Point"
-				}
-			},
-			{
-				type: "Feature",
-				properties: {
-					audit_task_id:1693,
-					gsv_panorama_id:"vxMmytN--JycF7nGKPUBvQ",
-					label_id:7827,
-					heading:143.375,
-					pitch:-25.4375,
-					label_type:"CurbRamp",
-					severity:1
-				},
-				geometry: {
-					coordinates: [-77.03838348388672,38.91253662109375],
-					type: "Point"
-				}
-			},
-			{
-				type: "Feature",
-				properties: {
-					audit_task_id:1693,
-					gsv_panorama_id:"vxMmytN--JycF7nGKPUBvQ",
-					label_id:7826,
-					heading:88.4375,
-					pitch:-25.25,
-					label_type:"CurbRamp",
-					severity:1
-				},
-				geometry: {
-					coordinates: [-77.03833770751953,38.91256332397461],
-					type: "Point"
-				}
-			},
-			{
-				type: "Feature",
-				properties: {
-					audit_task_id:1693,
-					gsv_panorama_id:"rb3UJINCRDFJwbPzKhOFyg",
-					label_id:7825,
-					heading:38.75,
-					pitch:-26.9375,
-					label_type:"CurbRamp",
-					severity:1
-				},
-				geometry: {
-					coordinates: [-77.03840637207031,38.91268539428711],
-					type: "Point"
-				}
-			},
-			{
-				type: "Feature",
-				properties: {
-					audit_task_id:4168,
-					gsv_panorama_id:"vxMmytN--JycF7nGKPUBvQ",
-					label_id:19538,
-					heading:21.4375,
-					pitch:-7.1875,
-					label_type:"CurbRamp",
-					severity:3
-				},
-				geometry: {
-					coordinates: [-77.03837585449219,38.91269302368164],
-					type: "Point"
-				}
-			},
-			{
-				type: "Feature",
-				properties: {
-					audit_task_id:4168,
-					gsv_panorama_id:"IhGTPdLdCI0xjErLEucW6g",
-					label_id:19541,
-					heading:3.25,
-					pitch:-23.0625,
-					label_type:"Obstacle",
-					severity:1
-				},
-				geometry: {
-					coordinates: [-77.0383529663086,38.91265106201172],
-					type: "Point"
-				}
-			},
-			{
-				type: "Feature",
-				properties: {
-					audit_task_id:4168,
-					gsv_panorama_id:"vxMmytN--JycF7nGKPUBvQ",
-					label_id:19539,
-					heading:62.125,
-					pitch:-0.375,
-					label_type:"CurbRamp",
-					severity:2
-				},
-				geometry: {
-					coordinates: [-77.03833770751953,38.91267776489258],
-					type: "Point"
-				}
-			}
-		],
-		type: "FeatureCollection"
+    features: [{
+        geometry: {
+            coordinates: [
+                -77.04338836669922,
+                38.90653991699219
+            ],
+            type: "Point"
+        },
+        properties: {
+            audit_task_id: 4306,
+            gsv_panorama_id: "tQCMMo25nchU87fdnJGmmQ",
+            label_id: 20232,
+            heading: 90.3125,
+            pitch: -35.0,
+            label_type: "NoCurbRamp",
+            severity: 3
+        },
+        type: "Feature"
+    },
+        {
+            geometry: {
+                coordinates: [
+                    -77.04351043701172,
+                    38.90653610229492
+                ],
+                type: "Point"
+            },
+            properties: {
+                audit_task_id: 4306,
+                gsv_panorama_id: "tQCMMo25nchU87fdnJGmmQ",
+                label_id: 20233,
+                heading: 242.9375,
+                pitch: -29.9375,
+                label_type: "NoCurbRamp",
+                severity: 3
+            },
+            type: "Feature"
+        },
+        {
+            geometry: {
+                coordinates: [
+                    -77.04351043701172,
+                    38.906402587890625
+                ],
+                type: "Point"
+            },
+            properties: {
+                audit_task_id: 4306,
+                gsv_panorama_id: "LyhDWKVOM1keWutriZGFqA",
+                label_id: 20239,
+                heading: 255.875,
+                pitch: -33.5,
+                label_type: "NoCurbRamp",
+                severity: 3
+            },
+            type: "Feature"
+        },
+        {
+            geometry: {
+                coordinates: [
+                    -77.04337310791016,
+                    38.90642547607422
+                ],
+                type: "Point"
+            },
+            properties: {
+                audit_task_id: 4306,
+                gsv_panorama_id: "GkI7OXvuFkXt53ys0BWD6g",
+                label_id: 20234,
+                heading: 122.1875,
+                pitch: -33.125,
+                label_type: "CurbRamp",
+                severity: 1
+            },
+            type: "Feature"
+        },
+        {
+            geometry: {
+                coordinates: [
+                    -77.04337310791016,
+                    38.906410217285156
+                ],
+                type: "Point"
+            },
+            properties: {
+                audit_task_id: 4306,
+                gsv_panorama_id: "bT-OJVyH3HsmoUh700xUpw",
+                label_id: 20228,
+                heading: 219.875,
+                pitch: -28.625,
+                label_type: "CurbRamp",
+                severity: 1
+            },
+            type: "Feature"
+        },
+        {
+            type: "Feature",
+            properties: {
+                audit_task_id: 1693,
+                gsv_panorama_id: "rb3UJINCRDFJwbPzKhOFyg",
+                label_id: 7823,
+                heading: 325.8125,
+                pitch: -29.1875,
+                label_type: "CurbRamp",
+                severity: 1
+            },
+            geometry: {
+                coordinates: [-77.03860473632812, 38.912681579589844],
+                type: "Point"
+            }
+        },
+        {
+            type: "Feature",
+            properties: {
+                audit_task_id: 4168,
+                gsv_panorama_id: "vxMmytN--JycF7nGKPUBvQ",
+                label_id: 19536,
+                heading: 314.5,
+                pitch: -6.0625,
+                label_type: "CurbRamp",
+                severity: 1
+            },
+            geometry: {
+                coordinates: [-77.03857421875, 38.912689208984375],
+                type: "Point"
+            }
+        },
+        {
+            type: "Feature",
+            properties: {
+                audit_task_id: 4168,
+                gsv_panorama_id: "ncHz5AFhGssO2GB6QIsosA",
+                label_id: 7805,
+                heading: 245.5625,
+                pitch: -7.1875,
+                label_type: "CurbRamp",
+                severity: 1
+            },
+            geometry: {
+                coordinates: [-77.0385513305664, 38.91270065307617],
+                type: "Point"
+            }
+        },
+        {
+            type: "Feature",
+            properties: {
+                audit_task_id: 1693,
+                gsv_panorama_id: "rb3UJINCRDFJwbPzKhOFyg",
+                label_id: 7822,
+                heading: 238.0625,
+                pitch: -20.0,
+                label_type: "CurbRamp",
+                severity: 1
+            },
+            geometry: {
+                coordinates: [-77.0385971069336, 38.91253662109375],
+                type: "Point"
+            }
+        },
+        {
+            type: "Feature",
+            properties: {
+                audit_task_id: 4168,
+                gsv_panorama_id: "vxMmytN--JycF7nGKPUBvQ",
+                label_id: 19535,
+                heading: 259.9375,
+                pitch: -9.625,
+                label_type: "CurbRamp",
+                severity: 1
+            },
+            geometry: {
+                coordinates: [-77.03857421875, 38.91255187988281],
+                type: "Point"
+            }
+        },
+        {
+            type: "Feature",
+            properties: {
+                audit_task_id: 1693,
+                gsv_panorama_id: "7BXtxjBQQeQESx0OIQvcRg",
+                label_id: 7828,
+                heading: 244.4375,
+                pitch: -23.9375,
+                label_type: "CurbRamp",
+                severity: 1
+            },
+            geometry: {
+                coordinates: [-77.03854370117188, 38.91252136230469],
+                type: "Point"
+            }
+        },
+        {
+            type: "Feature",
+            properties: {
+                audit_task_id: 1693,
+                gsv_panorama_id: "vxMmytN--JycF7nGKPUBvQ",
+                label_id: 7827,
+                heading: 143.375,
+                pitch: -25.4375,
+                label_type: "CurbRamp",
+                severity: 1
+            },
+            geometry: {
+                coordinates: [-77.03838348388672, 38.91253662109375],
+                type: "Point"
+            }
+        },
+        {
+            type: "Feature",
+            properties: {
+                audit_task_id: 1693,
+                gsv_panorama_id: "vxMmytN--JycF7nGKPUBvQ",
+                label_id: 7826,
+                heading: 88.4375,
+                pitch: -25.25,
+                label_type: "CurbRamp",
+                severity: 1
+            },
+            geometry: {
+                coordinates: [-77.03833770751953, 38.91256332397461],
+                type: "Point"
+            }
+        },
+        {
+            type: "Feature",
+            properties: {
+                audit_task_id: 1693,
+                gsv_panorama_id: "rb3UJINCRDFJwbPzKhOFyg",
+                label_id: 7825,
+                heading: 38.75,
+                pitch: -26.9375,
+                label_type: "CurbRamp",
+                severity: 1
+            },
+            geometry: {
+                coordinates: [-77.03840637207031, 38.91268539428711],
+                type: "Point"
+            }
+        },
+        {
+            type: "Feature",
+            properties: {
+                audit_task_id: 4168,
+                gsv_panorama_id: "vxMmytN--JycF7nGKPUBvQ",
+                label_id: 19538,
+                heading: 21.4375,
+                pitch: -7.1875,
+                label_type: "CurbRamp",
+                severity: 3
+            },
+            geometry: {
+                coordinates: [-77.03837585449219, 38.91269302368164],
+                type: "Point"
+            }
+        },
+        {
+            type: "Feature",
+            properties: {
+                audit_task_id: 4168,
+                gsv_panorama_id: "IhGTPdLdCI0xjErLEucW6g",
+                label_id: 19541,
+                heading: 3.25,
+                pitch: -23.0625,
+                label_type: "Obstacle",
+                severity: 1
+            },
+            geometry: {
+                coordinates: [-77.0383529663086, 38.91265106201172],
+                type: "Point"
+            }
+        },
+        {
+            type: "Feature",
+            properties: {
+                audit_task_id: 4168,
+                gsv_panorama_id: "vxMmytN--JycF7nGKPUBvQ",
+                label_id: 19539,
+                heading: 62.125,
+                pitch: -0.375,
+                label_type: "CurbRamp",
+                severity: 2
+            },
+            geometry: {
+                coordinates: [-77.03833770751953, 38.91267776489258],
+                type: "Point"
+            }
+        }
+    ],
+    type: "FeatureCollection"
 }
