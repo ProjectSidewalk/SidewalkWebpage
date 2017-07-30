0eeb1278cbc375edef8fca44990799aa743f9122
misaugstad <michaelssaugstad@gmail.com>
2017-07-30 12:14:20 -0400

    added endpoing for clustering

---
+++ app/controllers/ClusteringController.scala
@@ -0,0 +1,32 @@
+package controllers
+
+import javax.inject.Inject
+
+import com.mohiva.play.silhouette.api.{Environment, Silhouette}
+import com.mohiva.play.silhouette.impl.authenticators.SessionAuthenticator
+import controllers.headers.ProvidesHeader
+import models.user.User
+
+import scala.concurrent.Future
+
+
+class ClusteringController @Inject() (implicit val env: Environment[User, SessionAuthenticator])
+  extends Silhouette[User, SessionAuthenticator] with ProvidesHeader {
+
+  // Helper methods
+  def isAdmin(user: Option[User]): Boolean = user match {
+    case Some(user) =>
+      if (user.roles.getOrElse(Seq()).contains("Administrator")) true else false
+    case _ => false
+  }
+
+  // Pages
+  def index = UserAwareAction.async { implicit request =>
+    if (isAdmin(request.identity)) {
+      Future.successful(Ok(views.html.clustering("Project Sidewalk", request.identity)))
+    } else {
+      Future.successful(Redirect("/"))
+    }
+  }
+
+}
