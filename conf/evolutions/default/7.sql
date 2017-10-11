3d80c561abf032b2b09999851c8ca1531c8d0ea3
misaugstad <michaelssaugstad@gmail.com>
2017-10-11 18:19:31 -0400

    changed gsv_onboarding_panos to gsv_onboarding_pano to fit naming conventions

--- conf/evolutions/default/7.sql
+++ conf/evolutions/default/7.sql
@@ -1,14 +1,16 @@
 
 # --- !Ups
-CREATE TABLE gsv_onboarding_panos
+CREATE TABLE gsv_onboarding_pano
 (
   gsv_panorama_id TEXT NOT NULL,
   has_labels BOOLEAN NOT NULL,
   PRIMARY KEY (gsv_panorama_id)
 );
 
-INSERT INTO gsv_onboarding_panos VALUES ('stxXyCKAbd73DmkM2vsIHA', TRUE);
-INSERT INTO gsv_onboarding_panos VALUES ('PTHUzZqpLdS1nTixJMoDSw', FALSE);
+INSERT INTO gsv_onboarding_pano VALUES ('stxXyCKAbd73DmkM2vsIHA', TRUE);
+INSERT INTO gsv_onboarding_pano VALUES ('PTHUzZqpLdS1nTixJMoDSw', FALSE);
+INSERT INTO gsv_onboarding_pano VALUES ('bdmGHJkiSgmO7_80SnbzXw', TRUE);
+INSERT INTO gsv_onboarding_pano VALUES ('OgLbmLAuC4urfE5o7GP_JQ', TRUE);
 
 # --- !Downs
-DROP TABLE gsv_onboarding_panos;
+DROP TABLE gsv_onboarding_pano;
