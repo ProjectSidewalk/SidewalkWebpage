09754ac19c1b9c1804956022f487595b229abbb7
maddalihanumateja <hanumatejamaddali@gmail.com>
2017-11-12 13:39:25 -0500

    added a new table to store street edge priority for assignment during routing

---
+++ conf/evolutions/default/11.sql
@@ -0,0 +1,11 @@
+# --- !Ups
+CREATE TABLE street_edge_priority
+(
+ street_edge_priority_id SERIAL NOT NULL,
+ street_edge_id INT NOT NULL,
+ priority DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+ PRIMARY KEY (street_edge_priority_id),
+ FOREIGN KEY (street_edge_id) REFERENCES street_edge(street_edge_id)
+);
+# --- !Downs
+DROP TABLE street_edge_priority;
