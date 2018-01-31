a02e05882db90e7d536fa55417d277abe1eea298
misaugstad <michaelssaugstad@gmail.com>
2018-01-31 10:23:15 -0500

    updated completion count function, plus stylistic changes

--- conf/evolutions/default/12.sql
+++ conf/evolutions/default/12.sql
@@ -1,12 +1,12 @@
 # --- !Ups
 CREATE TABLE street_edge_priority
 (
- street_edge_priority_id SERIAL NOT NULL,
- region_id INT NOT NULL,
- street_edge_id INT NOT NULL,
- priority DOUBLE PRECISION NOT NULL DEFAULT 0.0,
- PRIMARY KEY (street_edge_priority_id),
- FOREIGN KEY (street_edge_id) REFERENCES street_edge(street_edge_id)
+  street_edge_priority_id SERIAL NOT NULL,
+  region_id INT NOT NULL,
+  street_edge_id INT NOT NULL,
+  priority DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+  PRIMARY KEY (street_edge_priority_id),
+  FOREIGN KEY (street_edge_id) REFERENCES street_edge(street_edge_id)
 );
 
 INSERT INTO street_edge_priority (street_edge_id, region_id)
