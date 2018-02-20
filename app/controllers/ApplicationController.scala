074130c7e3d8146277a20893daab288d64131c2e
pujaram <puja.ramanathan@gmail.com>
2018-02-19 18:02:20 -0800

    Deleted graveyard page

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -305,21 +305,6 @@ class ApplicationController @Inject() (implicit val env: Environment[User, Sessi
     }
   }
 
-  def graveyard = UserAwareAction.async { implicit request =>
-    val now = new DateTime(DateTimeZone.UTC)
-    val timestamp: Timestamp = new Timestamp(now.getMillis)
-    val ipAddress: String = request.remoteAddress
-
-    request.identity match {
-      case Some(user) =>
-        WebpageActivityTable.save(WebpageActivity(0, user.userId.toString, ipAddress, "Visit_Graveyard", timestamp))
-        Future.successful(Ok(views.html.graveyard("Project Sidewalk = About", Some(user))))
-      case None =>
-        WebpageActivityTable.save(WebpageActivity(0, anonymousUser.userId.toString, ipAddress, "Visit_Graveyard", timestamp))
-        Future.successful(Ok(views.html.graveyard("Project Sidewalk - About")))
-    }
-  }
-
   /**
     * Returns the terms page
     * @return
--- public/assets/pagetop-padding.js
+++ public/assets/pagetop-padding.js
@@ -2,8 +2,7 @@ function checkIfPaddingNeeded(){
     if(window.location.pathname === "/faq" || window.location.pathname === "/admin" ||
         window.location.pathname === "/labelingGuide" || window.location.pathname === "/curbRamps" ||
         window.location.pathname === "/surfaceProblems" || window.location.pathname === "/obstacles" ||
-        window.location.pathname === "/noSidewalk" || window.location.pathname === "/occlusion" ||
-        window.location.pathname === "/graveyard"){
+        window.location.pathname === "/noSidewalk" || window.location.pathname === "/occlusion"){
 
         document.body.style.paddingTop = "60px";
     }
