aca38d6f1765bd938d84478debbb0923a03d861c
pujaram <puja.ramanathan@gmail.com>
2018-03-05 22:57:48 -0800

    Added collapsible subtopics on sidebar

--- public/javascripts/sidebarSubtopics.js
+++ public/javascripts/sidebarSubtopics.js
@@ -2,12 +2,42 @@ $(document).ready(function(){
     /*
     Any h2 tag with a "question" class will be added to questions list
      */
+
     $("h2.question").each(function(){
         listItem = "<li><a href='#"+$(this).attr('id')+"'>"+$(this).text()+"</a></li>";
+        this.classList.add("subtopic");
         $("#subtopics").append(listItem);
     });
 
     $('.img-responsive').magnificPopup({
-        type:'image',
+        type:'image'
     });
+
+    /*$(".topic").click(function(){
+        if ($('.subtopic').is(':visible')) {
+            $(".subtopic").slideUp(300);
+            $(".plusminus").text('+');
+        }
+        if ($(this).next(".subtopic").is(':visible')){
+            $(this).next(".subtopic").slideUp(300);
+            $(this).children(".plusminus").text('+');
+        } else {
+            $(this).next(".subtopic").slideDown(300);
+            $(this).children(".plusminus").text('-');
+        }
+    });*/
+    
+    $(".topic").click(function() {
+        this.classList.toggle("active");
+
+        var panel = document.getElementById("subtopics");
+        if (panel.style.maxHeight){
+            panel.style.maxHeight = null;
+            $(".plusminus").text('+');
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+            $(".plusminus").text('-');
+        }
+    });
+
 });
