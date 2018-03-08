474067c3371caf4ba8e26514950038a64f33ffe4
Manaswi Saha <manaswi@cs.washington.edu>
2018-03-08 14:40:06 -0800

    Removed unnecesary semicolon

--- app/formats/json/SurveySubmissionFormats.scala
+++ app/formats/json/SurveySubmissionFormats.scala
@@ -7,7 +7,7 @@ import play.api.libs.functional.syntax._
 
 
 object SurveySubmissionFormats {
-  case class SurveySingleSubmission(surveyQuestionId: String, answerText: String);
+  case class SurveySingleSubmission(surveyQuestionId: String, answerText: String)
 
   implicit val surveySingleSubmissionReads: Reads[SurveySingleSubmission] = (
     (JsPath \ "name").read[String] and
