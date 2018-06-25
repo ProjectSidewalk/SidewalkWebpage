8452c7bbad108dc9d77ee31e0185bfa45a534db9
misaugstad <michaelssaugstad@gmail.com>
2018-06-25 11:07:35 -0500

    improved downs for adding Problem label type Play evolution

--- conf/evolutions/default/14.sql
+++ conf/evolutions/default/14.sql
@@ -4,4 +4,15 @@ INSERT INTO label_type VALUES (8, 'Problem', 'Composite type: represents cluster
 
 
 # --- !Downs
+DELETE FROM global_attribute_user_attribute
+WHERE global_attribute_id IN ( SELECT global_attribute_id FROM global_attribute WHERE label_type_id = 8 )
+    OR user_attribute_id IN ( SELECT user_attribute_id FROM user_attribute WHERE label_type_id = 8 );
+
+DELETE FROM global_attribute WHERE label_type_id = 8;
+
+DELETE FROM user_attribute_label
+WHERE user_attribute_id IN ( SELECT user_attribute_id FROM user_attribute WHERE label_type_id = 8 );
+
+DELETE FROM user_attribute WHERE label_type_id = 8;
+
 DELETE FROM label_type WHERE label_type.label_type = 'Problem';
