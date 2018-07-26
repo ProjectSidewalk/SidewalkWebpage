58daa32867dc424c2809a72bfdbe5b90e7c42f34
ShivenBhatt <ShivenBhatt@outlook.com>
2018-07-26 13:39:06 -0700

    Update LabelTable.scala
    working towards changes pointed out by mikey

--- app/models/label/LabelTable.scala
+++ app/models/label/LabelTable.scala
@@ -266,13 +266,12 @@ object LabelTable {
       """SELECT lb1.label_id, lb1.gsv_panorama_id, lp.heading, lp.pitch, lp.zoom, lp.canvas_x, lp.canvas_y,
         |       lp.canvas_width, lp.canvas_height, lb1.audit_task_id, u.user_id, u.username, lb1.time_created,
         |       lb_big.label_type, lb_big.label_type_desc, lb_big.severity, lb_big.temp_problem, lb_big.description
-        |
         |	FROM sidewalk.label as lb1, sidewalk.audit_task as at,
         |       sidewalk.user as u, sidewalk.label_point as lp,
         |				(SELECT lb.label_id, lb.gsv_panorama_id, lbt.label_type, lbt.description as label_type_desc, sev.severity,
         |               COALESCE(prob_temp.temporary_problem,'FALSE') as temp_problem,
         |               prob_desc.description
-        |	      				FROM label as lb
+        |                                      FROM label as lb
         |				LEFT JOIN sidewalk.label_type as lbt
         |					ON lb.label_type_id = lbt.label_type_id
         |				LEFT JOIN sidewalk.problem_severity as sev
