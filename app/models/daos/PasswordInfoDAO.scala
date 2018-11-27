6756a47f4aaaec3b7039c40ab2f1cd123cb04cc7
misaugstad <michaelssaugstad@gmail.com>
2018-11-26 17:32:20 -0800

    updated password DAOs for play-silhouette v3.0.5

--- app/models/daos/PasswordInfoDAO.scala
+++ app/models/daos/PasswordInfoDAO.scala
@@ -7,19 +7,62 @@ import models.daos.PasswordInfoDAO._
 
 import scala.collection.mutable
 import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
+// Most of this class was copied directly from this example code:
+// https://github.com/mohiva/play-silhouette-seed/blob/049f5948ff18f400efee8221f323ea978feafdb5/app/models/daos/PasswordInfoDAO.scala
 class PasswordInfoDAO extends DelegableAuthInfoDAO[PasswordInfo] {
 
   /**
-   * Saves the password info.
-   *
-   * @param loginInfo The login info for which the auth info should be saved.
-   * @param authInfo The password info to save.
-   * @return The saved password info.
-   */
-  def save(loginInfo: LoginInfo, authInfo: PasswordInfo): Future[PasswordInfo] = {
+    * Adds new auth info for the given login info.
+    *
+    * @param loginInfo The login info for which the auth info should be added.
+    * @param authInfo The auth info to add.
+    * @return The added auth info.
+    */
+  def add(loginInfo: LoginInfo, authInfo: PasswordInfo): Future[PasswordInfo] = {
     data += (loginInfo -> authInfo)
     Future.successful(authInfo)
+  }
+
+  /**
+    * Updates the auth info for the given login info.
+    *
+    * @param loginInfo The login info for which the auth info should be updated.
+    * @param authInfo The auth info to update.
+    * @return The updated auth info.
+    */
+  def update(loginInfo: LoginInfo, authInfo: PasswordInfo): Future[PasswordInfo] = {
+    data += (loginInfo -> authInfo)
+    Future.successful(authInfo)
+  }
+
+  /**
+    * Saves the auth info for the given login info.
+    *
+    * This method either adds the auth info if it doesn't exists or it updates the auth info
+    * if it already exists.
+    *
+    * @param loginInfo The login info for which the auth info should be saved.
+    * @param authInfo The auth info to save.
+    * @return The saved auth info.
+    */
+  def save(loginInfo: LoginInfo, authInfo: PasswordInfo): Future[PasswordInfo] = {
+    find(loginInfo).flatMap {
+      case Some(_) => update(loginInfo, authInfo)
+      case None => add(loginInfo, authInfo)
+    }
+  }
+
+  /**
+    * Removes the auth info for the given login info.
+    *
+    * @param loginInfo The login info for which the auth info should be removed.
+    * @return A future to wait for the process to be completed.
+    */
+  def remove(loginInfo: LoginInfo): Future[Unit] = {
+    data -= loginInfo
+    Future.successful(())
   }
 
   /**
--- app/models/daos/slickdaos/PasswordDAOSlick.scala
+++ app/models/daos/slickdaos/PasswordDAOSlick.scala
@@ -15,55 +15,97 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
  * The DAO to store the password information.
+  *
+  * Most of this class was copied directly from this example code:
+  * https://github.com/sbrunk/play-silhouette-slick-seed/blob/master/app/models/daos/PasswordInfoDAO.scala
  */
 class PasswordInfoDAOSlick extends DelegableAuthInfoDAO[PasswordInfo] {
   val dbConfig = DatabaseConfigProvider.get[JdbcProfile](Play.current)
   val db = dbConfig.db
 
-  /**
-   * Saves the password info.
-   *
-   * @param loginInfo The login info for which the auth info should be saved.
-   * @param authInfo The password info to save.
-   * @return The saved password info or None if the password info couldn't be saved.
-   */
-  def save(loginInfo: LoginInfo, authInfo: PasswordInfo): Future[PasswordInfo] = {
-    db.run(
-      slickLoginInfos.filter(
-        x => x.providerID === loginInfo.providerID && x.providerKey === loginInfo.providerKey
-      ).result.head
-    ).flatMap { info =>
-      db.run(
-        slickPasswordInfos += DBPasswordInfo(authInfo.hasher, authInfo.password, authInfo.salt, info.id.get)
-      ).map(_ => authInfo)
-    }
-  }
+  protected def loginInfoQuery(loginInfo: LoginInfo) =
+    slickLoginInfos.filter(x => x.providerID === loginInfo.providerID && x.providerKey === loginInfo.providerKey)
+
+  protected def passwordInfoQuery(loginInfo: LoginInfo) = for {
+    dbLoginInfo <- loginInfoQuery(loginInfo)
+    dbPasswordInfo <- slickPasswordInfos if dbPasswordInfo.loginInfoId === dbLoginInfo.loginInfoId
+  } yield dbPasswordInfo
+
+  // Use subquery workaround instead of join to get authinfo because slick only supports selecting
+  // from a single table for update/delete queries (https://github.com/slick/slick/issues/684).
+  protected def passwordInfoSubQuery(loginInfo: LoginInfo) =
+    slickPasswordInfos.filter(_.loginInfoId in loginInfoQuery(loginInfo).map(_.loginInfoId))
+
+  protected def addAction(loginInfo: LoginInfo, authInfo: PasswordInfo) =
+    loginInfoQuery(loginInfo).result.head.flatMap { dbLoginInfo =>
+      slickPasswordInfos +=
+        DBPasswordInfo(authInfo.hasher, authInfo.password, authInfo.salt, dbLoginInfo.id.get)
+    }.transactionally
+
+  protected def updateAction(loginInfo: LoginInfo, authInfo: PasswordInfo) =
+    passwordInfoSubQuery(loginInfo).
+      map(dbPasswordInfo => (dbPasswordInfo.hasher, dbPasswordInfo.password, dbPasswordInfo.salt)).
+      update((authInfo.hasher, authInfo.password, authInfo.salt))
 
   /**
-   * Finds the password info which is linked with the specified login info.
-   *
-   * @param loginInfo The linked login info.
-   * @return The retrieved password info or None if no password info could be retrieved for the given login info.
-   */
+    * Finds the auth info which is linked with the specified login info.
+    *
+    * @param loginInfo The linked login info.
+    * @return The retrieved auth info or None if no auth info could be retrieved for the given login info.
+    */
   def find(loginInfo: LoginInfo): Future[Option[PasswordInfo]] = {
-    db.run(
-      slickLoginInfos.filter(
-        info => info.providerID === loginInfo.providerID && info.providerKey === loginInfo.providerKey
-      ).result.headOption
-    ).flatMap {
-      case Some(info) => db.run(
-          slickPasswordInfos.filter(_.loginInfoId === info.id).result.head
-        ).map(p => Some(PasswordInfo(p.hasher, p.password, p.salt)))
-      case None => Future.successful(None)
+    db.run(passwordInfoQuery(loginInfo).result.headOption).map { dbPasswordInfoOption =>
+      dbPasswordInfoOption.map(dbPasswordInfo =>
+        PasswordInfo(dbPasswordInfo.hasher, dbPasswordInfo.password, dbPasswordInfo.salt))
     }
   }
 
-  //FIXME
-  override def add(loginInfo: LoginInfo, authInfo: PasswordInfo): Future[PasswordInfo] = ???
+  /**
+    * Adds new auth info for the given login info.
+    *
+    * @param loginInfo The login info for which the auth info should be added.
+    * @param authInfo The auth info to add.
+    * @return The added auth info.
+    */
+  def add(loginInfo: LoginInfo, authInfo: PasswordInfo): Future[PasswordInfo] = {
+    db.run(addAction(loginInfo, authInfo)).map(_ => authInfo)
+  }
 
-  //FIXME
-  override def update(loginInfo: LoginInfo, authInfo: PasswordInfo): Future[PasswordInfo] = ???
+  /**
+    * Updates the auth info for the given login info.
+    *
+    * @param loginInfo The login info for which the auth info should be updated.
+    * @param authInfo The auth info to update.
+    * @return The updated auth info.
+    */
+  def update(loginInfo: LoginInfo, authInfo: PasswordInfo): Future[PasswordInfo] =
+    db.run(updateAction(loginInfo, authInfo)).map(_ => authInfo)
 
-  //FIXME
-  override def remove(loginInfo: LoginInfo): Future[Unit] = ???
+  /**
+    * Saves the auth info for the given login info.
+    *
+    * This method either adds the auth info if it doesn't exists or it updates the auth info
+    * if it already exists.
+    *
+    * @param loginInfo The login info for which the auth info should be saved.
+    * @param authInfo The auth info to save.
+    * @return The saved auth info.
+    */
+  def save(loginInfo: LoginInfo, authInfo: PasswordInfo): Future[PasswordInfo] = {
+    val query = loginInfoQuery(loginInfo).joinLeft(slickPasswordInfos).on(_.loginInfoId === _.loginInfoId)
+    val action = query.result.head.flatMap {
+      case (dbLoginInfo, Some(dbPasswordInfo)) => updateAction(loginInfo, authInfo)
+      case (dbLoginInfo, None) => addAction(loginInfo, authInfo)
+    }
+    db.run(action).map(_ => authInfo)
+  }
+
+  /**
+    * Removes the auth info for the given login info.
+    *
+    * @param loginInfo The login info for which the auth info should be removed.
+    * @return A future to wait for the process to be completed.
+    */
+  def remove(loginInfo: LoginInfo): Future[Unit] =
+    db.run(passwordInfoSubQuery(loginInfo).delete).map(_ => ())
 }
