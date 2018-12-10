d514045d92aecd92a826a89707a1f34b0d03616b
Mikey Saugstad <michaelssaugstad@gmail.com>
2018-12-10 11:44:16 -0800

    fixed downs of evolution to change assignment_end

--- conf/evolutions/default/23.sql
+++ conf/evolutions/default/23.sql
@@ -5,4 +5,5 @@ ALTER TABLE amt_assignment ALTER COLUMN assignment_end SET NOT NULL;
 
 # --- !Downs
 
+ALTER TABLE amt_assignment ALTER COLUMN assignment_end DROP NOT NULL;
 ALTER TABLE amt_assignment ALTER COLUMN confirmation_code DROP NOT NULL;
