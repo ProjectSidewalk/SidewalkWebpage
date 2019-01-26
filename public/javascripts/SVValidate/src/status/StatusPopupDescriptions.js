8aa4309a1805e7bc043da09200e316b216c04342
Aileen Zeng <aileenz@uw.edu>
2019-01-26 02:28:22 -0800

    Change wording on descriptions

--- public/javascripts/SVValidate/src/status/StatusPopupDescriptions.js
+++ public/javascripts/SVValidate/src/status/StatusPopupDescriptions.js
@@ -54,19 +54,19 @@ function StatusPopupDescriptions () {
             case "example-image-1":
                 return "These construction cones are block a pedestrian crosswalk";
             case "example-image-2":
-                return "This tree blocks an entire sidewalk";
+                return "This tree blocks the entire sidewalk";
             case "example-image-3":
                 return "This pole blocks the sidewalk and a curb ramp entrance";
             case "example-image-4":
                 return "This fire hydrant blocks the sidewalk";
             case "counterexample-image-1":
-                return "There is enough space to comfortably pass this pole";
+                return "There is enough space to comfortably maneuver around this pole";
             case "counterexample-image-2":
                 return "This pole located between, but not on these curb ramps";
             case "counterexample-image-3":
                 return "This tree is not on the pedestrian path";
             case "counterexample-image-4":
-                return "There is enough space to comfortably pass this recycling can lid";
+                return "There is enough space to comfortably maneuver around this lid";
         }
     }
 
@@ -79,7 +79,7 @@ function StatusPopupDescriptions () {
             case "example-image-3":
                 return "This sidewalk is cracked";
             case "example-image-4":
-                return "There is a substantial amount of grass growing in the sidewalk cracks";
+                return "There is a substantial amount of grass growing between the sidewalk cracks";
             case "counterexample-image-1":
                 return "Surface problems on curb ramps should not be labeled as surface problems";
             case "counterexample-image-2":
@@ -87,7 +87,7 @@ function StatusPopupDescriptions () {
             case "counterexample-image-3":
                 return "This grass is not on the pedestrian path";
             case "counterexample-image-4":
-                return "Normal sidewalk tiling is not a surface problem";
+                return "Normal sidewalk tiles are not surface problems";
         }
     }
 
@@ -104,9 +104,9 @@ function StatusPopupDescriptions () {
             case "counterexample-image-1":
                 return "Narrow sidewalks should not be labeled as missing sidewalks";
             case "counterexample-image-2":
-                return "Driveways are not missing sidewalks";
+                return "Driveways should not be labeled as missing sidewalks";
             case "counterexample-image-3":
-                return "Traffic medians are not missing sidewalks";
+                return "Traffic medians should not be labeled as missing sidewalks";
             case "counterexample-image-4":
                 return "Alleyways and side streets should not be labeled as missing sidewalks";
         }
