a968e69159008e77a04a21b38f177304bc7ed180
Shiven Bhatt <shivenbhatt@outlook.com>
2019-02-13 14:20:00 -0800

    working evolution to calculate and add in street_edge_id column

---
+++ conf/evolutions/default/28.sql
@@ -0,0 +1,29 @@
+# --- !Ups
+ALTER TABLE label
+  ADD COLUMN street_edge_id INTEGER;
+
+UPDATE label
+  SET street_edge_id = (
+      SELECT street_edge_id FROM street_edge AS s
+      ORDER BY ST_Distance(s.geom,ST_SetSRID(ST_MakePoint((
+                                                SELECT lp.lng FROM label_point AS lp
+                                                      WHERE lp.label_id = label.label_id
+                                                ), (
+                                                SELECT lp.lat FROM label_point AS lp
+                                                      WHERE lp.label_id = label.label_id
+                                                )),Find_SRID('sidewalk', 'street_edge', 'geom'))) ASC
+      LIMIT 1
+  )
+  WHERE (SELECT lp.lat FROM label_point AS lp WHERE lp.label_id = label.label_id LIMIT 1) IS NOT NULL AND
+        (SELECT lp.lng FROM label_point AS lp WHERE lp.label_id = label.label_id) IS NOT NULL;
+
+UPDATE label
+  SET street_edge_id = -1
+    WHERE street_edge_id IS NULL;
+
+ALTER TABLE label
+  ALTER COLUMN street_edge_id SET NOT NULL;
+
+# --- !Downs
+ALTER TABLE label
+  DROP COLUMN street_edge_id;
