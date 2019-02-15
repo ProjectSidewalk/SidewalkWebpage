c6cc5684fdf712a7d0999d9a42e90dab0133f656
misaugstad <michaelssaugstad@gmail.com>
2019-02-15 11:16:16 -0800

    fixed indentation of evolutions file

--- conf/evolutions/default/28.sql
+++ conf/evolutions/default/28.sql
@@ -5,15 +5,24 @@ ALTER TABLE label
 UPDATE label
   SET street_edge_id = (
       SELECT street_edge_id FROM street_edge AS s
-      ORDER BY ST_Distance(s.geom,ST_SetSRID(ST_MakePoint((
-                                                SELECT lp.lng FROM label_point AS lp
-                                                  WHERE lp.label_id = label.label_id
-                                                LIMIT 1
-                                                ), (
-                                                SELECT lp.lat FROM label_point AS lp
-                                                  WHERE lp.label_id = label.label_id
-                                                LIMIT 1
-                                                )),Find_SRID('sidewalk', 'street_edge', 'geom'))) ASC
+      ORDER BY ST_Distance(
+          s.geom,
+          ST_SetSRID(
+              ST_MakePoint(
+                  (
+                      SELECT lp.lng
+                      FROM label_point AS lp
+                      WHERE lp.label_id = label.label_id
+                      LIMIT 1
+                  ), (
+                      SELECT lp.lat
+                      FROM label_point AS lp
+                      WHERE lp.label_id = label.label_id
+                      LIMIT 1
+                  )),
+              Find_SRID('sidewalk', 'street_edge', 'geom')
+          )
+      ) ASC
       LIMIT 1
   )
   WHERE (SELECT lp.lat FROM label_point AS lp WHERE lp.label_id = label.label_id LIMIT 1) IS NOT NULL AND
@@ -21,8 +30,9 @@ UPDATE label
 
 UPDATE label
   SET street_edge_id = (
-    SELECT a.street_edge_id FROM audit_task as a
-      WHERE a.audit_task_id = label.audit_task_id
+    SELECT a.street_edge_id
+    FROM audit_task as a
+    WHERE a.audit_task_id = label.audit_task_id
     LIMIT 1
   )
   WHERE street_edge_id IS NULL;
