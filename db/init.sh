da2408ef66711ca897805b0dea99e877aa506459
misaugstad <michaelssaugstad@gmail.com>
2019-03-01 11:56:23 -0800

    moved queries to fix auto-inc cols to a separate file

--- db/init.sh
+++ db/init.sh
@@ -24,15 +24,7 @@ psql -v ON_ERROR_STOP=1 -U postgres -d postgres <<-EOSQL
 EOSQL
 
 psql -U sidewalk -d sidewalk -a -f /opt/schema.sql
-
-psql -v ON_ERROR_STOP=1 -U sidewalk -d sidewalk <<-EOSQL
-    SELECT setval('label_type_label_type_id_seq', (SELECT MAX(label_type_id) from sidewalk.label_type));
-    SELECT setval('mission_type_mission_type_id_seq', (SELECT MAX(mission_type_id) from sidewalk.mission_type));
-    SELECT setval('role_role_id_seq', (SELECT MAX(role_id) from sidewalk.role));
-    SELECT setval('survey_category_option_survey_category_option_id_seq', (SELECT MAX(survey_category_option_id) from sidewalk.survey_category_option));
-    SELECT setval('survey_question_survey_question_id_seq', (SELECT MAX(survey_question_id) from sidewalk.survey_question));
-    SELECT setval('tag_tag_id_seq', (SELECT MAX(tag_id) from sidewalk.tag));
-EOSQL
+psql -U sidewalk -d sidewalk -a -f /opt/fix-auto-inc.sql
 
 psql -U sidewalk -d sidewalk -c 'CREATE EXTENSION IF NOT EXISTS postgis'
 psql -U sidewalk -d sidewalk -c 'CREATE EXTENSION IF NOT EXISTS postgis_topology'
