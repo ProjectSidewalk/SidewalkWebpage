22ed4cc031daa4f9047bdd14aa008a021b620001
misaugstad <michaelssaugstad@gmail.com>
2019-03-08 15:58:31 -0800

    fixed front-end timer for turker HITs

--- app/models/amt/AMTAssignmentTable.scala
+++ app/models/amt/AMTAssignmentTable.scala
@@ -1,6 +1,7 @@
 package models.amt
 
 import java.sql.Timestamp
+import java.time.Instant
 
 import models.utils.MyPostgresDriver.simple._
 import play.api.Play.current
@@ -57,6 +58,18 @@ object AMTAssignmentTable {
 
   def getMostRecentAsmtEnd(workerId: String): Option[Timestamp] = db.withSession { implicit session =>
     amtAssignments.filter(_.workerId === workerId).sortBy(_.assignmentStart.desc).map(_.assignmentEnd).list.headOption
+  }
+
+  /**
+    * Get the number of milliseconds between now and the end time of the worker's most recent assignment.
+    *
+    * @param workerId
+    * @return
+    */
+  def getMsLeftOnMostRecentAsmt(workerId: String): Option[Long] = db.withSession { implicit session =>
+    val now: Timestamp = new Timestamp(Instant.now.toEpochMilli)
+    val endOption: Option[Timestamp] = getMostRecentAsmtEnd(workerId)
+    endOption.map(end => end.getTime - now.getTime)
   }
 
   def getMostRecentConfirmationCode(workerId: String): Option[String] = db.withSession { implicit session =>
