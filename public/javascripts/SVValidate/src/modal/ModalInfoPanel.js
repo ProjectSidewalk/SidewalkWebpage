e724631b6f36feee5e0c88e1c019adf4124fd1ad
maubinle <marianne_aubin_lequere@brown.edu>
2019-03-15 18:13:52 -0700

    changed formatting to fit to mobile screens & added mission start screen

---
+++ public/javascripts/SVValidate/src/modal/ModalInfoPanel.js
@@ -0,0 +1,34 @@
+function ModalInfoPanel (uiModalMissionComplete) {
+    var self = this;
+
+    function _handleButtonClick() {
+        svv.tracker.push("ClickOk_MissionComplete");
+        self.hide();
+    }
+
+    function hide () {
+        uiModalMissionComplete.background.css('visibility', 'hidden');
+        uiModalMissionComplete.holder.css('visibility', 'hidden');
+        uiModalMissionComplete.foreground.css('visibility', 'hidden');
+    }
+
+    function show (mission) {
+        var message = "You just validated " + mission.getProperty("labelsValidated") + " " +
+            svv.labelTypeNames[mission.getProperty("labelTypeId")] + " labels!";
+
+        uiModalMissionComplete.background.css('visibility', 'visible');
+        uiModalMissionComplete.missionTitle.html("Great Job!");
+        uiModalMissionComplete.message.html(message);
+        uiModalMissionComplete.agreeCount.html(mission.getProperty("agreeCount"));
+        uiModalMissionComplete.disagreeCount.html(mission.getProperty("disagreeCount"));
+        uiModalMissionComplete.notSureCount.html(mission.getProperty("notSureCount"));
+
+        uiModalMissionComplete.holder.css('visibility', 'visible');
+        uiModalMissionComplete.foreground.css('visibility', 'visible');
+        uiModalMissionComplete.closeButton.html('Validate more labels');
+        uiModalMissionComplete.closeButton.on('click', _handleButtonClick);
+    }
+
+    self.hide = hide;
+    self.show = show;
+}
