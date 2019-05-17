f6b694864bc742ef164a62a5e1946d00f58b0059
misaugstad <michaelssaugstad@gmail.com>
2019-05-17 12:59:16 -0700

    fixed some typos in user_stat table definition

--- conf/evolutions/default/50.sql
+++ conf/evolutions/default/50.sql
@@ -1,13 +1,13 @@
 # --- !Ups
 CREATE TABLE user_stat
 (
-    user_stats_id INT NOT NULL,
+    user_stat_id SERIAL NOT NULL,
     user_id TEXT NOT NULL,
     meters_audited DOUBLE PRECISION NOT NULL,
     labels_per_meter DOUBLE PRECISION,
     high_quality BOOLEAN NOT NULL,
     high_quality_manual BOOLEAN,
-    PRIMARY KEY (user_stats_id),
+    PRIMARY KEY (user_stat_id),
     FOREIGN KEY (user_id) REFERENCES sidewalk_user(user_id)
 );
 
