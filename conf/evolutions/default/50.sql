30100b2ec2abf6a1023fb9579e82d4804f41f90d
misaugstad <michaelssaugstad@gmail.com>
2019-05-17 17:20:48 -0700

    user_stat table now initialized with data when created

--- conf/evolutions/default/50.sql
+++ conf/evolutions/default/50.sql
@@ -11,5 +11,24 @@ CREATE TABLE user_stat
     FOREIGN KEY (user_id) REFERENCES sidewalk_user(user_id)
 );
 
+INSERT INTO user_stat (user_id, meters_audited, labels_per_meter, high_quality, high_quality_manual)
+    SELECT sidewalk_user.user_id, COALESCE(meters_audited, 0), labels_per_meter, COALESCE(labels_per_meter > 0.0375, TRUE), NULL
+    FROM (
+        SELECT user_id, meters_audited, label_count / meters_audited AS labels_per_meter
+        FROM (
+            SELECT user_id, SUM(label_count) AS label_count, COALESCE(SUM(distance_progress), 0) AS meters_audited
+            FROM (
+                SELECT sidewalk_user.user_id, distance_progress, COALESCE(COUNT(label.label_id), 0) AS label_count
+                FROM sidewalk_user
+                LEFT JOIN mission ON sidewalk_user.user_id = mission.user_id
+                LEFT JOIN label ON mission.mission_id = label.mission_id
+                GROUP BY sidewalk_user.user_id, mission.mission_id, distance_progress
+            ) label_counts
+            GROUP BY user_id
+        ) distance_and_count
+        WHERE meters_audited > 0
+    ) labeling_freq
+    RIGHT JOIN sidewalk_user ON labeling_freq.user_id = sidewalk_user.user_id;
+
 # --- !Downs
 DROP TABLE user_stat;
