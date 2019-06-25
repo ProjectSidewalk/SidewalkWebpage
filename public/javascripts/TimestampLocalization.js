034376f49fef4f51f1066311e10dd6ff43984e79
Shiven Bhatt <shivenbhatt@outlook.com>
2019-06-25 14:04:22 -0700

    timestamp code now runs after data loads, so sorting is unaffected.

--- public/javascripts/TimestampLocalization.js
+++ public/javascripts/TimestampLocalization.js
@@ -1,6 +1,27 @@
-// on document load, finds each element with class 'timestamp', converts timestamp from UTC to user's local time. Requires moment.js.
-$(document).ready(function () {
+// requires moment.js
+
+// on window load, run the changeTimestamps function.
+$(window).load(function () {
+    changeTimestamps();
+    //if any html changes, call the changeTimestamps function.
+    var observer = new MutationObserver(changeTimestamps);
+
+    observer.observe(document, {
+        attributes: true,
+        childList: true,
+        subtree: true,
+        characterData: true
+    });
+});
+
+//changes anything with the class timestamp from UTC to local time.
+function changeTimestamps(){
     $(".timestamp").each(function(){
+        if($(this).hasClass('local')){
+            return;
+        }
+        $(this).addClass('local');
+
         var timestampText = this.textContent + " UTC";
         var localDate = moment(new Date(timestampText));
 
@@ -12,6 +33,6 @@ $(document).ready(function () {
         //if the date cannot be parsed, ignore it and leave the text as-is. Otherwise, parse into local datetime format. 
         if(localDate.format(format) !== "Invalid date"){
             this.textContent = localDate.format(format);
-        }  
+        }
     });
-});
+}
