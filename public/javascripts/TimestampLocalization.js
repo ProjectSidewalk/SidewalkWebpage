93c16503cf477a0a1238a76db273705361f83504
misaugstad <michaelssaugstad@gmail.com>
2019-08-20 14:57:26 -0700

    small style fixes

--- public/javascripts/TimestampLocalization.js
+++ public/javascripts/TimestampLocalization.js
@@ -1,25 +1,26 @@
-//changes timestamps from UTC to local time. Updates any data order variables for tables.
+// Changes timestamps from UTC to local time. Updates any data order variables for tables.
 $(document).ready(function () {
-        $(".timestamp").each(function(){
-        if($(this).hasClass('local')){
+    $(".timestamp").each(function() {
+        if ($(this).hasClass('local')) {
             return;
         }
         $(this).addClass('local');
 
-        var timestampText = this.textContent;
+        let timestampText = this.textContent;
 
-        //add sorting attribute, if it's part of a table it will be sorted by this instead of the nicely formatted timestamp.
+        // Adds a sorting attribute, if it's part of a table it will be sorted by this instead of the nicely
+        // formatted timestamp.
         $(this).attr("data-order", timestampText);
 
-        var localDate = moment(new Date(timestampText + " UTC"));
+        let localDate = moment(new Date(timestampText + " UTC"));
 
-        var format = 'MMMM Do YYYY, h:mm:ss'
-        if($(this).hasClass('date')){
-            format = 'MMMM Do YYYY';            
+        let format = 'MMMM Do YYYY, h:mm:ss';
+        if ($(this).hasClass('date')) {
+            format = 'MMMM Do YYYY';
         }
-        
-        //if the date cannot be parsed, ignore it and leave the text as-is. Otherwise, parse into local datetime format. 
-        if(localDate.format(format) !== "Invalid date"){
+
+        // If the date cannot be parsed, ignore it and leave the text as-is. O/w, parse into local datetime format.
+        if (localDate.format(format) !== "Invalid date") {
             this.textContent = localDate.format(format);
         }
     });
