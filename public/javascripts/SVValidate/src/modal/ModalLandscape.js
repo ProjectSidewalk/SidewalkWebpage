387f6545063fa7d6bdd678673ee22e8d4c57d5e7
Hank Tadeusiak <hankt@uw.edu>
2019-08-30 13:47:45 -0700

    fixes orrientation bug in console for validations

--- public/javascripts/SVValidate/src/modal/ModalLandscape.js
+++ public/javascripts/SVValidate/src/modal/ModalLandscape.js
@@ -8,31 +8,6 @@
 function ModalLandscape (uiModal) {
     let self = this;
 
-    $(document).ready(function() {
-        // If the site is loaded in landscape mode first, 'loadedScreenLandscape' will be set to true
-        // and when the screen is flipped back to portrait mode the site will be reloaded to set the panoramas
-        // correctly.
-        let loadedScreenLandscape = false;
-
-        if (orientation != 0) {
-            self.show();
-            loadedScreenLandscape = true;
-        } else {
-            self.hide();
-        }
-
-        $(window).on('orientationchange', function(event) {
-            if (orientation != 0) {
-                self.show();
-            } else if (loadedScreenLandscape) {
-                location.reload();
-            } else {
-                self.hide();
-
-            }
-        });
-    });
-
     function hide () {
         uiModal.background.css('visibility', 'hidden');
         uiModal.holder.css('visibility', 'hidden');
