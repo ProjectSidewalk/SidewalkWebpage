f2a554b4aaea3644996768b41dd78366efb0d6af
misaugstad <michaelssaugstad@gmail.com>
2019-11-01 15:14:39 -0700

    removed unnecessary change in an evolution

--- conf/evolutions/default/68.sql
+++ conf/evolutions/default/68.sql
@@ -3,7 +3,7 @@ INSERT INTO mission_type (mission_type) VALUES ('labelmapValidation');
 
 # --- !Downs
 UPDATE mission
-SET mission_type_id = 4, completed = TRUE, labels_validated = 1, labels_progress = 1
+SET mission_type_id = 4, completed = TRUE, labels_progress = 1
 WHERE mission_type_id = 7;
 
 DELETE FROM mission_type WHERE mission_type = 'labelmapValidation';
