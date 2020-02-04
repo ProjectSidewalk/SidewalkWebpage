a1724b0f912b5104b416405b9a1b1f48c829a976
misaugstad <michaelssaugstad@gmail.com>
2020-02-04 12:42:26 -0800

    removed unused code

--- public/javascripts/developer.js
+++ public/javascripts/developer.js
@@ -1,17 +1,11 @@
 $(document).ready(function () {
     L.mapbox.accessToken = 'pk.eyJ1Ijoia290YXJvaGFyYSIsImEiOiJDdmJnOW1FIn0.kJV65G6eNXs4ATjWCtkEmA';
 
-    // Construct a bounding box for this map that the user cannot move out of
-    // https://www.mapbox.com/mapbox.js/example/v1.0.0/maxbounds/
-    var southWest = L.latLng(45.265, -123.010),
-        northEast = L.latLng(45.345, -122.900),
-        bounds = L.latLngBounds(southWest, northEast),
-
     // var tileUrl = "https://a.tiles.mapbox.com/v4/kotarohara.mmoldjeh/page.html?access_token=pk.eyJ1Ijoia290YXJvaGFyYSIsImEiOiJDdmJnOW1FIn0.kJV65G6eNXs4ATjWCtkEmA#13/38.8998/-77.0638";
-        tileUrl = "https:\/\/a.tiles.mapbox.com\/v4\/kotarohara.8e0c6890\/{z}\/{x}\/{y}.png?access_token=pk.eyJ1Ijoia290YXJvaGFyYSIsImEiOiJDdmJnOW1FIn0.kJV65G6eNXs4ATjWCtkEmA",
-        mapboxTiles = L.tileLayer(tileUrl, {
-            attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>'
-        });
+    var tileUrl = "https:\/\/a.tiles.mapbox.com\/v4\/kotarohara.8e0c6890\/{z}\/{x}\/{y}.png?access_token=pk.eyJ1Ijoia290YXJvaGFyYSIsImEiOiJDdmJnOW1FIn0.kJV65G6eNXs4ATjWCtkEmA";
+    var mapboxTiles = L.tileLayer(tileUrl, {
+        attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>'
+    });
 
     // Maps
     var mapAccessAttributes = L.mapbox.map('developer-access-attribute-map', "kotarohara.8e0c6890", {
@@ -25,52 +19,18 @@ $(document).ready(function () {
         zoomSnap: 0.5
     });
     var mapAccessScoreNeighborhoods = L.mapbox.map('developer-access-score-neighborhoods-map', "kotarohara.8e0c6890", {
-            maxZoom: 19,
-            minZoom: 9
+        maxZoom: 19,
+        minZoom: 9
     });
-
-    // Create 3 white overlay polygons. Add an overlay to each map.
-    var overlay = L.geoJson({ "type": "FeatureCollection",
-        "features": [ {
-            "type": "Feature",
-            "geometry": {
-                "type": "Polygon",
-                "coordinates": [[[ -76.989, 38.909], [ -76.989, 38.912], [-76.982, 38.912], [-76.982, 38.909], [ -76.989, 38.909]]]
-                // "coordinates": [ [ [-75, 36], [-75, 40], [-80, 40], [-80, 36],[-75, 36] ] ]
-            }
-        } ]
-    }, {
-        style: {
-            fillColor: "#fff",
-            fillOpacity: 0.75,
-            color: "#fff",
-            weight: 0
-        }
-    });
-    var overlays = [0, 1].map(function (i) { return L.geoJson({ "type": "FeatureCollection",
-        "features": [ {
-            "type": "Feature",
-            "geometry": {
-                "type": "Polygon",
-                "coordinates": [[[ -77.028, 38.910], [ -77.028, 38.929], [-77.009, 38.929], [-77.009, 38.910], [ -77.028, 38.910]]]
-                // "coordinates": [ [ [-75, 36], [-75, 40], [-80, 40], [-80, 36],[-75, 36] ] ]
-                // lat1=38.899&lng1=-77.008&lat2=38.920&lng2=-76.971
-            }
-        } ]
-    }, {
-        style: {
-            fillColor: "#fff",
-            fillOpacity: 0.6,
-            color: "#fff",
-            weight: 0
-        }
-    }); });
-    overlay.addTo(mapAccessAttributes);
-    overlays[0].addTo(mapAccessScoreNeighborhoods);
-    overlays[1].addTo(mapAccessScoreStreets);
 
     var colorMapping = util.misc.getLabelColors();
 
+    function getColor(d) {
+        return d > 0.75 ? '#4dac26' :
+            d > 0.5 ? '#b8e186' :
+                d > 0.25 ? '#f1b6da' :
+                    '#d01c8b';
+    }
 
     // Get city-specific parameters for the maps.
     $.getJSON('/cityAPIDemoParams', function(data) {
@@ -144,11 +104,4 @@ $(document).ready(function () {
             }).addTo(mapAccessScoreNeighborhoods);
         });
     });
-
-    function getColor(d) {
-        return d > 0.75 ? '#4dac26' :
-            d > 0.5 ? '#b8e186' :
-                d > 0.25 ? '#f1b6da' :
-                    '#d01c8b';
-    }
 });
