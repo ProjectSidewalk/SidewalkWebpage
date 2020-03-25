d1d9b514cd931e7a4af36d50d66a67cec61039c7
misaugstad <michaelssaugstad@gmail.com>
2020-03-25 14:20:09 -0700

    adds a validation_task_environment table

--- conf/evolutions/default/77.sql
+++ conf/evolutions/default/77.sql
@@ -1,5 +1,25 @@
 # --- !Ups
 ALTER TABLE audit_task_environment ADD COLUMN language TEXT NOT NULL DEFAULT 'en';
 
+CREATE TABLE validation_task_environment (
+     validation_task_environment_id SERIAL NOT NULL,
+     mission_id INT,
+     browser TEXT,
+     browser_version TEXT,
+     browser_width INT,
+     browser_height INT,
+     avail_width INT,
+     avail_height INT,
+     screen_width INT,
+     screen_height INT,
+     operating_system TEXT,
+     ip_address TEXT,
+     language TEXT NOT NULL,
+     PRIMARY KEY (validation_task_environment_id),
+     FOREIGN KEY (mission_id) REFERENCES mission(mission_id)
+);
+
 # --- !Downs
+DROP TABLE validation_task_environment;
+
 ALTER TABLE audit_task_environment DROP COLUMN language;
