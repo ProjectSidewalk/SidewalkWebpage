2fd7a7f821befc318df755ec04e736bf8bfc4e6a
Leon Li <31671708+leonli33@users.noreply.github.com>
2020-05-26 13:49:52 -0700

    Storing data on user actions in textfield.
    Sends data to table whenever user clicks on textfield or submits data from textfield.

--- public/javascripts/SVValidate/src/label/Label.js
+++ public/javascripts/SVValidate/src/label/Label.js
@@ -253,6 +253,7 @@ function Label(params) {
         setProperty("isMobile", isMobile());
 
         if(comment) {
+            svv.tracker.push("ValidationTextField_DataEntered");
             let data = prepareLabelCommentData(comment, svv.panorama.getPosition(), userPov, zoom);
             submitComment(data);
         }
