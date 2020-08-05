6e06540708fc95a938c3d54a89ee4cfefcc8b445
shokiami <kiami.sho@gmail.com>
2020-08-04 17:07:10 -0700

    Updated comment for loading audio early.

--- public/javascripts/SVLabel/src/SVLabel/ribbon/RibbonMenu.js
+++ public/javascripts/SVLabel/src/SVLabel/ribbon/RibbonMenu.js
@@ -126,7 +126,8 @@ function RibbonMenu(overlayMessageBox, tracker, uiRibbonMenu) {
                     svl.ui.canvas.drawingLayer.triggerHandler('mousemove');
                 }
 
-                // Load sound effect early.
+                // Loads the audio for when a label is placed. Safari requires audios to be loaded each time before being played.
+                // Since this takes time, it is done early (when user selects label type) so that it is ready for when the label is placed.
                 if ('audioEffect' in svl) {
                     svl.audioEffect.load('drip');
                 }
