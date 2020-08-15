30aead1f4c522bf070005a7ff341535824b32348
Matthew Johnson <32688680+MattJ12@users.noreply.github.com>
2020-08-14 21:39:10 -0700

    Consolidates code for User Dash

--- public/javascripts/Progress/src/Progress.js
+++ public/javascripts/Progress/src/Progress.js
@@ -1,20 +1,10 @@
-function Progress (_, $, c3, L, role, difficultRegionIds) {
+function Progress (_, $, c3, L, role, map, initializeNeighborhoodPolygons) {
     var self = {};
     var completedInitializingNeighborhoodPolygons = false;
     var completedInitializingAuditedStreets = false;
     var completedInitializingSubmittedLabels = false;
     var completedInitializingAuditCountChart = false;
     var completedInitializingAuditedTasks = false;
-
-    var neighborhoodPolygonStyle = {
-            color: '#888',
-            weight: 2,
-            opacity: 0.80,
-            fillColor: "#808080",
-            fillOpacity: 0.1
-        },
-        layers = [],
-        currentLayer;
 
     var _data = {
         neighborhoodPolygons: null,
@@ -30,21 +20,6 @@ function Progress (_, $, c3, L, role, difficultRegionIds) {
     var tileUrl = "https:\/\/a.tiles.mapbox.com\/v4\/kotarohara.8e0c6890\/{z}\/{x}\/{y}.png?access_token=pk.eyJ1Ijoia290YXJvaGFyYSIsImEiOiJDdmJnOW1FIn0.kJV65G6eNXs4ATjWCtkEmA";
     var mapboxTiles = L.tileLayer(tileUrl, {
         attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>'
-    });
-    var map = L.mapbox.map('map', "mapbox.streets", {
-        maxZoom: 19,
-        minZoom: 9,
-        zoomSnap: 0.5
-    });
-
-    // Set the city-specific default zoom, location, and max bounding box to prevent the user from panning away.
-    $.getJSON('/cityMapParams', function(data) {
-        map.setView([data.city_center.lat, data.city_center.lng]);
-        var southWest = L.latLng(data.southwest_boundary.lat, data.southwest_boundary.lng);
-        var northEast = L.latLng(data.northeast_boundary.lat, data.northeast_boundary.lng);
-        map.setMaxBounds(L.latLngBounds(southWest, northEast));
-        map.setZoom(data.default_zoom);
-        initializeOverlayPolygon(map, data.city_center.lat, data.city_center.lng);
     });
 
     var popup = L.popup().setContent('<p>Hello!</p>');
@@ -79,202 +54,6 @@ function Progress (_, $, c3, L, role, difficultRegionIds) {
                 }
             }
         }
-    }
-
-    /**
-     * This function adds a semi-transparent white polygon on top of a map.
-     */
-    function initializeOverlayPolygon(map, lat, lng) {
-        var overlayPolygon = {
-            "type": "FeatureCollection",
-            "features": [{"type": "Feature", "geometry": {
-                "type": "Polygon", "coordinates": [
-                        [
-                            [lng + 2, lat - 2],
-                            [lng + 2, lat + 2],
-                            [lng - 2, lat + 2],
-                            [lng - 2, lat - 2],
-                            [lng + 2, lat - 2]
-                        ]
-                ]}}]};
-        var layer = L.geoJson(overlayPolygon);
-        layer.setStyle({color: "#ccc", fillColor: "#ccc"});
-        layer.addTo(map);
-    }
-
-    /**
-     * render points
-     */
-    function initializeNeighborhoodPolygons(map, rates) {
-        function onEachNeighborhoodFeature(feature, layer) {
-
-            var regionId = feature.properties.region_id;
-            var regionName = feature.properties.region_name;
-            var userCompleted = feature.properties.user_completed;
-            var url = "/audit/region/" + regionId;
-            // default popup content if we don't find neighborhood in list of neighborhoods from query
-            var popupContent = "Do you want to find accessibility problems in " + regionName + "? " +
-                    "<a href='" + url + "' class='region-selection-trigger' regionId='" + regionId + "'>Sure!</a>";
-            var compRate = 0;
-            var milesLeft = 0;
-            for (var i = 0; i < rates.length; i++) {
-                if (rates[i].region_id === feature.properties.region_id) {
-                    var measurementSystem = i18next.t('common:measurement-system');
-                    compRate = Math.round(100.0 * rates[i].rate);
-                    distanceLeft = rates[i].total_distance_m - rates[i].completed_distance_m;
-                    // If using metric system, convert from meters to kilometers. If using IS system, convert from meters to miles.
-                    if (measurementSystem === "metric") distanceLeft *= 0.001;
-                    else distanceLeft *= 0.000621371;
-                    distanceLeft = Math.round(distanceLeft);
-
-                    var advancedMessage = '';
-                    if (difficultRegionIds.includes(feature.properties.region_id)) {
-                           advancedMessage = '<br><b>Careful!</b> This neighborhood is not recommended for new users.<br><br>';
-                    }
-
-                    if (userCompleted) {
-                        popupContent = "<strong>" + regionName + "</strong>: " +
-                            i18next.t("common:map.100-percent-complete") + "<br>" +
-                            i18next.t("common:map.thanks");
-                    } else if (compRate === 100) {
-                        popupContent = "<strong>" + regionName + "</strong>: " +
-                            i18next.t("common:map.100-percent-complete") + "<br>" + advancedMessage +
-                            i18next.t("common:map.click-to-help", { url: url, regionId: regionId });
-                    } else if (distanceLeft === 0) {
-                        popupContent = "<strong>" + regionName + "</strong>: " +
-                            i18next.t("common:map.percent-complete", { percent: compRate }) + "<br>" +
-                            i18next.t("common:map.less-than-one-unit-left") + "<br>" + advancedMessage +
-                            i18next.t("common:map.click-to-help", { url: url, regionId: regionId });
-                    } else if (distanceLeft === 1) {
-                        var popupContent = "<strong>" + regionName + "</strong>: " +
-                            i18next.t("common:map.percent-complete", { percent: compRate }) + "<br>" +
-                            i18next.t("common:map.distance-left-one-unit") + "<br>" + advancedMessage +
-                            i18next.t("common:map.click-to-help", { url: url, regionId: regionId });
-                    } else {
-                        var popupContent = "<strong>" + regionName + "</strong>: " +
-                            i18next.t("common:map.percent-complete", { percent: compRate }) + "<br>" +
-                            i18next.t("common:map.distance-left", { n: distanceLeft }) + "<br>" + advancedMessage +
-                            i18next.t("common:map.click-to-help", { url: url, regionId: regionId });
-                    }
-                    break;
-                }
-            }
-            layer.bindPopup(popupContent);
-            layers.push(layer);
-
-            layer.on('mouseover', function (e) {
-                this.openPopup();
-                this.setStyle({color: "red", fillColor: "red"});
-
-            });
-            layer.on('mouseout', function (e) {
-                for (var i = layers.length - 1; i >= 0; i--) {
-                    if (currentLayer !== layers[i])
-                        layers[i].setStyle(neighborhoodPolygonStyle);
-                }
-                //this.setStyle(neighborhoodPolygonStyle);
-            });
-            layer.on('click', function (e) {
-                currentLayer = this;
-
-
-                // Log when a user clicks on a region on the user map
-                // Logs are of the form "Click_module=UserMap_regionId=<regionId>_distanceLeft=<"0", "<1", "1" or ">1">_target=inspect"
-                // Log is stored in WebpageActivityTable
-                var regionId = e.target.feature.properties.region_id;
-                var ratesEl = rates.find(function(x){
-                    return regionId == x.region_id;
-                });
-                var compRate = Math.round(100.0 * ratesEl.rate);
-                var milesLeft = Math.round(0.000621371 * (ratesEl.total_distance_m - ratesEl.completed_distance_m));
-                var distanceLeft = "";
-                if (compRate === 100) {
-                    distanceLeft = "0";
-                }
-                else if (milesLeft === 0) {
-                    distanceLeft = "<1";
-                }
-                else if (milesLeft === 1) {
-                    distanceLeft = "1";
-                }
-                else {
-                    distanceLeft = ">1";
-                }
-                var url = "/userapi/logWebpageActivity";
-                var async = true;
-                var data = "Click_module=UserMap_regionId="+regionId+"_distanceLeft="+distanceLeft+"_target=inspect";
-                $.ajax({
-                    async: async,
-                    contentType: 'application/json; charset=utf-8',
-                    url: url,
-                    type: 'post',
-                    data: JSON.stringify(data),
-                    dataType: 'json',
-                    success: function (result) {
-                    },
-                    error: function (result) {
-                        console.error(result);
-                    }
-                });
-            });
-
-        }
-
-        $.getJSON("/neighborhoods", function (data) {
-            _data.neighborhoodPolygons = data;
-
-            L.geoJson(data, {
-                style: function (feature) {
-                    return $.extend(true, {}, neighborhoodPolygonStyle);
-                },
-                onEachFeature: onEachNeighborhoodFeature
-            })
-                .addTo(map);
-            completedInitializingNeighborhoodPolygons = true;
-            handleInitializationComplete(map);
-        });
-
-
-        // Logs when a region is selected from the user map and 'Click here' is clicked
-        // Logs are of the form "Click_module=UserMap_regionId=<regionId>_distanceLeft=<"0", "<1", "1" or ">1">_target=audit"
-        // Log is stored in WebpageActivityTable
-        $("#map").on('click', '.region-selection-trigger', function () {
-            var regionId = $(this).attr('regionId');
-            var ratesEl = rates.find(function(x){
-                return regionId == x.region_id;
-            });
-            var compRate = Math.round(100.0 * ratesEl.rate);
-            var milesLeft = Math.round(0.000621371 * (ratesEl.total_distance_m - ratesEl.completed_distance_m));
-            var distanceLeft = "";
-            if (compRate === 100) {
-                distanceLeft = "0";
-            }
-            else if (milesLeft === 0) {
-                distanceLeft = "<1";
-            }
-            else if (milesLeft === 1) {
-                distanceLeft = "1";
-            }
-            else {
-                distanceLeft = ">1";
-            }
-            var url = "/userapi/logWebpageActivity";
-            var async = true;
-            var data = "Click_UserMap_regionId="+regionId+"_distanceLeft="+distanceLeft+"_target=audit";
-            $.ajax({
-                async: async,
-                contentType: 'application/json; charset=utf-8',
-                url: url,
-                type: 'post',
-                data: JSON.stringify(data),
-                dataType: 'json',
-                success: function (result) {
-                },
-                error: function (result) {
-                    console.error(result);
-                }
-            });
-        });
     }
 
     /**
