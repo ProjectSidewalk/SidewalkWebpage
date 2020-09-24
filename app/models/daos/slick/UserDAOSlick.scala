155144372bd6f33ad2e622ab8718e672e96a265a
Jon Froehlich <jonfroehlich@gmail.com>
2020-09-24 16:06:46 -0700

    cleaned up the code
    removed the today and yesterday specific functions for countAllUsersContributed

--- app/models/daos/slick/UserDAOSlick.scala
+++ app/models/daos/slick/UserDAOSlick.scala
@@ -196,53 +196,37 @@ object UserDAOSlick {
    * Returns a count of all users who have ever started (or completed) an audit or validation task (across all roles).
    * That is, this method counts the number of unique users across both audit users and validation users
    *
+   * This method is deprecated. Use the parameterized version of countAllUsersContributed instead
+   *
    * @param taskCompleted
    * @return
    */
-  def countAllUsersContributed(taskCompleted: Boolean): Int = db.withSession { implicit session =>
-    val tasks = if (taskCompleted) AuditTaskTable.completedTasks else auditTaskTable
-    val users = tasks.map(_.userId) ++ LabelValidationTable.validationLabels.map(_.userId)
+//  def countAllUsersContributedOld(taskCompleted: Boolean): Int = db.withSession { implicit session =>
+//    val tasks = if (taskCompleted) AuditTaskTable.completedTasks else auditTaskTable
+//    val users = tasks.map(_.userId) ++ LabelValidationTable.validationLabels.map(_.userId)
+//
+//    // groupBy gives us a list of pairs of userid to userid and then map gives us
+//    // the first element of the pair for each item
+//    val distinctUsers = users.groupBy(x => x).map(_._1)
+//
+//    distinctUsers.size.run
+//  }
 
-    // groupBy gives us a list of pairs of userid to userid and then map gives us
-    // the first element of the pair for each item
-    val distinctUsers = users.groupBy(x => x).map(_._1)
-
-    distinctUsers.size.run
-  }
-
-  /**
-   * Returns a count of all users who completed at least one audit task or at least one validation today
-   *
-   * @return
-   */
-  def countAllUsersContributedToday(): Int = db.withSession { implicit session =>
-    val countQuery = Q.queryNA[Int](
-      """SELECT COUNT(DISTINCT(users.user_id))
-        |FROM (
-        |    SELECT DISTINCT(user_id)
-        |    FROM label_validation
-        |    WHERE (label_validation.end_timestamp AT TIME ZONE 'PST')::date = (NOW() AT TIME ZONE 'PST')::date
-        |    UNION
-        |    SELECT DISTINCT(user_id)
-        |    FROM audit_task
-        |    WHERE audit_task.completed = TRUE
-        |       AND (audit_task.task_end AT TIME ZONE 'PST')::date = (NOW() AT TIME ZONE 'PST')::date
-        |) users
-        |INNER JOIN user_stat ON users.user_id = user_stat.user_id
-        """.stripMargin
-    )
-    countQuery.list.head
-  }
 
   /**
-   * Returns a count of all users who completed at least one audit task or at least one validation today
+   * Returns a count of all users who completed at least one audit task or at least one validation given
+   * the following parameters
    *
    * @param timeInterval: can be "today", "yesterday", "week", or "month". If anything else, defaults to "all time"
-   * @param taskCompletedOnly: if true, only counts users who have completed one audit task or at least one validation
-   * @param highQualityOnly: if true, only counts users who are marked as high quality
+   * @param taskCompletedOnly: if true, only counts users who have completed one audit task or at least one validation.
+   *                           Defaults to false.
+   * @param highQualityOnly: if true, only counts users who are marked as high quality. Defaults to false.
    * @return
    */
-  def countAllUsersContributedTimeInterval(timeInterval: String = "all time", taskCompletedOnly: Boolean = false, highQualityOnly: Boolean = false): Int = db.withSession { implicit session =>
+  def countAllUsersContributed(timeInterval: String = "all time", taskCompletedOnly: Boolean = false, highQualityOnly: Boolean = false): Int = db.withSession { implicit session =>
+
+    // Build up SQL string related to validation and audit task time intervals
+    // Defaults to *not* specifying a time (which is the same thing as "all time")
     var lblValidationTimeIntervalSql = ""
     var auditTaskTimeIntervalSql = ""
     if(timeInterval.equalsIgnoreCase("today")){
@@ -259,11 +243,14 @@ object UserDAOSlick {
       auditTaskTimeIntervalSql = """AND (audit_task.task_end AT TIME ZONE 'PST')::date > DATE_SUB(NOW() AT TIME ZONE 'PST', INTERVAL 1 MONTH)"""
     }
 
+    // Add in the optional SQL WHERE statement for filtering on high quality users
     var highQualityOnlySql = ""
     if(highQualityOnly){
       highQualityOnlySql = """WHERE user_stat.high_quality_manual = TRUE OR user_stat.high_quality_manual IS NULL;"""
     }
 
+    // Add in the task completion logic. Note: to keep the Scala code clean, if taskCompletedOnly is false
+    // then I put in a SQL statement to check both for all completed and all incompleted tasks.
     var auditTaskCompletedSql = ""
     if(taskCompletedOnly){
       auditTaskCompletedSql = """audit_task.completed = TRUE"""
@@ -285,34 +272,11 @@ object UserDAOSlick {
                    |INNER JOIN user_stat ON users.user_id = user_stat.user_id
                    |$highQualityOnlySql
                  """.stripMargin
+
     println(s"Running query: countAllUsersContributedTimeInterval(timeInterval=$timeInterval, taskCompletedOnly=$taskCompletedOnly, highQualityOnly=$highQualityOnly)")
     println(query)
+
     val countQuery = Q.queryNA[Int](query)
-    countQuery.list.head
-
-  }
-
-  /**
-   * Returns a count of all users who completed at least one audit task or at least one validation yesterday
-   *
-   * @return
-   */
-  def countAllUsersContributedYesterday(): Int = db.withSession { implicit session =>
-    val countQuery = Q.queryNA[Int](
-      """SELECT COUNT(DISTINCT(users.user_id))
-        |FROM (
-        |    SELECT DISTINCT(user_id)
-        |    FROM label_validation
-        |    WHERE (label_validation.end_timestamp AT TIME ZONE 'PST')::date = (now() AT TIME ZONE 'PST')::date - interval '1' day
-        |    UNION
-        |    SELECT DISTINCT(user_id)
-        |    FROM audit_task
-        |    WHERE audit_task.completed = TRUE
-        |        AND (audit_task.task_end AT TIME ZONE 'PST')::date = (now() AT TIME ZONE 'PST')::date - interval '1' day
-        |) users
-        |INNER JOIN user_stat ON users.user_id = user_stat.user_id
-        """.stripMargin
-    )
     countQuery.list.head
   }
 
