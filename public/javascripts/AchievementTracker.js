544798c8919fc8f645f2c0deaa54d55dcb255409
Jon Froehlich <jonfroehlich@gmail.com>
2020-10-02 05:31:14 -0700

    temporarily removing badges so i can rename them all lowercase

---
+++ public/javascripts/AchievementTracker.js
@@ -0,0 +1,31 @@
+class AchievementTracker{
+    /**
+     *
+     * @param curMissionCnt
+     * @param curDistance
+     * @param curLabelsCnt
+     * @param curValidationsCnt
+     * @param curAccuracy
+     */
+    constructor(curMissionCnt, curDistance, curLabelsCnt, curValidationsCnt, curAccuracy){
+        this.mapBadges = {
+            "missions" : {
+                1 : new Badge("mission", )
+            }
+        }
+    }
+
+    // images/badges/Badge_Missions_Badge1.png
+}
+
+class Badge{
+    /**
+     *
+     * @param name
+     * @param iconPath
+     * @param badgeAwardThreshold
+     */
+    constructor(name, iconFilename, badgeAwardThreshold){
+
+    }
+}
