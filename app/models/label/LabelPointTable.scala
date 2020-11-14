3bf3fc68eb88ad3a5ec1752aaeff799ed43f4087
Mikey Saugstad <michaelssaugstad@gmail.com>
2020-11-13 19:23:46 -0800

    adds rough approximation of latlng without depth data

--- app/models/label/LabelPointTable.scala
+++ app/models/label/LabelPointTable.scala
@@ -7,7 +7,8 @@ import scala.slick.lifted.ForeignKeyQuery
 
 case class LabelPoint(labelPointId: Int, labelId: Int, svImageX: Int, svImageY: Int, canvasX: Int, canvasY: Int,
                       heading: Float, pitch: Float, zoom: Int, canvasHeight: Int, canvasWidth: Int,
-                      alphaX: Float, alphaY: Float, lat: Option[Float], lng: Option[Float], geom: Option[Point])
+                      alphaX: Float, alphaY: Float, lat: Option[Float], lng: Option[Float], geom: Option[Point],
+                      computationMethod: Option[String])
 
 class LabelPointTable(tag: slick.lifted.Tag) extends Table[LabelPoint](tag, Some("sidewalk"), "label_point") {
   def labelPointId = column[Int]("label_point_id", O.PrimaryKey, O.AutoInc)
@@ -26,9 +27,10 @@ class LabelPointTable(tag: slick.lifted.Tag) extends Table[LabelPoint](tag, Some
   def lat = column[Option[Float]]("lat", O.Nullable)
   def lng = column[Option[Float]]("lng", O.Nullable)
   def geom = column[Option[Point]]("geom", O.Nullable)
+  def computationMethod = column[Option[String]]("computation_method", O.Nullable)
 
   def * = (labelPointId, labelId, svImageX, svImageY, canvasX, canvasY, heading, pitch, zoom,
-    canvasHeight, canvasWidth, alphaX, alphaY, lat, lng, geom) <> ((LabelPoint.apply _).tupled, LabelPoint.unapply)
+    canvasHeight, canvasWidth, alphaX, alphaY, lat, lng, geom, computationMethod) <> ((LabelPoint.apply _).tupled, LabelPoint.unapply)
 
   def label: ForeignKeyQuery[LabelTable, Label] =
     foreignKey("label_point_label_id_fkey", labelId, TableQuery[LabelTable])(_.labelId)
