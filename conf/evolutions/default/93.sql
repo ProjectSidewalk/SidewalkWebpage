f2bcb6410d2c29fd08889ad027466e2b28481c6c
Mikey Saugstad <michaelssaugstad@gmail.com>
2020-11-13 19:30:05 -0800

    fixes style of evolutions file

--- conf/evolutions/default/93.sql
+++ conf/evolutions/default/93.sql
@@ -10,7 +10,10 @@ UPDATE label_point
 SET computation_method = 'approximation1',
     lat = panorama_lat + (10 * cos(radians(heading)) / 111111),
     lng = panorama_lng + (10 * sin(radians(heading)) / (111111 * cos(radians(panorama_lat)))),
-    geom = ST_SetSRID( ST_Point( panorama_lng + (10 * sin(radians(heading)) / (111111 * cos(radians(panorama_lat)))), panorama_lat + (10 * cos(radians(heading)) / 111111)), 4326)
+    geom = ST_SetSRID( ST_Point(
+        panorama_lng + (10 * sin(radians(heading)) / (111111 * cos(radians(panorama_lat)))),
+        panorama_lat + (10 * cos(radians(heading)) / 111111)
+    ), 4326)
 FROM label
 WHERE label_point.label_id = label.label_id
     AND computation_method IS NULL;
