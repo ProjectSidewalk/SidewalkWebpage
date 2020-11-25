dc0900ea5fade03bf14c28399d78d0627264b5ef
Mikey Saugstad <michaelssaugstad@gmail.com>
2020-11-24 16:17:16 -0800

    fixes timestamp formatting issue on some phones

--- public/javascripts/common/timestampLocalization.js
+++ public/javascripts/common/timestampLocalization.js
@@ -6,16 +6,21 @@ function updateTimestamps(locale) {
                 return;
             }
             $(this).addClass('local');
+
+            // Skip if the timestamp is null.
+            if (_.isEmpty(this.textContent)) {
+                return;
+            }
     
             let timestampText = this.textContent;
-    
+
             // Adds a sorting attribute, if it's part of a table it will be sorted by this instead of the nicely
             // formatted timestamp.
             $(this).attr("data-order", timestampText);
     
             // Converts to local time and changes to local date format.
             moment.locale(locale);
-            let localDate = moment(new Date(timestampText + " UTC"));
+            let localDate = moment(timestampText + ' UTC');
     
             // If the date cannot be parsed, ignore it and leave the text as-is. O/w, parse into local datetime format.
             if (localDate.isValid()) {
