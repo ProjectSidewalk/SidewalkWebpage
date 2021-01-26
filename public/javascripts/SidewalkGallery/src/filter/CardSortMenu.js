3cce053bb5b3036b93f8ccaf56b14773a6ef1cdb
kumararoosh <arkumar@Aroosh-Computer.localdomain>
2021-01-25 17:48:28 -0800

    Added comments to CardSortMenu.js

--- public/javascripts/SidewalkGallery/src/filter/CardSortMenu.js
+++ public/javascripts/SidewalkGallery/src/filter/CardSortMenu.js
@@ -8,11 +8,13 @@
 function CardSortMenu(uiCardSortMenu) {
     let self = this;
 
+    // The code values associated with each sort.
     let orderCodes = {
         sort_LeastSevere: 0,
         sort_MostSevere: 1 
     }
 
+    // The status of the sorting at any point.
     let status = {
         severity: 1,
         sortType: "none"
@@ -25,7 +27,10 @@ function CardSortMenu(uiCardSortMenu) {
             });
         }
     }
-
+    
+    /**
+     * Callback function for when sorting order of cards is changed.
+     */
     function handleSortSwitchClickCallback() {
         let sortType = $(this).val();
         setStatus("sortType", sortType);
@@ -36,11 +41,19 @@ function CardSortMenu(uiCardSortMenu) {
         sg.cardContainer.sortCards(orderCodes[sortType]);
     }
 
-    // TODO: perhaps remove this if no other status added
+    /**
+     * Returns the status of the CardSortMenu
+     */
     function getStatus() {
+        // TODO: perhaps remove this if no other status added
         return status;
     }
 
+    /**
+     * Sets a specific key, value pair in the status
+     * @param {*} key 
+     * @param {*} value 
+     */
     function setStatus(key, value) {
         if (key in status) {
             status[key] = value;
