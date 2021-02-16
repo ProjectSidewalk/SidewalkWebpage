4ba81f882b677b4e706c09717ae66c2342a8e86a
Mikey Saugstad <michaelssaugstad@gmail.com>
2021-02-16 14:09:18 -0800

    removes some unused code

--- public/javascripts/SVLabel/src/SVLabel/canvas/Canvas.js
+++ public/javascripts/SVLabel/src/SVLabel/canvas/Canvas.js
@@ -131,7 +131,6 @@ function Canvas(ribbon) {
             canvasHeight: svl.canvasHeight,
             canvasDistortionAlphaX: svl.alpha_x,
             canvasDistortionAlphaY: svl.alpha_y,
-            //labelId: svl.getLabelCounter(),
             tutorial: svl.missionContainer.getCurrentMission().getProperty("missionType") === "auditOnboarding",
             labelType: labelDescription.id,
             labelDescription: labelDescription.text,
