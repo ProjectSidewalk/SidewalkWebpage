f41a5a58855ba6fedc21dd784bfc8610c3b5dae6
Mikey Saugstad <michaelssaugstad@gmail.com>
2021-02-25 15:55:53 -0800

    adds CSS linter settings

---
+++ .stylelintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "extends": "stylelint-config-standard",
+    "rules": {
+        // Overriding standard rules.
+        "indentation": 4,
+        "max-empty-lines": 2,
+        "comment-empty-line-before": null,
+        "color-hex-length": null,
+        "length-zero-no-unit": null
+        // Additional rules.
+    }
+}
+
