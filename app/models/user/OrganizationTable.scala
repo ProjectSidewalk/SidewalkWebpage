744a7f5065b1b064eb0a03f00214b44ffe7f645a
shokiami <kiami.sho@gmail.com>
2021-03-28 14:39:54 -0700

    Added organization and user_org tables and basic functionality.

---
+++ app/models/user/OrganizationTable.scala
@@ -0,0 +1,57 @@
+package models.label
+
+import models.utils.MyPostgresDriver.simple._
+import play.api.Play.current
+
+case class Organization(orgId: Int, orgName: String, orgDescription: String)
+
+class OrganizationTable(tag: slick.lifted.Tag) extends Table[Organization](tag, Some("sidewalk"), "organization") {
+  def orgId = column[Int]("org_id", O.PrimaryKey, O.AutoInc)
+  def orgName = column[String]("org_name", O.NotNull)
+  def orgDescription = column[String]("org_description")
+
+  def * = (orgId, orgName, orgDescription) <> ((Organization.apply _).tupled, Organization.unapply)
+}
+
+/**
+ * Data access object for the organization table.
+ */
+object OrganizationTable {
+  val db = play.api.db.slick.DB
+  val organizations = TableQuery[OrganizationTable]
+
+  /**
+    * Gets a list of all organizations.
+    */
+  def getAllOrganizations: List[Organization] = db.withSession { implicit session => 
+    organizations.list
+  }
+
+  /**
+    * Gets the organization name from the given organization id.
+    */
+  def getOrganizationName(orgId: Int): Option[String] = db.withTransaction { implicit session =>
+    organizations.filter(_.orgId === orgId).map(_.orgName).firstOption
+  }
+
+  /**
+    * Gets the organization description from the given organization id.
+    */
+  def getOrganizationDescription(orgId: Int): Option[String] = db.withTransaction { implicit session =>
+    organizations.filter(_.orgId === orgId).map(_.orgDescription).firstOption
+  }
+
+  /**
+    * Inserts an organization into the organization table.
+    */
+  def save(orgName: String, orgDescription: String): Int = db.withSession { implicit session =>
+    organizations.insertOrUpdate(Organization(0, orgName, orgDescription))
+  }
+
+  /**
+    * Deletes an organization from the organization table.
+    */
+  def remove(orgId: Int): Int = db.withSession { implicit session =>
+    organizations.filter(_.orgId === orgId).delete
+  }
+}
---
+++ app/models/user/UserOrgTable.scala
@@ -0,0 +1,51 @@
+package models.label
+
+import java.util.UUID
+import models.utils.MyPostgresDriver.simple._
+import play.api.Play.current
+
+case class UserOrg(userOrgId: Int, userId: String, orgId: Int)
+
+class UserOrgTable(tag: slick.lifted.Tag) extends Table[UserOrg](tag, Some("sidewalk"), "user_org_table") {
+  def userOrgId = column[Int]("user_org_id", O.PrimaryKey, O.AutoInc)
+  def userId = column[String]("user_id", O.NotNull)
+  def orgId = column[Int]("org_id", O.NotNull)
+
+  def * = (userOrgId, userId, orgId) <> ((UserOrg.apply _).tupled, UserOrg.unapply)
+}
+
+/**
+ * Data access object for the user_org table.
+ */
+object UserOrgTable {
+  val db = play.api.db.slick.DB
+  val userOrgs = TableQuery[UserOrgTable]
+
+  /**
+  * Get all organizations the given user is affiliated with.
+  */
+  def getAllOrgs(userId: UUID): List[Int] = db.withSession { implicit session =>
+    userOrgs.filter(_.userId === userId.toString).map(_.orgId).list
+  }
+
+  /**
+  * Get all users affiliated with the given organization.
+  */
+  def getAllUsers(orgId: Int): List[String] = db.withSession { implicit session =>
+    userOrgs.filter(_.orgId === orgId).map(_.userId).list
+  }
+  
+  /**
+    * Inserts a user organization affiliation into the user_org table.
+    */
+  def save(userId: UUID, orgId: Int): Int = db.withSession { implicit session =>
+    userOrgs.insertOrUpdate(UserOrg(0, userId.toString, orgId))
+  }
+
+  /**
+    * Removes a user organization affiliation from the user_org table.
+    */
+  def remove(userId: UUID, orgId: Int): Int = db.withSession { implicit session =>
+    userOrgs.filter(r => r.userId === userId.toString && r.orgId === orgId).delete
+  }
+}
