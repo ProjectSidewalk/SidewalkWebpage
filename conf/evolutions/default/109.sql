6fae574b090684127c9b3020da4d128634c692a9
Mikey Saugstad <michaelssaugstad@gmail.com>
2021-03-31 15:48:41 -0700

    adds evolution to delete extra GSV tables

--- conf/evolutions/default/109.sql
+++ conf/evolutions/default/109.sql
@@ -16,12 +16,50 @@ UPDATE region
 
 DROP TABLE region_property;
 
+DROP TABLE gsv_location;
+
+DROP TABLE gsv_model;
+
+DROP TABLE gsv_projection;
+
 # --- !Downs
+CREATE TABLE gsv_projection (
+    gsv_panorama_id CHARACTER VARYING(2044) COLLATE pg_catalog."POSIX" NOT NULL,
+    projection_type CHARACTER VARYING(2044) COLLATE pg_catalog."POSIX" NOT NULL,
+    pano_yaw_deg DOUBLE PRECISION NOT NULL,
+    tilt_yaw_deg DOUBLE PRECISION NOT NULL,
+    tilt_pitch_deg DOUBLE PRECISION NOT NULL,
+    CONSTRAINT gsv_panorama_projection_pkey PRIMARY KEY (gsv_panorama_id)
+);
+
+CREATE TABLE gsv_model (
+    gsv_panorama_id CHARACTER VARYING(2044) COLLATE pg_catalog."POSIX" NOT NULL,
+    depth_map TEXT COLLATE pg_catalog."POSIX" NOT NULL,
+    pano_map TEXT COLLATE pg_catalog."POSIX" NOT NULL,
+    CONSTRAINT gsv_model_pkey PRIMARY KEY (gsv_panorama_id)
+);
+
+CREATE TABLE gsv_location (
+    gsv_panorama_id CHARACTER VARYING(2044) COLLATE pg_catalog."POSIX" NOT NULL,
+    zoom_levels INTEGER NOT NULL,
+    lat DOUBLE PRECISION NOT NULL,
+    lng DOUBLE PRECISION NOT NULL,
+    original_lat DOUBLE PRECISION NOT NULL,
+    original_lng DOUBLE PRECISION NOT NULL,
+    elevation_wgs84_m DOUBLE PRECISION NOT NULL,
+    description CHARACTER VARYING(2044) COLLATE pg_catalog."POSIX" NOT NULL,
+    street_range INTEGER,
+    region CHARACTER VARYING(2044) COLLATE pg_catalog."POSIX" NOT NULL,
+    country CHARACTER VARYING(2044) COLLATE pg_catalog."POSIX" NOT NULL,
+    elevation_egm96_m DOUBLE PRECISION NOT NULL,
+    CONSTRAINT gsv_location_pkey PRIMARY KEY (gsv_panorama_id)
+);
+
 CREATE TABLE region_property (
     region_property_id SERIAL NOT NULL,
-    region_id integer NOT NULL,
-    key text NOT NULL,
-    value text NOT NULL,
+    region_id INTEGER NOT NULL,
+    key TEXT NOT NULL,
+    value TEXT NOT NULL,
     PRIMARY KEY (region_property_id),
     FOREIGN KEY (region_id) REFERENCES region(region_id)
 );
@@ -30,7 +68,7 @@ SELECT region_id, 'Neighborhood Name', description FROM region;
 
 CREATE TABLE region_type (
     region_type_id SERIAL NOT NULL,
-    region_type character varying(2044) COLLATE pg_catalog."POSIX" NOT NULL,
+    region_type CHARACTER VARYING(2044) COLLATE pg_catalog."POSIX" NOT NULL,
     PRIMARY KEY (region_type_id)
 );
 INSERT INTO region_type (region_type) VALUES ('city');
