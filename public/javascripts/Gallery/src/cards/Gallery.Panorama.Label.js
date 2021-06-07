d9c146e56602fa839b29bf52b707ec97c4c92915
kumararoosh <arkumar@Aroosh-Computer.localdomain>
2021-06-06 22:20:47 -0700

    Added comments to Gallery.Panorama.Label.js

--- public/javascripts/Gallery/src/cards/Gallery.Panorama.Label.js
+++ public/javascripts/Gallery/src/cards/Gallery.Panorama.Label.js
@@ -1,38 +1,40 @@
 /**
  *
- * @param labelId
- * @param labelType
- * @param canvasX
- * @param canvasY
- * @param originalCanvasWidth
- * @param originalCanvasHeight
- * @param heading
- * @param pitch
- * @param zoom
- * @returns {{className: string}}
+ * An object that contains all of the information to render a label onto a GSVPanorama.
+ * Used in the GalleryPanorama.renderLabel() method
+ * 
+ * @param {String} labelId The ID of the label
+ * @param {String} labelType The type of the label
+ * @param {Number} canvasX The X position of the label on the canvas
+ * @param {Number} canvasY The Y position of the label on the canvas
+ * @param {Number} originalCanvasWidth The width of the canvas when the label was added
+ * @param {Number} originalCanvasHeight The height of the canvas when the label was added
+ * @param {Number} heading The heading of the GSV pano when the label was added in audit
+ * @param {Number} pitch The pitch of the GSV pano when the label was added in audit
+ * @param {Number} zoom The zoom of the GSV pano when the label was added in audit
+ * @returns {GalleryPanoramaLabel}
  * @constructor
  */
  function GalleryPanoramaLabel(labelId, labelType, canvasX, canvasY, originalCanvasWidth, originalCanvasHeight,
     heading, pitch, zoom) {
-    var self = { className: "AdminPanoramaLabel" };
+    var self = { className: "GalleryPanoramaLabel" };
 
     /**
-    * This function initializes the Panorama
-    */
+     * Initializes the instance variables to the values provided in the constructor
+     * 
+     */
     function _init () {
-    self.labelId = labelId;
-    self.label_type = labelType;
-    self.canvasX = canvasX;
-    self.canvasY = canvasY;
-    self.originalCanvasWidth = originalCanvasWidth;
-    self.originalCanvasHeight = originalCanvasHeight;
-    self.heading = heading;
-    self.pitch = pitch;
-    self.zoom = zoom;
-    return this;
+        self.labelId = labelId;
+        self.label_type = labelType;
+        self.canvasX = canvasX;
+        self.canvasY = canvasY;
+        self.originalCanvasWidth = originalCanvasWidth;
+        self.originalCanvasHeight = originalCanvasHeight;
+        self.heading = heading;
+        self.pitch = pitch;
+        self.zoom = zoom;
     }
 
-    //init
     _init();
 
     return self;
