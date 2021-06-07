9dee4bbe5f05d19f0143798d4b3e717e80ac5b05
kumararoosh <arkumar@Aroosh-Computer.localdomain>
2021-06-07 00:13:13 -0700

    Added comments to SeverityDisplay.js

--- public/javascripts/Gallery/src/displays/SeverityDisplay.js
+++ public/javascripts/Gallery/src/displays/SeverityDisplay.js
@@ -1,3 +1,12 @@
+
+/**
+ * An object that creates a display for severity based on the Mock for Sidewalk Gallery v1.1
+ * 
+ * @param {HTMLElement} container The DOM element that contains the display
+ * @param {Number} severity The severity to display
+ * @param {Boolean} isModal a toggle to determine if this SeverityDisplay is in a modal, or in a card
+ * @returns {SeverityDisplay} the generated object
+ */
 function SeverityDisplay(container, severity, isModal=false) {
     let self = this;
     self.severity = severity;
@@ -5,6 +14,7 @@ function SeverityDisplay(container, severity, isModal=false) {
 
     let circles = []
     function _init() {
+        // This logic sets the different classes and ids depending on whether the severity display is in a Modal or in a card
         let severityCircleClass = isModal ? 'modal-severity-circle' : 'severity-circle'
         let selectedCircleID = isModal ? 'modal-current-severity' : 'current-severity'
 
@@ -15,21 +25,22 @@ function SeverityDisplay(container, severity, isModal=false) {
         title.innerHTML = '<b>Severity:</b>'
         container.append(title)
 
+        // Creates all of the circles for the severities
         for (let i = 1; i <= 5; i++) {
             let severityCircle = document.createElement('div')
             severityCircle.className = severityCircleClass
             severityCircle.innerText = i;
             circles.push(severityCircle)
         }
+        // Highlight the correct severity
         if (severity) {
             $(circles[severity - 1]).attr('id', selectedCircleID)
         }
+        // Add all of the severity circles to the DOM
         for (let i = 0; i < circles.length; i++) {
             holder.appendChild(circles[i])
         }
         container.append(holder)
- 
-      
     }
 
     _init()
