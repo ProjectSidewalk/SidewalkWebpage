607f7e046476e9375542e6a00a5c9f360b7c7bb7
Mikey Saugstad <michaelssaugstad@gmail.com>
2021-08-03 18:52:44 -0700

    Gallery interactive GSV border changes color with validation

--- public/javascripts/Gallery/src/validation/ValidationMenu.js
+++ public/javascripts/Gallery/src/validation/ValidationMenu.js
@@ -18,7 +18,6 @@ function ValidationMenu(uiCardImage, cardProperties, onExpandedView) {
     let self = this;
 
     let currentCardProperties = cardProperties;
-
     let referenceCard = null;
 
     // A kind of wack way to do this, explore better options.
@@ -31,6 +30,11 @@ function ValidationMenu(uiCardImage, cardProperties, onExpandedView) {
         "Agree": "validate-agree",
         "Disagree": "validate-disagree",
         "NotSure": "validate-not-sure"
+    };
+    const validationOptionToColor = {
+        'Agree': '#78c9ab',
+        'Disagree': '#eb734d',
+        'NotSure': '#fbd78b'
     };
 
     let currSelected = null;
@@ -71,19 +75,19 @@ function ValidationMenu(uiCardImage, cardProperties, onExpandedView) {
 
         for (const [valKey, button] of Object.entries(validationButtons)) {
             button.click(function() {
-                _showValidated(classToValidationOption[valKey]);
+                showValidated(classToValidationOption[valKey]);
                 validateLabel(classToValidationOption[valKey]);
             });
         }
         // If the signed in user had already validated this label before loading the page, style the card to show that.
         if (currentCardProperties !== null && currentCardProperties.user_validation) {
-            _showValidated(currentCardProperties.user_validation);
+            showValidated(currentCardProperties.user_validation);
         }
         uiCardImage.append(overlay[0]);
     }
 
     // Sets the look of the card to show that the label has been validated.
-    function _showValidated(validationOption) {
+    function showValidated(validationOption) {
         const validationClass = validationOptionToClass[validationOption];
 
         // If the label had already been validated differently, remove the visual effects from the older validation.
@@ -96,7 +100,6 @@ function ValidationMenu(uiCardImage, cardProperties, onExpandedView) {
             } else {
                 validationButtons[currSelected].attr('class', 'modal-validation-button');
             }
-           
         }
 
         // Add the visual effects from the new validation.
@@ -108,9 +111,21 @@ function ValidationMenu(uiCardImage, cardProperties, onExpandedView) {
             galleryCard.classList.add(validationClass);
             validationButtons[validationClass].attr('class', 'validation-button-selected');
         } else {
-            referenceCard.validationMenu._showValidated(validationOption);
+            referenceCard.validationMenu.showValidated(validationOption);
             validationButtons[validationClass].attr('class', 'modal-validation-button-selected');
+            uiCardImage.css('border-color', validationOptionToColor[validationOption]);
+            uiCardImage.css('background-color', validationOptionToColor[validationOption]);
         }
+    }
+
+    /**
+     * Resets the border to be transparent and the buttons to be less opaque, indicating a lack of validation.
+     * @private
+     */
+    function _removeExpandedValidationVisuals() {
+        uiCardImage.css('border-color', 'transparent');
+        uiCardImage.css('background-color', 'transparent');
+        Object.values(validationButtons).forEach(valButton => valButton.attr('class', 'modal-validation-button'));
     }
 
     /**
@@ -120,8 +135,6 @@ function ValidationMenu(uiCardImage, cardProperties, onExpandedView) {
      * @private
      */
     function validateLabel(action) {
-        console.log("validate method called");
-
         // TODO: do we need this log?
         sg.tracker.push("Validate_MenuClick=" + action);
         let validationTimestamp = new Date().getTime();
@@ -151,7 +164,6 @@ function ValidationMenu(uiCardImage, cardProperties, onExpandedView) {
             data: JSON.stringify(data),
             dataType: 'json',
             success: function (result) {
-                showConfirmation(action);
             },
             error: function (result) {
                 console.error(result);
@@ -160,17 +172,7 @@ function ValidationMenu(uiCardImage, cardProperties, onExpandedView) {
     }
 
     /**
-     * Confirm successful submit of validation.
-     * TODO: Probably want to remove for prod or show confirmation through something else.
-     * 
-     * @param {*} action Validation result.
-     */
-    function showConfirmation(action) {
-        console.log(action + ": validation submitted successfully :)");
-    }
-
-    /**
-     * Updates the card properties neccesary for validation.
+     * Updates the card properties necessary for validation.
      * @param {*} newProperties The properties to update to.
      */
     function updateCardProperties(newProperties) {
@@ -185,13 +187,15 @@ function ValidationMenu(uiCardImage, cardProperties, onExpandedView) {
     function updateReferenceCard(newCard) {
         referenceCard = newCard;
         if (currentCardProperties !== null && currentCardProperties.user_validation) {
-            _showValidated(currentCardProperties.user_validation);
+            showValidated(currentCardProperties.user_validation);
+        } else if (onExpandedView) {
+            _removeExpandedValidationVisuals();
         }
     }
 
     self.updateCardProperties = updateCardProperties;
     self.updateReferenceCard = updateReferenceCard;
-    self._showValidated = _showValidated;
+    self.showValidated = showValidated;
     _init();
     return self;
 }
