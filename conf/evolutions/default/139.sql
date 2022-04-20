1cdd8a912e5dc99e06be2439b28731b177c3ee4b
Mikey Saugstad <michaelssaugstad@gmail.com>
2022-04-20 16:59:21 -0700

    renames missing friction strip to missing tactile warning in logs

--- conf/evolutions/default/139.sql
+++ conf/evolutions/default/139.sql
@@ -6,7 +6,23 @@ INSERT INTO tag (tag_id, label_type_id, tag) SELECT 55, label_type_id, 'tactile 
 -- Rename 'missing friction strip' to 'missing tactile warning'.
 UPDATE tag SET tag = 'missing tactile warning' WHERE tag = 'missing friction strip';
 
+-- Rename in logs as well. We check the `action` column in the `audit_task_interaction` table so we don't overwrite user-input descriptions.
+UPDATE gallery_task_interaction SET note = REPLACE(note, 'missing friction strip', 'missing tactile warning') WHERE note LIKE '%missing friction strip%';
+
+UPDATE audit_task_interaction
+SET note = REPLACE(note, 'missing friction strip', 'missing tactile warning')
+WHERE action IN ('ContextMenu_TagAdded', 'ContextMenu_TagRemoved', 'KeyboardShortcut_TagAdded', 'KeyboardShortcut_TagRemoved')
+    AND note LIKE '%missing friction strip%';
+
 # --- !Downs
+-- Rename 'missing tactile warning' to 'missing friction strip'. Including in logs.
+UPDATE audit_task_interaction
+SET note = REPLACE(note, 'missing tactile warning', 'missing friction strip')
+WHERE action IN ('ContextMenu_TagAdded', 'ContextMenu_TagRemoved', 'KeyboardShortcut_TagAdded', 'KeyboardShortcut_TagRemoved')
+    AND note LIKE '%missing friction strip%';
+
+UPDATE gallery_task_interaction SET note = REPLACE(note, 'missing tactile warning', 'missing friction strip') WHERE note LIKE '%missing friction strip%';
+
 UPDATE tag SET tag = 'missing friction strip' WHERE tag = 'missing tactile warning';
 
 -- Remove 'no pedestrian priority' tag for Crosswalk and 'tactile warning' tag for CurbRamp.
