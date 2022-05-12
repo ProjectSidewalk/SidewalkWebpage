d551860dd9b170eed535f29fb27733535998dc81
Mikey Saugstad <michaelssaugstad@gmail.com>
2022-05-12 13:05:54 -0700

    fixes old Gallery logs to standardize them

--- conf/evolutions/default/144.sql
+++ conf/evolutions/default/144.sql
@@ -7,6 +7,11 @@ ALTER TABLE gallery_task_interaction
     ADD COLUMN user_id TEXT,
     ADD CONSTRAINT gallery_task_interaction_user_id_fkey FOREIGN KEY (user_id) REFERENCES sidewalk_user(user_id);
 
+-- Standardize logs. We switched from Validate_MenuClick=<result> to Validate_MenuClick<result> awhile back.
+UPDATE gallery_task_interaction
+SET action = REPLACE(action, 'Validate_MenuClick=', 'Validate_MenuClick')
+WHERE action LIKE 'Validate_MenuClick=%';
+
 # --- !Downs
 ALTER TABLE gallery_task_interaction
     DROP CONSTRAINT IF EXISTS gallery_task_interaction_user_id_fkey,
