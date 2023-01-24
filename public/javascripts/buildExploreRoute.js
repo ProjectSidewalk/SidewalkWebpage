37489801604ac69904e1edef06688bd6a899d236
Mikey Saugstad <michaelssaugstad@gmail.com>
2023-01-23 16:13:06 -0800

    focuses new route builder map on current city

--- public/javascripts/buildExploreRoute.js
+++ public/javascripts/buildExploreRoute.js
@@ -2,7 +2,9 @@ $(document).ready(function () {
     mapboxgl.accessToken = 'pk.eyJ1IjoibWlzYXVnc3RhZCIsImEiOiJjajN2dTV2Mm0wMDFsMndvMXJiZWcydDRvIn0.IXE8rQNF--HikYDjccA7Ug';
     var map = new mapboxgl.Map({
         container: 'route-builder-map',
-        style: 'mapbox://styles/mapbox/streets-v11'
+        style: 'mapbox://styles/mapbox/streets-v11',
+        minZoom: 9,
+        maxZoom: 19
     });
     map.addControl(new mapboxgl.NavigationControl({ visualizePitch: true }), 'top-left');
 
@@ -22,24 +24,20 @@ $(document).ready(function () {
                 },
                 'paint': {
                     'line-color': '#888',
-                    'line-width': 8
+                    'line-width': 5
                 }
             });
         });
     })
 
 
-    // Get city-specific parameters for the maps.
+    // Get city-specific parameters for the map.
     $.getJSON('/cityMapParams', function(data) {
-        // Set view center and max bounds for each map.
-        // mapAccessAttributes.setView([data.attribute.center_lat, data.attribute.center_lng], data.attribute.zoom);
-        // mapAccessScoreStreets.setView([data.street.center_lat, data.street.center_lng], data.street.zoom);
-        // mapAccessScoreNeighborhoods.setView([data.region.center_lat, data.region.center_lng], data.region.zoom);
-        //
-        // var southWest = L.latLng(data.southwest_boundary.lat, data.southwest_boundary.lng);
-        // var northEast = L.latLng(data.northeast_boundary.lat, data.northeast_boundary.lng);
-        // mapAccessAttributes.setMaxBounds(L.latLngBounds(southWest, northEast));
-        // mapAccessScoreStreets.setMaxBounds(L.latLngBounds(southWest, northEast));
-        // mapAccessScoreNeighborhoods.setMaxBounds(L.latLngBounds(southWest, northEast));
+        map.setZoom(data.default_zoom - 1);
+        map.setCenter([data.city_center.lng, data.city_center.lat]);
+        map.setMaxBounds([
+            [data.southwest_boundary.lng, data.southwest_boundary.lat],
+            [data.northeast_boundary.lng, data.northeast_boundary.lat]
+        ]);
     });
 });
