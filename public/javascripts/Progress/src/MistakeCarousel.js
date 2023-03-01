07bf8fb991586c67122f47afc84077eb6dc140d3
dylanbun <56641275+dylanbun@users.noreply.github.com>
2023-03-01 12:12:35 -0800

    added comments and removed console.log

--- public/javascripts/Progress/src/MistakeCarousel.js
+++ public/javascripts/Progress/src/MistakeCarousel.js
@@ -13,24 +13,24 @@ function MistakeCarousel() {
         let labelTypesWithoutData = [];
         labelTypes.forEach((l) => (data[l].length > 0 ? labelTypesWithData : labelTypesWithoutData).push(l));
 
+        // This is the list of label types without validations
         const translatedTypes = labelTypesWithoutData.map((l) => i18next.t(`common:${util.camelToKebab(l)}`));
 
-        // Add subheader for labeling mistakes section, listing label types without validations.
+        // Get state of user (has no mistakes or has a mistake labeling)
         let subheader = '';
+        // User has no mistakes with their labels
         if (labelTypesWithoutData.length === labelTypes.length) {
             subheader = 'no-mistakes-subheader'
+        // Otherwise user has at least one mistake with their labels
         } else {
             subheader = 'mistakes-subheader'
         }
 
         let mistakesSubheader = document.getElementById(subheader);
-
-        console.log(mistakesSubheader)
-
-        console.log("here1")
-
         mistakesSubheader.textContent = i18next.t(subheader)
 
+        // If the user has made a mistake and they have not made a mistake of all types
+        // Append the text regarding what label types they have not made a mistake on
         if (subheader === 'mistakes-subheader' && labelTypesWithData.length !== labelTypes.length) {
             mistakesSubheader.textContent += " " + i18next.t('mistakes-info', { labelTypes: translatedTypes });
         }
