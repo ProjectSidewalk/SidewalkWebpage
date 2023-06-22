d09b45ccb5937fbbad1e8a4a1a93bdb6cdf367c1
dylanbun <56641275+dylanbun@users.noreply.github.com>
2023-06-22 12:19:30 -0700

    finished scala file and evolutions. need to delete previous get calls

--- app/models/attribute/Config.scala
+++ app/models/attribute/Config.scala
@@ -1,56 +1,62 @@
 package models.route
 
+import models.daos.slick.DBTableDefinitions.{DBUser, UserTable}
+import models.region.{Region, RegionTable}
+import models.utils.MyPostgresDriver.simple._
+import play.api.Play.current
+
 case class Config(openStatus: String, mapathonEventLink: String, cityCenterLat: Double, cityCenterLng: Double, southwestBoundLat: Double,
                   southwestBoundaryLng: Double, northeastBoundaryLat: Double, northeastBoundaryLng: Double, defaultMapZoom: Double,
-                  tutorialStreetEdgeID: Int, offsetHours: Int, excludedTags: String, apiAttributeCenterLat: Double, apiAttributeCenterLng: Double,
-                  apiAttributeZoom: Double, apiAttributeLatOne: Double, apiAttributeLngOne: Double, apiAttributeLatTwo: Double,
-                  apiAttributeLngTwo: Double, apiStreetCenterLat: Double, apiStreetCenterLng: Double, apiStreetZoom: Double,
-                  apiStreetLatOne: Double, apiStreetLngOne: Double, apiStreetLatTwo: Double, apiStreetLngTwo: Double,
-                  apiRegionCenterLat: Double, apiRegionCenterLng: Double, apiRegionZoom: Double, apiRegionLatOne: Double,
-                  apiRegionLngOne: Double, apiRegionLatTwo: Double, apRegionLngTwo: Double)
+                  tutorialStreetEdgeID: Int, offsetHours: Int, excludedTags: String)
 
-// what is a tag
-class ConfigTable(tag: Tag) extends Table[Config](tag, Some("sidewalk"), "config") {
-  def openStatus: Column[String] = column[String]("open_status", 0.NotNull)
-  def mapathonEventLink: Column[String] = column[String]("mapathon_event_link")
-  def cityCenterLat: Column[Double] = column[Double]("city_center_lat", 0.NotNull)
-  def cityCenterLng: Column[Double] = column[Double]("city_center_lng", 0.NotNull)
-  def southwestBoundaryLat: Column[Double] = column[Double]("southwest_boundary_lat", 0.NotNull)
-  def southwestBoundaryLng: Column[Double] = column[Double]("southwest_boundary_lng", 0.NotNull)
-  def northeastBoundaryLat: Column[Double] = column[Double]("northeast_boundary_lat", 0.NotNull)
-  def northeastBoundaryLng: Column[Double] = column[Double]("northeast_boundary_lng", 0.NotNull)
-  def defaultMapZoom: Column[Double] = column[Double]("default_map_zoom", 0.NotNull)
-  def tutorialStreetEdgeID: Column[Int] = column[Int]("tutorial_street_edge_id", 0.NotNull)
-  def offsetHours: Column[Int] = column[Int]("update_offset_hours", 0.NotNull)
-  def excludedTags: Column[String] = column[String]("excluded_tags", 0.NotNull)
-  def apiAttributeCenterLat: Column[Double] = column[Double]("api_attribute_center_lat", 0.NotNull)
-  def apiAttributeCenterLng: Column[Double] = column[Double]("api_attribute_center_lng", 0.NotNull)
-  def apiAttributeZoom: Column[Double] = column[Double]("api_attribute_zoom", 0.NotNull)
-  def apiAttributeLatOne: Column[Double] = column[Double]("api_attribute_lat1", 0.NotNull)
-  def apiAttributeLngOne: Column[Double] = column[Double]("api_attrubute_lng1", 0.NotNull)
-  def apiAttributeLatTwo: Column[Double] = column[Double]("api_attribute_lat2", 0.NotNull)
-  def apiAttributeLngTwo: Column[Double] = column[Double]("api_attrubute_lng2", 0.NotNull)
-  def apiStreetCenterLat: Column[Double] = column[Double]("api_street_center_lat", 0.NotNull)
-  def apiStreetCenterLng: Column[Double] = column[Double]("api_street_center_lng", 0.NotNull)
-  def apiStreetZoom: Column[Double] = column[Double]("api_street_zoom", 0.NotNull)
-  def apiStreetLatOne: Column[Double] = column[Double]("api_street_lat1", 0.NotNull)
-  def apiStreetLngOne: Column[Double] = column[Double]("api_street_lng1", 0.NotNull)
-  def apiStreetLatTwo: Column[Double] = column[Double]("api_street_lat2", 0.NotNull)
-  def apiStreetLngTwo: Column[Double] = column[Double]("api_street_lng2", 0.NotNull)
-  def apiRegionCenterLat: Column[Double] = column[Double]("api_region_center_lat", 0.NotNull)
-  def apiRegionCenterLng: Column[Double] = column[Double]("api_region_center_lng", 0.NotNull)
-  def apiRegionZoom: Column[Double] = column[Double]("api_region_zoom", 0.NotNull)
-  def apiRegionLatOne: Column[Double] = column[Double]("api_region_lat1", 0.NotNull)
-  def apiRegionLngOne: Column[Double] = column[Double]("api_region_lng1", 0.NotNull)
-  def apiRegionLatTwo: Column[Double] = column[Double]("api_region_lat2", 0.NotNull)
-  def apiRegionLngTwo: Column[Double] = column[Double]("api_region_lng2", 0.NotNull)
+case class ConfigApi(apiAttributeCenterLat: Double, apiAttributeCenterLng: Double,
+                     apiAttributeZoom: Double, apiAttributeLatOne: Double, apiAttributeLngOne: Double, apiAttributeLatTwo: Double,
+                     apiAttributeLngTwo: Double, apiStreetCenterLat: Double, apiStreetCenterLng: Double, apiStreetZoom: Double,
+                     apiStreetLatOne: Double, apiStreetLngOne: Double, apiStreetLatTwo: Double, apiStreetLngTwo: Double,
+                     apiRegionCenterLat: Double, apiRegionCenterLng: Double, apiRegionZoom: Double, apiRegionLatOne: Double,
+                     apiRegionLngOne: Double, apiRegionLatTwo: Double, apiRegionLngTwo: Double)
 
-  def * = (openStatus, mapathonEventLink, cityCenterLat, cityCenterLng, southwestBoundaryLat, southwestBoundaryLng, northeastBoundaryLat, northeastBoundaryLng, defaultMapZoom, tutorialStreetEdgeID, offsetHours, excludedTags, apiAttributeCenterLat, apiAttributeCenterLng, apiAttributeZoom, apiAttributeLatOne, apiAttributeLngOne, apiAttributeLatTwo, apiAttributeLngTwo, apiStreetCenterLat, apiStreetCenterLng, apiStreetZoom, apiStreetLatOne, apiStreetLngOne, apiStreetLatTwo, apiStreetLngTwo, apiRegionCenterLat, apiRegionCenterLng, apiRegionZoom, apiRegionLatOne, apiRegionLngOne, apiRegionLatTwo, apiRegionLngTwo) <> ((Version.apply _).tupled, Version.unapply)
+class ConfigApiTable(tag: slick.lifted.Tag) extends Table[ConfigApi](tag, Some("sidewalk"), "config_api") {
+//  def openStatus: Column[String] = column[String]("open_status", O.NotNull)
+//  def mapathonEventLink: Column[String] = column[String]("mapathon_event_link")
+//  def cityCenterLat: Column[Double] = column[Double]("city_center_lat", O.NotNull)
+//  def cityCenterLng: Column[Double] = column[Double]("city_center_lng", O.NotNull)
+//  def southwestBoundaryLat: Column[Double] = column[Double]("southwest_boundary_lat", O.NotNull)
+//  def southwestBoundaryLng: Column[Double] = column[Double]("southwest_boundary_lng", O.NotNull)
+//  def northeastBoundaryLat: Column[Double] = column[Double]("northeast_boundary_lat", O.NotNull)
+//  def northeastBoundaryLng: Column[Double] = column[Double]("northeast_boundary_lng", O.NotNull)
+//  def defaultMapZoom: Column[Double] = column[Double]("default_map_zoom", O.NotNull)
+//  def tutorialStreetEdgeID: Column[Int] = column[Int]("tutorial_street_edge_id", O.NotNull)
+//  def offsetHours: Column[Int] = column[Int]("update_offset_hours", O.NotNull)
+//  def excludedTags: Column[String] = column[String]("excluded_tags", O.NotNull)
+  def apiAttributeCenterLat: Column[Double] = column[Double]("api_attribute_center_lat", O.NotNull)
+  def apiAttributeCenterLng: Column[Double] = column[Double]("api_attribute_center_lng", O.NotNull)
+  def apiAttributeZoom: Column[Double] = column[Double]("api_attribute_zoom", O.NotNull)
+  def apiAttributeLatOne: Column[Double] = column[Double]("api_attribute_lat1", O.NotNull)
+  def apiAttributeLngOne: Column[Double] = column[Double]("api_attrubute_lng1", O.NotNull)
+  def apiAttributeLatTwo: Column[Double] = column[Double]("api_attribute_lat2", O.NotNull)
+  def apiAttributeLngTwo: Column[Double] = column[Double]("api_attrubute_lng2", O.NotNull)
+  def apiStreetCenterLat: Column[Double] = column[Double]("api_street_center_lat", O.NotNull)
+  def apiStreetCenterLng: Column[Double] = column[Double]("api_street_center_lng", O.NotNull)
+  def apiStreetZoom: Column[Double] = column[Double]("api_street_zoom", O.NotNull)
+  def apiStreetLatOne: Column[Double] = column[Double]("api_street_lat1", O.NotNull)
+  def apiStreetLngOne: Column[Double] = column[Double]("api_street_lng1", O.NotNull)
+  def apiStreetLatTwo: Column[Double] = column[Double]("api_street_lat2", O.NotNull)
+  def apiStreetLngTwo: Column[Double] = column[Double]("api_street_lng2", O.NotNull)
+  def apiRegionCenterLat: Column[Double] = column[Double]("api_region_center_lat", O.NotNull)
+  def apiRegionCenterLng: Column[Double] = column[Double]("api_region_center_lng", O.NotNull)
+  def apiRegionZoom: Column[Double] = column[Double]("api_region_zoom", O.NotNull)
+  def apiRegionLatOne: Column[Double] = column[Double]("api_region_lat1", O.NotNull)
+  def apiRegionLngOne: Column[Double] = column[Double]("api_region_lng1", O.NotNull)
+  def apiRegionLatTwo: Column[Double] = column[Double]("api_region_lat2", O.NotNull)
+  def apiRegionLngTwo: Column[Double] = column[Double]("api_region_lng2", O.NotNull)
+
+  def * = (apiAttributeCenterLat, apiAttributeCenterLng, apiAttributeZoom, apiAttributeLatOne, apiAttributeLngOne, apiAttributeLatTwo, apiAttributeLngTwo, apiStreetCenterLat, apiStreetCenterLng, apiStreetZoom, apiStreetLatOne, apiStreetLngOne, apiStreetLatTwo, apiStreetLngTwo, apiRegionCenterLat, apiRegionCenterLng, apiRegionZoom, apiRegionLatOne, apiRegionLngOne, apiRegionLatTwo, apiRegionLngTwo) <> ((ConfigApi.apply _).tupled, ConfigApi.unapply)
 }
 
 /**
  * Data access object for the config table.
  */
-object RouteTable {
+object ConfigTable {
   val db = play.api.db.slick.DB
 }
--- conf/evolutions/default/191.sql
+++ conf/evolutions/default/191.sql
@@ -36,6 +36,7 @@ CREATE TABLE config (
 );
 
 -- note: may need to add the current database indicator to these values
+-- also add test server?
 
 -- seattle
 INSERT INTO config VALUES ('partially', NULL , 47.615, -122.332, 47.400, -122.664, 47.850, -122.000, 11.75, 27645, 3, '["tactile warning" "garage entrance" "street vendor" "no pedestrian priority" "uncovered manhole" "level with sidewalk" "APS" "missing crosswalk" "painted sidewalk" "pedestrian arcade" "too close to traffic"]', 47.619, -122.300, 15.5, 47.615, -122.307, 47.623, -122.293, 47.618, -122.299, 16.0, 47.611, -122.309, 47.625, -122.289, 47.616, -122.296, 13.0, 47.600, -122.320, 47.636, -122.275);
@@ -84,6 +85,9 @@ INSERT INTO config VALUES('partially', NULL, -2.898, -79.005, -3.892, -80.008, -
 
 -- newberg
 INSERT INTO config VALUES('fully', NULL, 45.306, -122.958, 45.265, -123.010, 45.345, -122.900, 13.75, 1692, 4, '["tactile warning" "garage entrance" "street vendor" "no pedestrian priority" "uncovered manhole" "level with sidewalk" "APS" "missing crosswalk" "painted sidewalk" "pedestrian arcade" "too close to traffic"]', 45.319, -122.975, 16.0, 45.305, -123.000, 45.327, -122.960, 45.319, -122.975, 14.0, 45.310, -123.000, 45.327, -122.960, 45.319, -122.975, 13.0, 45.305, -123.010, 45.345, -122.950);
+
+-- crowdstudy
+INSERT INTO config VALUES ('partially', NULL , 47.615, -122.332, 47.400, -122.664, 47.850, -122.000, 11.75, 27645, 7, '["tactile warning" "garage entrance" "street vendor" "no pedestrian priority" "uncovered manhole" "level with sidewalk" "APS" "missing crosswalk" "painted sidewalk" "pedestrian arcade" "too close to traffic"]', 47.619, -122.300, 15.5, 47.615, -122.307, 47.623, -122.293, 47.618, -122.299, 16.0, 47.611, -122.309, 47.625, -122.289, 47.616, -122.296, 13.0, 47.600, -122.320, 47.636, -122.275);
 
 # --- !Downs
 DROP TABLE config;
