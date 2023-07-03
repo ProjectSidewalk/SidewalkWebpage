ccb27d348a83559d1be2cb0a6966c10e1b01ff41
dylanbun <56641275+dylanbun@users.noreply.github.com>
2023-07-03 14:31:52 -0700

    fixed spacing with ShapeFileCreatorHelper. third time is the charm?

--- app/controllers/helper/ShapefilesCreatorHelper.java
+++ app/controllers/helper/ShapefilesCreatorHelper.java
@@ -24,7 +24,7 @@ import controllers.StreetAttributeSignificance;
 
 /**
  * This class handles the creation of Shapefile archives to be used by the SidewalkAPIController.
- * 
+ *
  * Code was started and modified from the Geotools feature tutorial: 
  * https://docs.geotools.org/stable/tutorials/feature/csv2shp.html
  *
@@ -430,4 +430,4 @@ public class ShapefilesCreatorHelper {
 
         return new File(name + ".zip");
     }
-}
+} 
