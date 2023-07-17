9decb64167829ac36b5ed41240b622dfcc990b27
Ethan Y <theethan5753@gmail.com>
2023-07-17 12:07:24 -0700

    Finished off text edits for #2677

--- public/javascripts/SVValidate/src/modal/ModalNoNewMission.js
+++ public/javascripts/SVValidate/src/modal/ModalNoNewMission.js
@@ -27,7 +27,7 @@ function ModalNoNewMission (uiModalMission) {
         uiModalMission.missionTitle.html(i18next.t('mission-complete.no-new-mission-title'));
         uiModalMission.holder.css('visibility', 'visible');
         uiModalMission.foreground.css('visibility', 'visible');
-        let [city, shorthand] = $('#city-name-info').text().replace(/\s/g, "").split(","); // Get cit info from Scala
+        let [city, shorthand] = $('#city-name-info').text().replace(/\s/g, "").split(","); // Get city info from Scala
         uiModalMission.closeButton.html(i18next.t('mission-complete.no-new-mission-button') + ` ${city}, ${shorthand}`);
         uiModalMission.closeButton.on('click', _handleButtonClick);
     }
