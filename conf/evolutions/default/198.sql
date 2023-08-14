a1a30ecf115fb20be64863640be58f1853e2490e
Mikey Saugstad <michaelssaugstad@gmail.com>
2023-08-14 13:17:30 -0700

    adds missing delete from evolution Downs for new tag

--- conf/evolutions/default/198.sql
+++ conf/evolutions/default/198.sql
@@ -12,3 +12,9 @@ WHERE label_tag.tag_id = tag.tag_id
     AND tag.label_type_id = label_type.label_type_id
     AND label_type.label_type = 'Obstacle'
     AND tag.tag IN ('outdoor dining area');
+
+DELETE FROM tag
+    USING label_type
+WHERE tag.label_type_id = label_type.label_type_id
+  AND label_type.label_type = 'Obstacle'
+  AND tag.tag IN ('outdoor dining area');
