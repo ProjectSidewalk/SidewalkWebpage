6d8e5d9955acbde7a45ae78ba295cc75253ba786
dylanbun <56641275+dylanbun@users.noreply.github.com>
2023-08-17 16:00:28 -0700

    fixing 198.sql again

--- conf/evolutions/default/198.sql
+++ conf/evolutions/default/198.sql
@@ -9,9 +9,9 @@ UPDATE config SET excluded_tags = REPLACE(excluded_tags, ' "outdoor dining area"
 DELETE FROM label_tag
 USING tag, label_type
 WHERE label_tag.tag_id = tag.tag_id
-  AND tag.label_type_id = label_type.label_type_id
-  AND label_type.label_type = 'Obstacle'
-  AND tag.tag IN ('outdoor dining area');
+    AND tag.label_type_id = label_type.label_type_id
+    AND label_type.label_type = 'Obstacle'
+    AND tag.tag IN ('outdoor dining area');
 
 DELETE FROM tag
     USING label_type
