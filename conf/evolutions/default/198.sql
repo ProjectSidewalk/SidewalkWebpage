adfc3adff9f17f403c7a709db596d452333719c4
dylanbun <56641275+dylanbun@users.noreply.github.com>
2023-08-17 15:59:13 -0700

    spacing fix

--- conf/evolutions/default/198.sql
+++ conf/evolutions/default/198.sql
@@ -7,7 +7,7 @@ UPDATE config SET excluded_tags = REPLACE(excluded_tags, '"]', '" "outdoor dinin
 UPDATE config SET excluded_tags = REPLACE(excluded_tags, ' "outdoor dining area"', '');
 
 DELETE FROM label_tag
-    USING tag, label_type
+USING tag, label_type
 WHERE label_tag.tag_id = tag.tag_id
   AND tag.label_type_id = label_type.label_type_id
   AND label_type.label_type = 'Obstacle'
