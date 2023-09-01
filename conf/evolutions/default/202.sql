b4a4cb36098929b87558ce04a46749559f7afe1a
dylanbun <56641275+dylanbun@users.noreply.github.com>
2023-09-01 13:41:03 -0700

    redid source column. preexisitng mobile validations are marked

--- conf/evolutions/default/202.sql
+++ conf/evolutions/default/202.sql
@@ -1,7 +1,13 @@
 # --- !Ups
 ALTER TABLE label_validation ADD source TEXT;
+
+UPDATE label_validation SET source = 'ValidateMobile' WHERE is_mobile = TRUE;
+
 ALTER TABLE label_validation DROP COLUMN is_mobile;
 
 # --- !Downs
-ALTER TABLE label_validation DROP COLUMN source;
 ALTER TABLE label_validation ADD is_mobile BOOLEAN;
+
+UPDATE label_validation SET is_mobile = (source = 'ValidateMobile');
+
+ALTER TABLE label_validation DROP COLUMN source;
