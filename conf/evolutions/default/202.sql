47f4030a0c4bd670e68b53af6c44f343c0228150
dylanbun <56641275+dylanbun@users.noreply.github.com>
2023-09-12 12:40:43 -0700

    fixed column, added old data label for source column

--- conf/evolutions/default/202.sql
+++ conf/evolutions/default/202.sql
@@ -1,5 +1,5 @@
 # --- !Ups
-ALTER TABLE label_validation ADD source TEXT;
+ALTER TABLE label_validation ADD source TEXT NULL;
 
 UPDATE label_validation SET source = 'ValidateMobile' WHERE is_mobile = TRUE;
 
@@ -10,6 +10,13 @@ WHERE label_validation.mission_id = mission.mission_id
     AND mission.mission_type_id = mission_type.mission_type_id
     AND mission_type.mission_type = 'validation'
     AND is_mobile = FALSE;
+
+UPDATE label_validation
+SET source = 'Old data, unknown source'
+WHERE source IS NULL OR
+(source IS NOT NULL AND NOT (source = 'ValidateDesktop' or source = 'ValidateMobile'));
+
+ALTER TABLE label_validation ALTER COLUMN source SET NOT NULL;
 
 ALTER TABLE label_validation DROP COLUMN is_mobile;
 
