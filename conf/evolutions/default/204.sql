5fcd59fdfa2b3a4934c79b0e9154942421b4be7a
dylanbun <56641275+dylanbun@users.noreply.github.com>
2023-10-18 11:08:38 -0700

    apprently there are null values. setting them to -1 for now

--- conf/evolutions/default/204.sql
+++ conf/evolutions/default/204.sql
@@ -1,9 +1,11 @@
 # --- !Ups
-ALTER TABLE label ALTER COLUMN temporaryLabelId SET NOT NULL;
+UPDATE label SET temporary_label_id = -1 WHERE temporary_label_id IS NULL;
+ALTER TABLE label ALTER COLUMN temporary_label_id SET NOT NULL;
 
-ALTER TABLE AuditTaskInteractionTable ALTER COLUMN temporaryLabelId SET NOT NULL;
+UPDATE audit_task_interaction SET temporary_label_id = -1 WHERE temporary_label_id IS NULL;
+ALTER TABLE audit_task_interaction ALTER COLUMN temporary_label_id SET NOT NULL;
 
 # --- !Downs
-ALTER TABLE label ALTER COLUMN temporaryLabelId SET NULL;
+ALTER TABLE label ALTER COLUMN temporary_label_id SET NULL;
 
-ALTER TABLE audit_task_interaction ALTER COLUMN temporaryLabelId SET NULL;
+ALTER TABLE audit_task_interaction ALTER COLUMN temporary_label_id SET NULL;
