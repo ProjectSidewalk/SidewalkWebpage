28319def95b8a9399bbc0ba3013f734f9b3f6d5c
Mikey Saugstad <michaelssaugstad@gmail.com>
2023-10-23 13:39:12 -0700

    updates CSS For no new missions on Validate

--- public/javascripts/SVValidate/src/modal/ModalNoNewMission.js
+++ public/javascripts/SVValidate/src/modal/ModalNoNewMission.js
@@ -35,16 +35,19 @@ function ModalNoNewMission (uiModalMission) {
         uiModalMission.missionTitle.html(i18next.t('mission-complete.no-new-mission-title'));
         uiModalMission.holder.css('visibility', 'visible');
         uiModalMission.foreground.css('visibility', 'visible');
+
         // Update and widen the button to fit more text when there is no new mission.
-        uiModalMission.closeButton.css('font-size', '20pt');
-        uiModalMission.closeButton.css('width', '40%');
-        uiModalMission.closeButton.css('margin-right', '30%');
-        let buttonText = i18next.t('mission-complete.no-new-mission-button');
         if (isMobile()) {
-            buttonText = i18next.t('mobile.no-new-mission-button') + ` Seattle, WA`;
+            uiModalMission.closeButton.html(i18next.t('mobile.no-new-mission-button') + ` Seattle, WA`);
             uiModalMission.closeButton.css('font-size', '40pt');
+            uiModalMission.closeButton.css('width', '76%');
+            uiModalMission.closeButton.css('margin-right', '12%');
+        } else {
+            uiModalMission.closeButton.html(i18next.t('mission-complete.no-new-mission-button'));
+            uiModalMission.closeButton.css('font-size', '20pt');
+            uiModalMission.closeButton.css('width', '40%');
+            uiModalMission.closeButton.css('margin-right', '30%');
         }
-        uiModalMission.closeButton.html(buttonText);
         uiModalMission.closeButton.on('click', _handleButtonClick);
         uiModalMission.holder.css('display', '');
     }
