52152f380eb7b2b35642d4c16706eb0de57eff2e
dylanbun <56641275+dylanbun@users.noreply.github.com>
2023-11-27 10:26:13 -0800

    finally got it working?

--- conf/evolutions/default/206.sql
+++ conf/evolutions/default/206.sql
@@ -1,7 +1,7 @@
 # --- !Ups
-DELETE FROM webpage_activity WHERE user_id IS NULL OR user_is NOT IN (SELECT user_id FROM sidewalk_user);
+DELETE FROM webpage_activity WHERE user_id IS NULL OR user_id NOT IN (SELECT user_id FROM sidewalk_user);
 
-ALTER TABLE webpage_activity ADD CONSTRAINT fk_webpage_activity_user_id FOREIGN KEY (user_id) REFERENCES sidewalk_user (user_id);
+ALTER TABLE webpage_activity ADD CONSTRAINT webpage_activity_user_id_fkey FOREIGN KEY (user_id) REFERENCES sidewalk_user (user_id);
 
 # --- !Downs
-ALTER TABLE webpage_activity DROP CONSTRAINT fk_webpage_activity_user_id;
+ALTER TABLE webpage_activity DROP CONSTRAINT webpage_activity_user_id_fkey;
