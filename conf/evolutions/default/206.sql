0dea45b9fadb7a46968b22847af8d26720ae7d91
dylanbun <56641275+dylanbun@users.noreply.github.com>
2023-11-29 10:27:05 -0800

    mysql syntax != postgresql...

--- conf/evolutions/default/206.sql
+++ conf/evolutions/default/206.sql
@@ -1,5 +1,5 @@
 # --- !Ups
-DELETE FROM webpage_activity FROM webpage_activity LEFT JOIN sidewalk_user ON webpage_activity.user_id = sidewalk_user.user_id WHERE webpage_activity.user_id IS NULL OR sidewalk_user.user_id IS NULL;
+DELETE FROM webpage_activity USING sidewalk_user WHERE webpage_activity.user_id = sidewalk_user.user_id AND sidewalk_user.user_id IS NULL;
 
 ALTER TABLE webpage_activity ADD CONSTRAINT webpage_activity_user_id_fkey FOREIGN KEY (user_id) REFERENCES sidewalk_user (user_id);
 
