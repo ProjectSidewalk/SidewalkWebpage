8adfaa6888dbcf7286abe1495e243981b3d92075
Sanyam <sanyam.savla@somaiya.edu>
2024-01-01 19:28:48 -0800

    Fix: #3413 Changed Data type

---
+++ conf/evolutions/default/211.sql
@@ -0,0 +1,14 @@
+# --- !Ups
+
+ALTER TABLE osm_way_street_edge
+ALTER COLUMN osm_way_id TYPE BIGINT;
+
+# --- !Downs
+
+
+ALTER TABLE osm_way_street_edge ADD COLUMN osm_way_id_temp INT;
+UPDATE osm_way_street_edge SET osm_way_id_temp = CAST(LEFT(osm_way_id::TEXT, 9) AS INT);
+
+ALTER TABLE osm_way_street_edge DROP COLUMN osm_way_id;
+
+ALTER TABLE osm_way_street_edge RENAME COLUMN osm_way_id_temp TO osm_way_id;
