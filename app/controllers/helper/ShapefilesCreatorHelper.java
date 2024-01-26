b0d7314f78d458f16b12be6ffc6505e75075e9b3
SanyamSavla <sanyam.savla@somaiya.edu>
2024-01-25 16:51:33 -0800

    Fix: #3438 committing all changes!!

--- app/controllers/helper/ShapefilesCreatorHelper.java
+++ app/controllers/helper/ShapefilesCreatorHelper.java
@@ -66,17 +66,20 @@ public class ShapefilesCreatorHelper {
          *
          * Each data store has different limitations so check the resulting SimpleFeatureType.
          */
+        // Implementing Batch processing to send features in batchsize of 20k
+        final int batchSize = 20000;
         if (featureSource instanceof SimpleFeatureStore) {
             SimpleFeatureStore featureStore = (SimpleFeatureStore) featureSource;
-            /*
-             * SimpleFeatureStore has a method to add features from a
-             * SimpleFeatureCollection object, so we use the ListFeatureCollection
-             * class to wrap our list of features.
-             */
-            SimpleFeatureCollection collection = new ListFeatureCollection(TYPE, features);
-            featureStore.setTransaction(transaction);
             try {
-                featureStore.addFeatures(collection);
+
+                for (int i = 0; i < features.size(); i += batchSize) {
+                    int endIdx = Math.min(i + batchSize, features.size());
+                    List<SimpleFeature> batchFeatures = features.subList(i, endIdx);
+
+                    SimpleFeatureCollection collection = new ListFeatureCollection(TYPE, batchFeatures);
+                    featureStore.setTransaction(transaction);
+                    featureStore.addFeatures(collection);
+                }
                 transaction.commit();
             } catch (Exception problem) {
                 problem.printStackTrace();
