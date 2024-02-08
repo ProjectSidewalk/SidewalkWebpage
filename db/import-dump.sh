829fcb4ebe544db3c716a9e5ccf48542b0210213
Mikey Saugstad <michaelssaugstad@gmail.com>
2024-02-07 17:10:35 -0800

    can now import dumps for multiple cities in dev env

--- db/import-dump.sh
+++ db/import-dump.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
-psql -v ON_ERROR_STOP=1 -U postgres -d postgres <<-EOSQL
+psql -v ON_ERROR_STOP=1 -U postgres -d sidewalk <<-EOSQL
     SELECT pg_terminate_backend(pg_stat_activity.pid)
     FROM pg_stat_activity
-    WHERE (pg_stat_activity.datname = '$1')
+    WHERE (pg_stat_activity.datname = 'sidewalk')
     AND pid <> pg_backend_pid();
 
-    DROP DATABASE IF EXISTS "$1";
-    DROP SCHEMA IF EXISTS sidewalk;
+    DROP SCHEMA IF EXISTS $1 CASCADE;
+    DROP USER IF EXISTS $1;
 
-    CREATE DATABASE "$1" WITH OWNER=sidewalk TEMPLATE template0;
-    GRANT ALL PRIVILEGES ON DATABASE "$1" to sidewalk;
-
-    ALTER USER sidewalk SUPERUSER;
-    GRANT ALL PRIVILEGES ON DATABASE "$1" TO sidewalk;
-
-    CREATE SCHEMA sidewalk;
-    GRANT ALL ON ALL TABLES IN SCHEMA sidewalk TO sidewalk;
-    ALTER DEFAULT PRIVILEGES IN SCHEMA sidewalk GRANT ALL ON TABLES TO sidewalk;
-    ALTER DEFAULT PRIVILEGES IN SCHEMA sidewalk GRANT ALL ON SEQUENCES TO sidewalk;
+    CREATE USER $1;
+    GRANT sidewalk TO $1;
 EOSQL
 
-pg_restore -U sidewalk -d $1 /opt/$1-dump
+pg_restore -U sidewalk -Ft -d sidewalk /opt/$1-dump
+
+# Set the schema search path for the user so that it points to the schema for the correct city.
+psql -v ON_ERROR_STOP=1 -U postgres -d sidewalk <<-EOSQL
+    ALTER ROLE $1 SET search_path = public, $1;
+EOSQL
