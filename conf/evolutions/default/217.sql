95dddfd371fa139cb067b3752b908d4976beacd3
Mikey Saugstad <michaelssaugstad@gmail.com>
2024-02-26 11:02:35 -0800

    hides button waist height tag for Ped Signal

--- conf/evolutions/default/217.sql
+++ conf/evolutions/default/217.sql
@@ -4,7 +4,12 @@ INSERT INTO tag (tag_id, label_type_id, tag) SELECT 67, label_type_id, 'hard to 
 -- Add "APS" tag back to Chicago server by removing from list of excluded tags.
 UPDATE config SET excluded_tags = REPLACE(excluded_tags, ' "APS"', '') WHERE CURRENT_SCHEMA = 'sidewalk_chicago';
 
+-- Hide "button waist height" tag everyone, since it is being replaced by "hard to reach button".
+UPDATE config SET excluded_tags = REPLACE(excluded_tags, '"]', '" "button waist height"]');
+
 # --- !Downs
+UPDATE config SET excluded_tags = REPLACE(excluded_tags, ' "button waist height"', '');
+
 UPDATE config SET excluded_tags = REPLACE(excluded_tags, '"]', '" "APS"]') WHERE CURRENT_SCHEMA = 'sidewalk_chicago';
 
 DELETE FROM label_tag
