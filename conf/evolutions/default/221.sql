a336ce29d4fad3f7cf3ac9237fbc372e1f9bf713
Srihari Krishnaswamy <sriharik844@gmail.com>
2024-05-02 12:21:16 -0700

    everything should work now fingers crossed

--- conf/evolutions/default/221.sql
+++ conf/evolutions/default/221.sql
@@ -1,4 +1,5 @@
 # --- !Ups
+ALTER TABLE gsv_data ADD pano_history_saved TIMESTAMP;
 CREATE TABLE pano_history (
    pano_id TEXT NOT NULL,
    capture_date TEXT NOT NULL,
@@ -6,9 +7,8 @@ CREATE TABLE pano_history (
    FOREIGN KEY (location_current_pano_id) REFERENCES gsv_data(gsv_panorama_id)
 );
 
-ALTER TABLE gsv_data ADD pano_history_saved TIMESTAMP;
-ALTER TABLE pano_history OWNER TO sidewalk;
+-- ALTER TABLE pano_history OWNER TO sidewalk;
 
 # --- !Downs
-DROP TABLE pano_history;
 ALTER TABLE gsv_data DROP pano_history_saved;
+DROP TABLE pano_history;
