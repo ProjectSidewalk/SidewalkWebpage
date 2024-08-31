2d344a8357e99933ab5f5bfeb2f6e35beb15686a
jsomeara <johnomeara40@gmail.com>
2024-08-31 15:22:52 -0700

    Add speed limit to validate page

--- Gruntfile.js
+++ Gruntfile.js
@@ -78,6 +78,7 @@ module.exports = function(grunt) {
                     'public/javascripts/common/Panomarker.js',
                     'public/javascripts/common/UtilitiesSidewalk.js',
                     'public/javascripts/common/GSVInfoPopover.js',
+                    'public/javascripts/common/SpeedLimit.js',
                     'public/javascripts/common/MissionStartTutorial.js'
                 ],
                 dest: 'public/javascripts/SVValidate/build/SVValidate.js'
