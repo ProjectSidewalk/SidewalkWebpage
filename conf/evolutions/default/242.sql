45645289fc1d29d7b5d0b2242c4989854a390bf1
jsomeara <johnomeara40@gmail.com>
2024-09-02 12:50:52 -0700

    Minor changes to evolution 242

--- conf/evolutions/default/242.sql
+++ conf/evolutions/default/242.sql
@@ -1,18 +1,4 @@
 # --- !Ups
-DELETE FROM label_validation
-WHERE label_validation_id IN (
-  SELECT label_validation_id
-  FROM (
-    SELECT 
-      label_validation_id,
-      ROW_NUMBER() OVER (
-        PARTITION BY label_id, user_id 
-        ORDER BY end_timestamp DESC
-      ) AS validation_rank
-    FROM label_validation
-  )
-  WHERE validation_rank > 1
-);
 
 /*
 This evolution addresses an issue where multiple validations could be created per label-user pair.
@@ -25,8 +11,23 @@ We found no instances on prod where a user validated through the New Validate Be
 their validation elsewhere, so we don't have to worry about the label_history or label table.
 */
 
+-- Delete duplicate validations, keeping only the most recent one.
+DELETE FROM label_validation
+WHERE label_validation_id IN (
+    SELECT label_validation_id
+    FROM (
+        SELECT 
+            label_validation_id,
+            ROW_NUMBER() OVER (
+                PARTITION BY label_id, user_id 
+                ORDER BY end_timestamp DESC
+            ) AS validation_rank
+        FROM label_validation
+    )
+    WHERE validation_rank > 1
+);
+
 -- Add a unique constraint to ensure user_id and label_id are unique together.
-ALTER TABLE label_validation DROP CONSTRAINT IF EXISTS label_validation_user_id_label_id_unique;
 ALTER TABLE label_validation
 ADD CONSTRAINT label_validation_user_id_label_id_unique
 UNIQUE (user_id, label_id);
