d2b385833806a49404e4292c18b6044a3a77d64d
jsomeara <johnomeara40@gmail.com>
2024-09-13 19:41:23 -0700

    auto resize: Passthrough iframe url changes to parent document

--- public/javascripts/common/wrapperFrame.js
+++ public/javascripts/common/wrapperFrame.js
@@ -1,4 +1,22 @@
 const iframe = document.getElementById('wrapperFrame');
+const testUserBanner = document.querySelector('.test-server-banner');
+
+function iframeURLChange(callback) {
+    var unloadHandler = function () {
+        iframe.style.display = "none";
+        setTimeout(() => {
+            callback(iframe.contentWindow.location.href);
+        }, 0)
+    };
+
+    function attachUnload() {
+        iframe.contentWindow.removeEventListener("unload", unloadHandler);
+        iframe.contentWindow.addEventListener("unload", unloadHandler);
+    }
+
+    iframe.addEventListener("load", attachUnload);
+    attachUnload();
+}
 
 function scaleIframeContent() {
     iframe.contentWindow.devicePixelRatio = 1;
@@ -7,7 +25,7 @@ function scaleIframeContent() {
 
     if (contentElement) {
         const iframeWidth = window.innerWidth;
-        const iframeHeight = window.innerHeight - 70;
+        const iframeHeight = window.innerHeight - 70 - (testUserBanner ? testUserBanner.clientHeight : 0);
         const contentWidth = contentElement.clientWidth;
         const contentHeight = contentElement.clientHeight + 100; // Add 100px for padding purposes.
 
@@ -23,3 +41,13 @@ iframe.addEventListener('load', scaleIframeContent);
 window.addEventListener('resize', scaleIframeContent);
 
 iframe.src = window.location.href;
+const firstLoadEventListener = iframe.addEventListener("load", function() {
+    iframeURLChange(function (newURL) {
+        window.location.href = newURL;
+    });
+    iframe.removeEventListener("load", firstLoadEventListener)
+});
+
+setInterval(() => {
+    window.scrollTo(0, 0);
+}, 100)
