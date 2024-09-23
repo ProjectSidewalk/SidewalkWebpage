4cb912d634161010f4b7a2ee687897a16117e923
Mikey Saugstad <michaelssaugstad@gmail.com>
2024-09-23 13:58:33 -0700

    cleans up evolutions for unified login

--- conf/evolutions/default/250.sql
+++ conf/evolutions/default/250.sql
@@ -1,9 +1,8 @@
--- Create tables and indexes for login tables and grant "sidewalk" user access
+-- Create tables and indexes for login tables and grant "sidewalk" user access.
 
 # --- !Ups
-
 -- TABLE: user_password_info
-CREATE TABLE sidewalk_login.user_password_info (
+CREATE TABLE IF NOT EXISTS sidewalk_login.user_password_info (
     user_password_info_id SERIAL NOT NULL,
     login_info_id bigint NOT NULL,
     password VARCHAR(254) NOT NULL,
@@ -16,7 +15,7 @@ GRANT ALL ON TABLE sidewalk_login.user_password_info TO sidewalk;
 GRANT ALL ON TABLE sidewalk_login.user_password_info TO postgres;
 
 -- TABLE: login_info
-CREATE TABLE sidewalk_login.login_info(
+CREATE TABLE IF NOT EXISTS sidewalk_login.login_info(
     login_info_id BIGSERIAL NOT NULL,
     provider_id VARCHAR(254),
     provider_key VARCHAR(254),
@@ -27,46 +26,46 @@ GRANT ALL ON TABLE sidewalk_login.login_info TO sidewalk;
 GRANT ALL ON TABLE sidewalk_login.login_info TO postgres;
 
 -- TABLE: user_login_info
-CREATE TABLE sidewalk_login.user_login_info(
+CREATE TABLE IF NOT EXISTS sidewalk_login.user_login_info(
     user_login_info_id SERIAL NOT NULL,
     login_info_id bigint NOT NULL,
     user_id VARCHAR(254) NOT NULL,
     PRIMARY KEY (user_login_info_id)
 );
-CREATE INDEX user_login_info_login_info_id_idx ON sidewalk_login.user_login_info USING btree(login_info_id);
-CREATE INDEX user_login_info_user_id_idx ON sidewalk_login.user_login_info USING btree(user_id);
+CREATE INDEX IF NOT EXISTS user_login_info_login_info_id_idx ON sidewalk_login.user_login_info USING btree(login_info_id);
+CREATE INDEX IF NOT EXISTS user_login_info_user_id_idx ON sidewalk_login.user_login_info USING btree(user_id);
 ALTER TABLE sidewalk_login.user_login_info OWNER TO sidewalk;
 GRANT ALL ON TABLE sidewalk_login.user_login_info TO sidewalk;
 GRANT ALL ON TABLE sidewalk_login.user_login_info TO postgres;
 
 -- TABLE: sidewalk_user
-CREATE TABLE sidewalk_login.sidewalk_user(
+CREATE TABLE IF NOT EXISTS sidewalk_login.sidewalk_user(
     user_id TEXT UNIQUE NOT NULL,
     username TEXT UNIQUE NOT NULL,
     email TEXT NOT NULL,
     PRIMARY KEY (user_id)
 );
-CREATE INDEX user_id_idx ON sidewalk_login.sidewalk_user USING btree(user_id);
-CREATE INDEX username_idx ON sidewalk_login.sidewalk_user USING btree(username);
-CREATE INDEX email_idx ON sidewalk_login.sidewalk_user USING btree(email);
+CREATE INDEX IF NOT EXISTS user_id_idx ON sidewalk_login.sidewalk_user USING btree(user_id);
+CREATE INDEX IF NOT EXISTS username_idx ON sidewalk_login.sidewalk_user USING btree(username);
+CREATE INDEX IF NOT EXISTS email_idx ON sidewalk_login.sidewalk_user USING btree(email);
 ALTER TABLE sidewalk_login.sidewalk_user OWNER TO sidewalk;
 GRANT ALL ON TABLE sidewalk_login.sidewalk_user TO sidewalk;
 GRANT ALL ON TABLE sidewalk_login.sidewalk_user TO postgres;
 
 # --- !Downs
--- TABLE: user_password_info
-DROP TABLE IF EXISTS sidewalk_login.user_password_info;
+-- TABLE: sidewalk_user
+DROP INDEX IF EXISTS email_idx;
+DROP INDEX IF EXISTS username_idx;
+DROP INDEX IF EXISTS user_id_idx;
+DROP TABLE IF EXISTS sidewalk_login.sidewalk_user;
+
+-- TABLE: user_login_info
+DROP INDEX IF EXISTS user_login_info_user_id_idx;
+DROP INDEX IF EXISTS user_login_info_login_info_id_idx;
+DROP TABLE IF EXISTS sidewalk_login.user_login_info;
 
 -- TABLE: login_info
 DROP TABLE IF EXISTS sidewalk_login.login_info;
 
--- TABLE: user_login_info
-DROP TABLE IF EXISTS sidewalk_login.user_login_info;
-DROP INDEX IF EXISTS user_login_info_login_info_id_idx;
-DROP INDEX IF EXISTS user_login_info_user_id_idx;
-
--- TABLE: sidewalk_user
-DROP TABLE IF EXISTS sidewalk_login.sidewalk_user;
-DROP INDEX IF EXISTS user_id_idx;
-DROP INDEX IF EXISTS username_idx;
-DROP INDEX IF EXISTS email_idx;
+-- TABLE: user_password_info
+DROP TABLE IF EXISTS sidewalk_login.user_password_info;
--- conf/evolutions/default/251.sql
+++ conf/evolutions/default/251.sql
@@ -1,17 +1,24 @@
--- Move data from city schemas to new login data schema
+-- Move data from city schemas to new login data schema.
 -- TODO: Copy login data from ALL cities to login schema
 -- TODO: Among duplicates across cities, keep data from most recently logged in user.
 -- TODO: Update pkey sequence to the end of the data's sequence
 
--- Copy all login info from Pittsburgh into the new sidewalk_login schema
+# --- !Ups
+-- Copy all login info from Seattle into the new sidewalk_login schema
 INSERT INTO sidewalk_login.sidewalk_user(user_id, username, email)
-SELECT user_id, username, email FROM sidewalk_pittsburgh.sidewalk_user;
+SELECT user_id, username, email FROM sidewalk_seattle.sidewalk_user;
 
 INSERT INTO sidewalk_login.login_info(login_info_id, provider_id, provider_key)
-SELECT login_info_id, provider_id, provider_key FROM sidewalk_pittsburgh.login_info;
+SELECT login_info_id, provider_id, provider_key FROM sidewalk_seattle.login_info;
 
 INSERT INTO sidewalk_login.user_login_info(user_login_info_id, user_id, login_info_id)
-SELECT user_login_info_id, user_id, login_info_id FROM sidewalk_pittsburgh.user_login_info;
+SELECT user_login_info_id, user_id, login_info_id FROM sidewalk_seattle.user_login_info;
 
 INSERT INTO sidewalk_login.user_password_info (user_password_info_id, login_info_id, "password", salt, hasher)
-SELECT user_password_info_id, login_info_id, "password", salt, hasher FROM sidewalk_pittsburgh.user_password_info;
+SELECT user_password_info_id, login_info_id, "password", salt, hasher FROM sidewalk_seattle.user_password_info;
+
+# --- !Downs
+TRUNCATE sidewalk_login.user_password_info;
+TRUNCATE sidewalk_login.user_login_info;
+TRUNCATE sidewalk_login.login_info;
+TRUNCATE sidewalk_login.sidewalk_user;
