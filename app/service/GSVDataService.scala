023905eefa8c746fa74c5f852f90e08a534a0558
Mikey Saugstad <michaelssaugstad@gmail.com>
2025-01-16 13:14:35 -0700

    includes missing file for making Gallery work with Play 2.4

---
+++ app/service/GSVDataService.scala
@@ -0,0 +1,136 @@
+package service
+
+import scala.concurrent.{ExecutionContext, Future}
+import javax.inject._
+import com.google.inject.ImplementedBy
+import models.gsv.GSVDataTable
+import models.label.LabelPointTable
+import play.api.Configuration
+import play.api.libs.json.Json
+import play.api.libs.ws.WSClient
+import service.utils.ConfigService
+
+import java.io.IOException
+import java.net.{SocketTimeoutException, URL}
+import java.sql.Timestamp
+import java.time.Instant
+import java.util.Base64
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
+
+@ImplementedBy(classOf[GSVDataServiceImpl])
+trait GSVDataService {
+  def panoExists(gsvPanoId: String): Future[Option[Boolean]]
+  def getImageUrl(gsvPanoramaId: String, heading: Float, pitch: Float, zoom: Int): String
+}
+
+@Singleton
+class GSVDataServiceImpl @Inject()(
+                                    config: Configuration,
+                                    ws: WSClient,
+                                    implicit val ec: ExecutionContext,
+                                    configService: ConfigService,
+                                    gsvDataTable: GSVDataTable
+                                 ) extends GSVDataService {
+  //  import driver.api._
+
+  // Grab secret from ENV variable.
+  val secretKeyString: String = config.getString("google-maps-secret").get
+
+  // Decode secret key as Byte[].
+  val secretKey: Array[Byte] = Base64.getDecoder().decode(secretKeyString.replace('-', '+').replace('_', '/'))
+
+  // Get an HMAC-SHA1 signing key from the raw key bytes.
+  val sha1Key: SecretKeySpec = new SecretKeySpec(secretKey, "HmacSHA1")
+
+  /**
+   * Checks if the panorama associated with a label exists by pinging Google Maps.
+   *
+   * @param gsvPanoId  Panorama ID
+   * @return           True if the panorama exists, false otherwise
+   */
+  def panoExists(gsvPanoId: String): Future[Option[Boolean]] = {
+    val url = s"https://maps.googleapis.com/maps/api/streetview/metadata?pano=$gsvPanoId&key=${config.getString("google-maps-api-key").get}"
+    val signedUrl = signUrl(url)
+
+    ws.url(signedUrl)
+      .withRequestTimeout(5000)
+      .get()
+      .map { response =>
+        val imageStatus = (Json.parse(response.body) \ "status").as[String]
+        val imageExists = imageStatus == "OK"
+
+        // Mark the expired status, last_checked, and last_viewed columns in the db.
+        val timestamp = new Timestamp(Instant.now.toEpochMilli)
+        gsvDataTable.updateExpiredStatus(gsvPanoId, !imageExists, timestamp)
+
+        Some(imageExists)
+      }
+      .recover { // If there was an exception, don't assume it means a lack of GSV imagery.
+        case _: SocketTimeoutException => None
+        case _: IOException => None
+        case _: Exception => None
+      }
+  }
+
+  /**
+   * Signs a Google Maps request using a signing secret.
+   * https://developers.google.com/maps/documentation/maps-static/get-api-key#dig-sig-manual
+   */
+  def signUrl(urlString: String): String = {
+    // Convert to Java URL for easy parsing of URL parts.
+    val url: URL = new URL(urlString)
+
+    // Gets everything but URL protocol and host that we want to sign.
+    val resource: String = url.getPath() + '?' + url.getQuery()
+
+    // Get an HMAC-SHA1 Mac instance and initialize it with the HMAC-SHA1 key.
+    val mac: Mac = Mac.getInstance("HmacSHA1")
+    mac.init(sha1Key)
+
+    // Compute the binary signature for the request.
+    val sigBytes: Array[Byte] = mac.doFinal(resource.getBytes())
+
+    // Base 64 encode the binary signature and convert the signature to 'web safe' base 64.
+    val signature: String = Base64.getEncoder().encodeToString(sigBytes).replace('+', '-').replace('/', '_')
+
+    // Return signed url.
+    urlString + "&signature=" + signature
+  }
+
+  /**
+   * Retrieves the static image of the label panorama from the Google Street View Static API.
+   * Note that this returns the image of the panorama, but doesn't actually include the label.
+   * More information here: https://developers.google.com/maps/documentation/streetview/intro
+   *
+   * @param gsvPanoramaId Id of gsv pano.
+   * @param heading Compass heading of the camera.
+   * @param pitch Up or down angle of the camera relative to the Street View vehicle.
+   * @param zoom Zoom level of the canvas (for fov calculation).
+   * @return Image URL that represents the background of the label.
+   */
+  def getImageUrl(gsvPanoramaId: String, heading: Float, pitch: Float, zoom: Int): String = {
+    val url = "https://maps.googleapis.com/maps/api/streetview?" +
+      "pano=" + gsvPanoramaId +
+      "&size=" + LabelPointTable.canvasWidth + "x" + LabelPointTable.canvasHeight +
+      "&heading=" + heading +
+      "&pitch=" + pitch +
+      "&fov=" + getFov(zoom) +
+      "&key=" + config.getString("google-maps-api-key").get
+    signUrl(url)
+  }
+
+  /**
+   * Hacky fix to generate the FOV for an image.
+   * Determined experimentally.
+   * @param zoom Zoom level of the canvas (for fov calculation).
+   * @return FOV of image
+   */
+  def getFov(zoom: Int): Double = {
+    if (zoom <= 2) {
+      126.5 - zoom * 36.75
+    } else {
+      195.93 / scala.math.pow(1.92, zoom * 1.0)
+    }
+  }
+}
