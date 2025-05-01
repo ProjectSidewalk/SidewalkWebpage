6a5b01fe2653ffe2818ea2fb24847b010542797f
Mikey Saugstad <michaelssaugstad@gmail.com>
2025-05-01 12:44:06 -0700

    updates the db to have user_id as the primary key in auth_tokens table

--- conf/evolutions/default/269.sql
+++ conf/evolutions/default/269.sql
@@ -1,7 +1,11 @@
 # --- !Ups
+ALTER TABLE auth_tokens ADD PRIMARY KEY (user_id);
+
 CREATE INDEX idx_street_edge_geom ON street_edge USING GIST(geom);
 CREATE INDEX idx_region_geom ON region USING GIST(geom);
 
 # --- !Downs
 DROP INDEX idx_region_geom;
 DROP INDEX idx_street_edge_geom;
+
+ALTER TABLE auth_tokens DROP CONSTRAINT auth_tokens_pkey;
