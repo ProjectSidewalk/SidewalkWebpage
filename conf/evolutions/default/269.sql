002714d00c47595d523f752431a6fc4414bc6c3d
Mikey Saugstad <michaelssaugstad@gmail.com>
2025-05-27 14:33:52 -0700

    fixes broken database evolution with semicolon in string

--- conf/evolutions/default/269.sql
+++ conf/evolutions/default/269.sql
@@ -1,5 +1,5 @@
 # --- !Ups
-INSERT INTO version VALUES ('8.1.12', now(), 'Adds deployments in Madison, WI; Niagara Falls, NY; Chandigarh, India; and Tainan City, Taiwan');
+INSERT INTO version VALUES ('8.1.12', now(), 'Adds deployments in Madison,  Niagara Falls, Chandigarh, and Tainan City');
 
 # --- !Downs
 DELETE FROM version WHERE version_id = '8.1.12';
