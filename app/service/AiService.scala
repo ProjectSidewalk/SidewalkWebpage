ff7e4dd3f40c4c7fc695bd1d2007a0be396a832f
Mikey Saugstad <michaelssaugstad@gmail.com>
2025-07-30 16:19:55 -0700

    adds mission_type for AI validation, improves logic for using AI API results

--- app/service/AiService.scala
+++ app/service/AiService.scala
@@ -5,6 +5,7 @@ import models.label.{LabelAi, LabelDataForAi, LabelPointTable, LabelTypeEnum}
 import models.user.SidewalkUserTable
 import models.utils._
 import models.validation.LabelValidation
+import play.api.cache.AsyncCacheApi
 import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
 import play.api.libs.json.JsValue
 import play.api.libs.ws.WSClient
@@ -16,7 +17,7 @@ import scala.concurrent.{ExecutionContext, Future}
 
 @ImplementedBy(classOf[AiServiceImpl])
 trait AiService {
-  def validateLabelsWithAi(labelIds: Seq[Int]): Future[Seq[Option[LabelAi]]]
+  def validateLabelsWithAi(labelIds: Seq[Int]): Future[Seq[LabelAi]]
 }
 
 @Singleton
@@ -24,9 +25,11 @@ class AiServiceImpl @Inject() (
     protected val dbConfigProvider: DatabaseConfigProvider,
     config: Configuration,
     ws: WSClient,
+    cacheApi: AsyncCacheApi,
     labelTable: models.label.LabelTable,
     validationService: ValidationService,
-    labelAiTable: models.label.LabelAiTable
+    labelAiTable: models.label.LabelAiTable,
+    missionTable: models.mission.MissionTable
 )(implicit val ec: ExecutionContext)
     extends AiService
     with HasDatabaseConfigProvider[MyPostgresProfile] {
@@ -36,51 +39,61 @@ class AiServiceImpl @Inject() (
   private val AI_TAG_SUGGESTIONS_ON: Boolean = config.get[Boolean](s"city-params.ai-tag-suggestions-enabled.$cityId")
   private val AI_VALIDATION_MIN_ACCURACY: Double = config.get[Double](s"city-params.ai-validation-min-accuracy.$cityId")
 
-  def validateLabelsWithAi(labelIds: Seq[Int]): Future[Seq[Option[LabelAi]]] = {
+  /**
+   * Validates labels using AI by fetching label metadata, calling the AI API, and saving results.
+   * @param labelIds A sequence of label_ids to validate
+   * @return A Future containing a sequence of LabelAi objects with validation results and tags
+   */
+  def validateLabelsWithAi(labelIds: Seq[Int]): Future[Seq[LabelAi]] = {
     if (AI_VALIDATIONS_ON || AI_TAG_SUGGESTIONS_ON) {
       val startTime = OffsetDateTime.now
       Future.sequence {
         labelIds.map { labelId =>
-          // Fetch label metadata from the database.
-          val labelMetadataFuture: Future[Option[LabelDataForAi]] = db.run(labelTable.getLabelDataForAi(labelId))
 
-          labelMetadataFuture
+          // Fetch label metadata from the database.
+          db.run(labelTable.getLabelDataForAi(labelId))
             .flatMap {
+              // Call the AI API to process the panorama data and validate labels.
               case Some(labelData) =>
-                // Call the AI API to process the panorama data and validate labels.
-                callAIAPI(labelData).map(_.map(aiResults => (aiResults, labelData)))
+                callAiApi(labelData).map(_.map(aiResults => (aiResults, labelData)))
               case None =>
                 logger.warn(s"Info for label with ID $labelId not found.")
                 Future.failed(new Exception(s"Label with ID $labelId not found."))
             }
-            .map(_.map { case (aiResults, labelData) =>
-              println(aiResults)
-              val aiValidation: Option[Int] =
-                if (aiResults.validationAccuracy >= AI_VALIDATION_MIN_ACCURACY) Some(aiResults.validationResult)
-                else None
+            .flatMap {
+              case None => Future.successful(None) // No AI results; callAiApi already logs all errors.
+
+              // Save AI results to the database and submit validation if applicable.
+              case Some((aiResults, labelData)) =>
+                println(aiResults)
+                // Add AI information to the label_ai table.
+                val saveLabelAiAction = db.run(labelAiTable.save(aiResults))
 
               // If AI validations are enabled and confidence is above the threshold, submit a validation.
-              if (AI_VALIDATIONS_ON && aiValidation.isDefined) {
+              val submitValidationAction = if (AI_VALIDATIONS_ON && aiResults.validationAccuracy >= AI_VALIDATION_MIN_ACCURACY) {
                 val label      = labelData.label
                 val labelPoint = labelData.labelPoint
-                val validation = LabelValidation(
-                  0, labelId, aiValidation.get, label.severity, label.severity, label.tags, label.tags,
-                  SidewalkUserTable.aiUserId, label.missionId, Some(labelPoint.canvasX), Some(labelPoint.canvasY),
-                  labelPoint.heading, labelPoint.pitch, labelPoint.zoom.toFloat, LabelPointTable.canvasWidth,
-                  LabelPointTable.canvasHeight, startTime, aiResults.timeCreated, "SidewalkAI"
-                )
-                // TODO Need to check if this is a redone validation.
-                validationService.submitValidations(
-                  Seq(ValidationSubmission(validation, comment = None, undone = false, redone = false))
-                )
-              }
 
-              // Add AI information to the label_ai table.
-              db.run(labelAiTable.save(aiResults))
-              aiResults
-            })
+                // Get the AI's mission_id, then create and submit the validation.
+                getAiValidateMissionId(label.labelTypeId).flatMap { aiMissionId =>
+                  println("aiMissionId: " + aiMissionId)
+                  val validation = LabelValidation(
+                    0, labelId, aiResults.validationResult, label.severity, label.severity, label.tags, label.tags,
+                    SidewalkUserTable.aiUserId, aiMissionId, Some(labelPoint.canvasX), Some(labelPoint.canvasY),
+                    labelPoint.heading, labelPoint.pitch, labelPoint.zoom.toFloat, LabelPointTable.canvasWidth,
+                    LabelPointTable.canvasHeight, startTime, aiResults.timeCreated, "SidewalkAI"
+                  )
+                  validationService.submitValidations(
+                    Seq(ValidationSubmission(validation, comment = None, undone = false, redone = false))
+                  )
+                }
+              } else Future.successful(Seq.empty[Int])
+
+                // Run both actions in parallel and return the AI results.
+                saveLabelAiAction.zip(submitValidationAction).map(_ => Some(aiResults))
+            }
         }
-      }
+      }.map(_.flatten)
     } else {
       logger.info("AI validations or tag suggestions are disabled for this city.")
       Future.successful(Seq.empty)
@@ -93,7 +106,7 @@ class AiServiceImpl @Inject() (
    * @throws Exception if the API call fails or the response cannot be parsed.
    * @return A Future containing an optional LabelAi object, including tags, validation result, accuracy, API version.
    */
-  private def callAIAPI(labelData: LabelDataForAi): Future[Option[LabelAi]] = {
+  private def callAiApi(labelData: LabelDataForAi): Future[Option[LabelAi]] = {
     val SIDEWALK_AI_API_HOSTNAME: String = config.get[String]("sidewalk-ai-api-hostname")
     val url: String                      = s"https://${SIDEWALK_AI_API_HOSTNAME}/process"
     val labelId: Int                     = labelData.label.labelId
@@ -122,18 +135,26 @@ class AiServiceImpl @Inject() (
 
             Some(LabelAi(0, labelData.label.labelId, valResult, valAccuracy, tags, apiVersion, OffsetDateTime.now))
           } else {
-            logger.warn(s"AI API for label ${labelId} returned error status: ${response.status} - ${response.statusText}")
+            logger.warn(s"AI API for label $labelId returned error status: ${response.status} - ${response.statusText}")
             None
           }
         } catch {
           case e: Exception =>
-            logger.warn(s"Failed to parse AI API response for label ${labelId}: ${e.getMessage}")
+            logger.warn(s"Failed to parse AI API response for label $labelId: ${e.getMessage}")
             None
         }
       }
       .recover { case e: Exception =>
-        logger.warn(s"AI API request failed for label ${labelId}: ${e.getMessage}")
+        logger.warn(s"AI API request failed for label $labelId: ${e.getMessage}")
         None
       }
   }
+
+  /**
+   * Retrieves the AI validation mission_id from the db for a given label_type_id, cached since it doesn't change.
+   * @param labelTypeId The ID of the label type for which to get the AI validation mission_id
+   * @return A Future containing the AI validation mission_id
+   */
+  def getAiValidateMissionId(labelTypeId: Int): Future[Int] =
+    cacheApi.getOrElseUpdate[Int](s"getAiValidateMissionId($labelTypeId)")(db.run(missionTable.getAiValidateMissionId(labelTypeId)))
 }
--- conf/evolutions/default/281.sql
+++ conf/evolutions/default/281.sql
@@ -28,12 +28,42 @@ INSERT INTO user_current_region (user_id, region_id)
 SELECT '51b0b927-3c8a-45b2-93de-bd878d1e5cf4', 0
 WHERE NOT EXISTS (SELECT 1 FROM user_current_region WHERE user_id = '51b0b927-3c8a-45b2-93de-bd878d1e5cf4');
 
+-- Add a new mission_type for AI validation.
+-- While we're doing this, let's fix the skipped mission_type_id 6 by moving labelmapValidation from 7 to 6.
+UPDATE mission_type SET mission_type = 'aiValidation' WHERE mission_type_id = 7;
+INSERT INTO mission_type (mission_type_id, mission_type) SELECT 6, 'labelmapValidation';
+UPDATE mission SET mission_type_id = 6 WHERE mission_type_id = 7;
+
+-- Add missions to hold the AI validations, one for each label type. All AI validation will be attached to these.
+INSERT INTO mission (mission_type_id, user_id, completed, paid, distance_meters, distance_progress, region_id,
+                     labels_validated, labels_progress, skipped, label_type_id, current_audit_task_id)
+VALUES (7, '51b0b927-3c8a-45b2-93de-bd878d1e5cf4', FALSE, FALSE, NULL, NULL, NULL, 1, 0, FALSE, 1, NULL),
+       (7, '51b0b927-3c8a-45b2-93de-bd878d1e5cf4', FALSE, FALSE, NULL, NULL, NULL, 1, 0, FALSE, 2, NULL),
+       (7, '51b0b927-3c8a-45b2-93de-bd878d1e5cf4', FALSE, FALSE, NULL, NULL, NULL, 1, 0, FALSE, 3, NULL),
+       (7, '51b0b927-3c8a-45b2-93de-bd878d1e5cf4', FALSE, FALSE, NULL, NULL, NULL, 1, 0, FALSE, 4, NULL),
+       (7, '51b0b927-3c8a-45b2-93de-bd878d1e5cf4', FALSE, FALSE, NULL, NULL, NULL, 1, 0, FALSE, 5, NULL),
+       (7, '51b0b927-3c8a-45b2-93de-bd878d1e5cf4', FALSE, FALSE, NULL, NULL, NULL, 1, 0, FALSE, 6, NULL),
+       (7, '51b0b927-3c8a-45b2-93de-bd878d1e5cf4', FALSE, FALSE, NULL, NULL, NULL, 1, 0, FALSE, 7, NULL),
+       (7, '51b0b927-3c8a-45b2-93de-bd878d1e5cf4', FALSE, FALSE, NULL, NULL, NULL, 1, 0, FALSE, 9, NULL),
+       (7, '51b0b927-3c8a-45b2-93de-bd878d1e5cf4', FALSE, FALSE, NULL, NULL, NULL, 1, 0, FALSE, 10, NULL);
+
 -- Fix validation_task_interaction logs that started with "TagAdd" instead of "Click=TagAdd"
 UPDATE validation_task_interaction SET action = 'Click=' || action WHERE action LIKE 'TagAdd%';
 
 # --- !Downs
+-- Revert the interaction logs to their original state.
 UPDATE validation_task_interaction SET action = REPLACE(action, 'Click=', '') WHERE action LIKE 'Click=TagAdd%';
 
+-- Remove the validations created by AI validation.
+DELETE FROM label_validation WHERE user_id = '51b0b927-3c8a-45b2-93de-bd878d1e5cf4';
+
+-- Remove dummy missions created for AI validation and revert the labelmapValidation mission_type_id to 7.
+DELETE FROM mission WHERE user_id = '51b0b927-3c8a-45b2-93de-bd878d1e5cf4';
+UPDATE mission_type SET mission_type = 'labelmapValidation' WHERE mission_type_id = 7;
+UPDATE mission SET mission_type_id = 7 WHERE mission_type_id = 6;
+DELETE FROM mission_type WHERE mission_type_id = 6;
+
+-- Remove the dummy user and all associated data.
 DELETE FROM user_current_region WHERE user_id = '51b0b927-3c8a-45b2-93de-bd878d1e5cf4';
 DELETE FROM user_stat WHERE user_id = '51b0b927-3c8a-45b2-93de-bd878d1e5cf4';
 DELETE FROM sidewalk_login.user_role WHERE user_id = '51b0b927-3c8a-45b2-93de-bd878d1e5cf4';
