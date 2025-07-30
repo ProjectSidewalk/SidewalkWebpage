340e37ab386eceb2b6288d03ee9ba7bf7d23fdf5
Mikey Saugstad <michaelssaugstad@gmail.com>
2025-07-30 11:43:30 -0700

    fixes validate interaction logs that were missing Click= prefix

--- conf/evolutions/default/281.sql
+++ conf/evolutions/default/281.sql
@@ -1,4 +1,5 @@
 # --- !Ups
+-- Add new label_ai table.
 CREATE TABLE IF NOT EXISTS label_ai (
     label_ai_id SERIAL PRIMARY KEY,
     label_id INT NOT NULL,
@@ -10,39 +11,29 @@ CREATE TABLE IF NOT EXISTS label_ai (
     FOREIGN KEY (label_id) REFERENCES label (label_id)
 );
 
+-- Add a dummy user for SidewalkAI so that we can create entries in label_validation on its behalf.
 INSERT INTO sidewalk_login.sidewalk_user (user_id, username, email)
 SELECT '51b0b927-3c8a-45b2-93de-bd878d1e5cf4', 'SidewalkAI', 'sidewalkai@dummysitethatdoesnotexist.com'
-WHERE NOT EXISTS (
-    SELECT 1
-    FROM sidewalk_login.sidewalk_user
-    WHERE user_id = '51b0b927-3c8a-45b2-93de-bd878d1e5cf4'
-);
+WHERE NOT EXISTS (SELECT 1 FROM sidewalk_login.sidewalk_user WHERE user_id = '51b0b927-3c8a-45b2-93de-bd878d1e5cf4');
 
 INSERT INTO sidewalk_login.user_role (user_id, role_id)
 SELECT '51b0b927-3c8a-45b2-93de-bd878d1e5cf4', 1
-WHERE NOT EXISTS (
-    SELECT 1
-    FROM sidewalk_login.user_role
-    WHERE user_id = '51b0b927-3c8a-45b2-93de-bd878d1e5cf4'
-);
+WHERE NOT EXISTS (SELECT 1 FROM sidewalk_login.user_role WHERE user_id = '51b0b927-3c8a-45b2-93de-bd878d1e5cf4');
 
 INSERT INTO user_stat (user_id, meters_audited, labels_per_meter, high_quality, high_quality_manual, own_labels_validated, accuracy, excluded)
 SELECT '51b0b927-3c8a-45b2-93de-bd878d1e5cf4', 0.0, NULL, TRUE, NULL, 0, NULL, FALSE
-WHERE NOT EXISTS (
-    SELECT 1
-    FROM user_stat
-    WHERE user_id = '51b0b927-3c8a-45b2-93de-bd878d1e5cf4'
-);
+WHERE NOT EXISTS (SELECT 1 FROM user_stat WHERE user_id = '51b0b927-3c8a-45b2-93de-bd878d1e5cf4');
 
 INSERT INTO user_current_region (user_id, region_id)
 SELECT '51b0b927-3c8a-45b2-93de-bd878d1e5cf4', 0
-WHERE NOT EXISTS (
-    SELECT 1
-    FROM user_current_region
-    WHERE user_id = '51b0b927-3c8a-45b2-93de-bd878d1e5cf4'
-);
+WHERE NOT EXISTS (SELECT 1 FROM user_current_region WHERE user_id = '51b0b927-3c8a-45b2-93de-bd878d1e5cf4');
+
+-- Fix validation_task_interaction logs that started with "TagAdd" instead of "Click=TagAdd"
+UPDATE validation_task_interaction SET action = 'Click=' || action WHERE action LIKE 'TagAdd%';
 
 # --- !Downs
+UPDATE validation_task_interaction SET action = REPLACE(action, 'Click=', '') WHERE action LIKE 'Click=TagAdd%';
+
 DELETE FROM user_current_region WHERE user_id = '51b0b927-3c8a-45b2-93de-bd878d1e5cf4';
 DELETE FROM user_stat WHERE user_id = '51b0b927-3c8a-45b2-93de-bd878d1e5cf4';
 DELETE FROM sidewalk_login.user_role WHERE user_id = '51b0b927-3c8a-45b2-93de-bd878d1e5cf4';
