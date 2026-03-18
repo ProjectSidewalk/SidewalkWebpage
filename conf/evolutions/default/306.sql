86532b3aafcfbdf6e6817d9b7a925cc29b88068a
Mikey Saugstad <michaelssaugstad@gmail.com>
2026-03-17 17:01:10 -0700

    region_completion columns are now Doubles instead of Floats

--- conf/evolutions/default/306.sql
+++ conf/evolutions/default/306.sql
@@ -1,5 +1,13 @@
 # --- !Ups
 ALTER TABLE pano_data ADD COLUMN camera_roll DOUBLE PRECISION;
 
+ALTER TABLE region_completion
+    ALTER COLUMN total_distance TYPE DOUBLE PRECISION,
+    ALTER COLUMN audited_distance TYPE DOUBLE PRECISION;
+
 # --- !Downs
+ALTER TABLE region_completion
+    ALTER COLUMN total_distance TYPE REAL,
+    ALTER COLUMN audited_distance TYPE REAL;
+
 ALTER TABLE pano_data DROP COLUMN camera_roll;
