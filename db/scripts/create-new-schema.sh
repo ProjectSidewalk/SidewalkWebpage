4b1aaeeed2cce0de6eeebaf36c0be4245def0125
Mikey Saugstad <michaelssaugstad@gmail.com>
2026-05-12 11:34:56 -0700

    a fix for creating new dbs caused by adding the readonly_user role

--- db/scripts/create-new-schema.sh
+++ db/scripts/create-new-schema.sh
@@ -13,4 +13,14 @@ psql -v ON_ERROR_STOP=1 -U postgres -d sidewalk <<-EOSQL
     GRANT sidewalk TO $1;
     ALTER SCHEMA $1 OWNER TO sidewalk;
     ALTER ROLE $1 SET search_path = $1,sidewalk_login,public;
+
+    -- import-dump.sh registered default privileges under the sidewalk_init role; rebind them to the new role
+    -- so sidewalk_init has no lingering dependencies blocking it from being dropped on the next run.
+    DO \$\$
+    BEGIN
+        IF EXISTS (SELECT FROM pg_roles WHERE rolname = 'readonly_user') THEN
+            EXECUTE format('ALTER DEFAULT PRIVILEGES FOR ROLE sidewalk_init IN SCHEMA %I REVOKE SELECT ON TABLES FROM readonly_user', '$1');
+            EXECUTE format('ALTER DEFAULT PRIVILEGES FOR ROLE %I IN SCHEMA %I GRANT SELECT ON TABLES TO readonly_user', '$1', '$1');
+        END IF;
+    END \$\$;
 EOSQL
